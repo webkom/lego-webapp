9a88cd9f98423426191556dbb9cb6c0ba3a51426
Hanse <hanse19@gmail.com>
2015-01-30 19:08:41 +0100

    Initial calendar impl

---
+++ src/components/EventCalendar.js
@@ -0,0 +1,79 @@
+'use strict';
+
+var React = require('react');
+var moment = require('moment');
+var EventStore = require('../stores/EventStore');
+
+var EventCalendar = React.createClass({
+
+  getDefaultProps() {
+    return {
+      weekOffset: 0
+    };
+  },
+
+  getInitialState() {
+    return {
+      date: moment()
+    };
+  },
+
+  days() {
+    var days = [];
+    var date = this.state.date.startOf('month');
+    var diff = date.weekday() - this.props.weekOffset;
+
+    var i, day;
+    for (i = 0; i < diff; i++) {
+      day = moment([this.state.date.year(), this.state.date.month(), i-diff+1]);
+      days.push({day: day, classNames: 'prev-month'});
+    }
+
+    var numberOfDays = date.daysInMonth();
+    for (i = 1; i <= numberOfDays; i++) {
+      day = moment([this.state.date.year(), this.state.date.month(), i]);
+      days.push({day: day});
+    }
+
+    i = 1;
+    while (days.length % 7 !== 0) {
+      day = moment([this.state.date.year(), this.state.date.month(), numberOfDays+1]);
+      days.push({day: day, classNames: 'next-month'});
+    }
+
+    return days;
+  },
+
+  _onNext() {
+    this.setState({date: this.state.date.add(1, 'months')});
+  },
+
+  _onPrev() {
+    this.setState({date: this.state.date.subtract(1, 'months')});
+  },
+
+  render() {
+    return (
+      <div className='content'>
+        <div className='calendar'>
+          <h2>
+            <span onClick={this._onPrev}>&laquo;</span>
+            <span>{this.state.date.format('MMMM YYYY')}</span>
+            <span onClick={this._onNext}>&raquo;</span>
+          </h2>
+          <div className='calendar-grid'>
+            {this.days().map(function(day, i) {
+              return (
+                <div key={'day-' + i} className={day.classNames}>
+                  <span className='day-number'>{day.day.date()}</span>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = EventCalendar;
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Router from 'react-router';
 import App from './components/App';
 import Overview from './components/Overview';
-import EventCalendarPage from './components/EventCalendarPage';
+import EventCalendar from './components/EventCalendar';
 import EventPage from './components/EventPage';
 import NotFoundPage from './components/NotFoundPage';
 import StaticPage from './components/StaticPage';
@@ -11,7 +11,7 @@ var {Routes, Route, DefaultRoute, NotFoundRoute} = Router;
 
 var routes = (
   <Route handler={App} path='/'>
-    <Route name='events' handler={EventCalendarPage} />
+    <Route name='events' handler={EventCalendar} />
     <Route name='event' path='/events/:eventId' handler={EventPage} />
     <Route name='staticPage' path='/p/:pageSlug' handler={StaticPage} />
     <DefaultRoute name='overview' handler={Overview} />
