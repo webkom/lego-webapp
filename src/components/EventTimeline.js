3e717e06bb15c156beefbc81db8d2182e1327aa4
Hanse <hanse19@gmail.com>
2015-01-30 18:17:54 +0100

    Realign the front page

--- src/components/EventTimeline.js
+++ src/components/EventTimeline.js
@@ -10,7 +10,8 @@ function randomImage() {
     'http://www.iteraconsulting.com.ua/App_Themes/Default/Images/Logo_stor.png',
     'https://abakus.no/uploads/events/thumbs/2015-01-16-1359-TD%20Logo-260x110.jpg',
     'https://abakus.no/uploads/events/thumbs/2011-06-08-1343-Accenture-260x110.jpg',
-    'https://abakus.no/uploads/events/thumbs/2014-11-15-0157-2013-08-10-2050-visma_bilde-578x150-578x150.jpg'
+    'https://abakus.no/uploads/events/thumbs/2014-11-15-0157-2013-08-10-2050-visma_bilde-578x150-578x150.jpg',
+    'https://abakus.no/uploads/events/thumbs/2015-01-21-0910-lvvvvve-260x110.jpg'
   ];
   return urls[(Math.random() * urls.length) | 0];
 }
@@ -24,14 +25,11 @@ var EventTimeline = React.createClass({
       {events.map(function(event) {
         return (
           <Link to='event' params={{eventId: event.id}} key={event.id}>
-            <div className={'event-timeline-item ' + event.type}>
-              <div className='feed-event-image'><img src={randomImage()} width='150px' height='100px'/></div>
-              <div className='feed-event-description'>
-                <p className='event-time-location' style={{backgroundColor: event.color}}>
-                  <Icon name='clock-o' /> <Time time={event.starts_at} format='dddd HH:mm' /> @ H3
-                </p>
-              </div>
-            </div>
+            <p className='event-location'><Icon name='map-marker'/> H3</p>
+            <img src={randomImage()} />
+            <p className='event-time' style={{backgroundColor: event.color}}>
+              <Icon name='clock-o' /> <Time time={event.starts_at} format='dddd HH:mm' />
+            </p>
           </Link>
         );
       })}
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -30,7 +30,8 @@ var Overview = React.createClass({
     return (
       <section>
         <div className='content'>
-          <EventTimeline events={events.slice(0, 4)} />
+          <h2 className='inside-feed'>Kommende aktiviteter</h2>
+          <EventTimeline events={events.slice(0, 5)} />
 
           <div className='sidebar'>
             <SidebarBlock title='Mine arrangementer'>
@@ -42,23 +43,30 @@ var Overview = React.createClass({
               <p><a>Finn noen som passer deg &rarr;</a></p>
             </SidebarBlock>
 
-            <SidebarBlock title='Nyeste README'>
-              <img src='http://readme.abakus.no/bilder/14/2014-03.jpg' />
-            </SidebarBlock>
-
-            <SidebarBlock title='Komitéene'>
-              <p>Hello World</p>
+            <SidebarBlock title='Live feed'>
+              <ul className='live-feed'>
+                <li><Icon name='user' /> Syll er i venteliste på Låvefest</li>
+                <li><Icon name='beer' /> Ek meldte seg på Låvefest</li>
+                <li><Icon name='flag' /> Påmelding åpnet for Låvefest</li>
+                <li><Icon name='clock-o' /> Bedpress Steria om 15 min</li>
+              </ul>
             </SidebarBlock>
           </div>
 
           <LoadingIndicator loading={events.length === 0}>
             <div className='feed-container'>
+              <h2 className='inside-feed'>På plakaten</h2>
               <div className='event-grid'>
                 {events.slice(4).map(function(event) {
                   return (
                     <Link to='event' params={{eventId: event.id}} key={event.id} className={'feed-event-box ' + event.type}>
-                      <h3>{event.name}</h3>
-                      {event.description.slice(0, 140)}
+                      <article>
+                        <div><img src={Math.random() > .5 ? 'https://s3.amazonaws.com/f.cl.ly/items/411l1P330u2X0M3P3D0q/Skjermbilde%202015-01-30%20kl.%2017.27.53.png' : 'http://himmelpartner.no/wp-content/uploads/2013/09/L%C3%A5vefest31-08-2013-3869.jpg'} /></div>
+                        <div>
+                          <h3>{event.name}</h3>
+                          {event.description.slice(0, 200)}
+                        </div>
+                      </article>
                     </Link>
                   );
                 })}
