9a8e2abea796f63820c4fa7812436d44e366f62b
Orhan Hirsch <orhanhenrik@yahoo.no>
2015-10-01 22:14:04 +0200

    Removed more comments

--- src/containers/HeaderContainer.js
+++ src/containers/HeaderContainer.js
@@ -1,4 +1,4 @@
-import React, { Component /* , PropTypes */ } from 'react';
+import React, { Component } from 'react';
 
 import Header from '../components/Header';
 import { connect } from 'react-redux';
