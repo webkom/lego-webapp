1cf4e7bca53c89aa12551116b2d5f8081a631315
ekmartin <mail@ekmartin.no>
2015-10-27 16:05:38 +0100

    Use a functional react component instead of renderEvent

--- src/components/EventPage.js
+++ src/components/EventPage.js
@@ -2,51 +2,47 @@ import React, { Component, PropTypes } from 'react';
 import RequireLogin from './RequireLogin';
 import LoadingIndicator from './LoadingIndicator';
 
+const Event = ({ event, loggedIn }) => (
+  <section className='content event-page'>
+    <h2>{event.title}</h2>
+    <article className='event-ingress'>
+      {event.ingress}
+    </article>
+    <article className='event-body'>
+      {event.text}
+    </article>
+    <div className='event-open-for'>
+      <h3>Åpent for</h3>
+    </div>
+
+    <RequireLogin loggedIn={loggedIn}>
+      <h3>Bli med på dette arrangementet</h3>
+      <form className='event-participate'>
+        <textarea placeholder='Melding til arrangører' />
+        <button type='submit'>Bli med</button>
+
+        <p>Påmeldingen stenger 13:37</p>
+      </form>
+    </RequireLogin>
+  </section>
+);
+
 export default class EventPage extends Component {
   static propTypes = {
     event: PropTypes.object,
     loggedIn: PropTypes.bool.isRequired
   }
 
-  renderEvent(event) {
-    return (
-      <section className='content event-page'>
-        <h2>{event.title}</h2>
-        <article className='event-ingress'>
-          {event.ingress}
-        </article>
-        <article className='event-body'>
-          {event.text}
-        </article>
-        <div className='event-open-for'>
-          <h3>Åpent for</h3>
-        </div>
-
-        <RequireLogin loggedIn={this.props.loggedIn}>
-          <h3>Bli med på dette arrangementet</h3>
-          <form className='event-participate'>
-            <textarea placeholder='Melding til arrangører' />
-            <button type='submit'>Bli med</button>
-
-            <p>Påmeldingen stenger 13:37</p>
-          </form>
-        </RequireLogin>
-      </section>
-    );
-  }
-
   render() {
     const { event } = this.props;
-
     return (
       <section>
         <LoadingIndicator loading={!event}>
           <div>
-            {event && this.renderEvent(event)}
+            {event && <Event {...this.props} />}
           </div>
         </LoadingIndicator>
       </section>
     );
   }
-
 }
