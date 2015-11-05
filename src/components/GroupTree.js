8104b82ac3e2ca2206562ce9416cef8d450e48a5
ekmartin <mail@ekmartin.no>
2015-11-05 19:55:41 +0100

    Simplify tree generation

--- src/components/GroupTree.js
+++ src/components/GroupTree.js
@@ -1,11 +1,12 @@
+import '../styles/react-treeview.css';
 import React, { Component, PropTypes } from 'react';
 import TreeView from 'react-treeview';
-import '../styles/react-treeview.css';
 import { Link } from 'react-router';
+import { generateTreeStructure } from '../utils';
 
 function generateTreeView(groups) {
-  return groups.map( group => {
-    if (group.children && group.children.length) {
+  return groups.map(group => {
+    if (group.children.length) {
       const nodeLabel = <Link to={`/admin/groups/${group.id}`}>{group.name}</Link>;
       return (
         <TreeView key={group.id} nodeLabel={nodeLabel} defaultCollapsed={false}>
@@ -13,40 +14,13 @@ function generateTreeView(groups) {
         </TreeView>
       );
     }
-    return <div key={group.id} className='info'><Link to={`/admin/groups/${group.id}`}>{group.name}</Link></div>;
-  });
-}
 
-function generateTreeStructure(groups) {
-  const tree = groups.reduce( (acc, group) => {
-    group.children = [];
-    acc[group.id] = group;
-    return acc;
-  }, {});
-  let maxDepth = 0;
-  for (const id in tree) {
-    let depth = 0;
-    let current = tree[id];
-    while (current.parent) {
-      depth += 1;
-      current = tree[current.parent];
-    }
-    tree[id].depth = depth;
-    maxDepth = maxDepth < depth ? depth : maxDepth;
-  }
-  for (let depth = maxDepth; depth >= 1; depth--) {
-    for (const id in tree) {
-      if (tree[id].depth === depth) {
-        tree[tree[id].parent].children.push(tree[id]);
-        delete tree[id];
-      }
-    }
-  }
-  const treeArray = [];
-  for (const id in tree) {
-    treeArray.push(tree[id]);
-  }
-  return treeArray;
+    return (
+      <div key={group.id} className='info'>
+        <Link to={`/admin/groups/${group.id}`}>{group.name}</Link>
+      </div>
+    );
+  });
 }
 
 export default class GroupTree extends Component {
@@ -64,11 +38,5 @@ export default class GroupTree extends Component {
         {generateTreeView(tree)}
       </div>
     );
-    /*
-        <TreeView key={1} nodeLabel={"Hello"} defaultCollapsed={false}>
-          <TreeView key={2} nodeLabel={"Hello 2"} defaultCollapsed={false}/>
-          <TreeView key={3} nodeLabel={"Hello 3"} defaultCollapsed={false}/>
-        </TreeView>
-    */
   }
 }
---
+++ src/utils/index.js
@@ -0,0 +1,38 @@
+/**
+ * Generates a tree structure on the form of:
+ * [
+ *   {
+ *   	 ...node,
+ *   	 children: [
+ *   	   {
+ *   	     ...node,
+ *   	     children: [...]
+ *   	   }
+ *   	 ]
+ *   }
+ * ]
+ *
+ * @param  {Object[]} nodes
+ * @return {Object[]} tree
+ */
+export function generateTreeStructure(nodes) {
+  // Create a map of id -> node for retrievals later:
+  const nodeMap = nodes.reduce((acc, node) => {
+    return {
+      ...acc,
+      [node.id]: { ...node, children: [] }
+    };
+  }, {});
+
+  return nodes.reduce((acc, { id }) => {
+    const node = nodeMap[id];
+    if (!node.parent) {
+      acc.push(node);
+    } else {
+      const parent = nodeMap[node.parent];
+      parent.children.push(node);
+    }
+
+    return acc;
+  }, []);
+}
