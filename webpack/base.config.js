5baa63714ef97244ffaf9806e21d136af9ebb37c
ekmartin <mail@ekmartin.com>
2016-02-05 00:17:00 +0100

    Fix live reload

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -6,10 +6,11 @@ module.exports = {
     publicPath: ''
   },
   resolve: {
-    extensions: ['', '.js', '.jsx', '.css', '.png']
+    extensions: ['', '.js', '.jsx', '.json', '.css', '.png']
   },
   module: {
     loaders: [
+      { include: /\.json$/, loaders: ['json-loader'] },
       { test: /\.jsx?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
       { test: /\.css$/, loader: 'style!css!postcss' },
       { test: /\.png$/, loader: 'url-loader?mimetype=image/png' }
