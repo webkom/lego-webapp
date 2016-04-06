435c6448a664e8115e2fd1069a8f2a9a52719622
Hanse <hanse19@gmail.com>
2016-04-06 20:11:44 +0200

    Remove karma :(

--- testing/setup.js
+++ testing/setup.js
@@ -1,11 +1,15 @@
-import 'babel-polyfill';
-
 import moment from 'moment';
 import expect from 'expect';
 import expectJSX from 'expect-jsx';
+import { jsdom } from 'jsdom';
 import sinon from 'sinon';
 import chai from 'chai';
 import chaiEnzyme from 'chai-enzyme';
+
+moment.locale('nb-NO');
+
+require.extensions['.css'] = () => {};
+require.extensions['.png'] = () => {};
 
 chai.use(chaiEnzyme());
 
@@ -16,9 +20,6 @@ expect.extend(expectJSX);
 global.chai = chai;
 global.sinon = sinon;
 global.expect = chai.expect;
-global.should = chai.should();
+global.document = jsdom('<!doctype html><html><body></body></html>');
 global.window = document.defaultView;
 global.navigator = global.window.navigator;
-
-const context = require.context('../app', true, /.spec\.js$/);
-context.keys().forEach(context);
