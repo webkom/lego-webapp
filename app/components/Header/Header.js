c9fbca75c0a50c0b3005635d01f37cf69941f0ab
Martin Ek <mail@ekmartin.com>
2016-04-21 02:55:41 +0200

    Work in progress design

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -3,11 +3,12 @@
 import styles from './Header.css';
 import React, { Component } from 'react';
 import { Link, IndexLink } from 'react-router';
-import { Modal } from 'react-overlays';
-import cx from 'classnames';
-import LoginForm from '../LoginForm';
-import Search from '../Search';
-import ButtonTriggeredDropdown from '../ButtonTriggeredDropdown';
+
+import Icon from './Icon';
+
+type State = {
+  searchOpen: boolean;
+};
 
 type Props = {
   children: any;
@@ -18,103 +19,34 @@ type Props = {
   loginFailed: boolean;
 };
 
-type State = {
-  accountOpen: boolean;
-  searchOpen: boolean;
-  notificationsOpen: boolean;
-};
-
 export default class Header extends Component {
   props: Props;
 
   state: State = {
-    accountOpen: false,
-    searchOpen: false,
-    notificationsOpen: false
+    searchOpen: false
   };
 
   render() {
-    const {
-      auth,
-      login, logout,
-      loggedIn, loginFailed
-    } = this.props;
-
     return (
-      <header className={styles.root}>
-        <div className={styles.content}>
-          <IndexLink
-            to='/'
-            className={styles.logo}
-          >
-            Abakus
+      <header className='Header'>
+        <div className='Header__content'>
+          <IndexLink to='/' className='Header__logo'>
+            <img src='/images/logo_dark.png' />
           </IndexLink>
 
-          <div className={styles.navigation}>
-            <Link to='/events' activeClassName={styles.activeLink}>Arrangementer</Link>
-            <Link to='/career' activeClassName={styles.activeLink}>Karriere</Link>
-            <Link to='/readme' activeClassName={styles.activeLink}>README</Link>
-            <Link to='/quotes' activeClassName={styles.activeLink}>Sitater</Link>
-          </div>
-
-          <div>
-            <ButtonTriggeredDropdown
-              buttonClassName={styles.contentButton}
-              contentClassName={styles.dropdown}
-              iconName='bell'
-              show={this.state.notificationsOpen}
-              toggle={() => this.setState({
-                notificationsOpen: !this.state.notificationsOpen
-              })}
-            >
-              <h2>No Notifications</h2>
-            </ButtonTriggeredDropdown>
-
-            <ButtonTriggeredDropdown
-              buttonClassName={styles.contentButton}
-              contentClassName={cx(styles.dropdown, loginFailed && 'animated shake')}
-              iconName='user'
-              show={this.state.accountOpen}
-              toggle={() => this.setState({ accountOpen: !this.state.accountOpen })}
-            >
-              {!loggedIn && (
-                <LoginForm
-                  login={login}
-                  className={styles.loginForm}
-                />
-              )}
-
-              {loggedIn && (
-                <div>
-                  <h2>{auth && auth.username}</h2>
-                  <ul className={styles.dropdownMenu}>
-                    <li><Link to='/users/me'>My Profile</Link></li>
-                    <li><Link to='/users/me/settings'>Settings</Link></li>
-                    <li><Link to='events'>Favorites</Link></li>
-                    <li><a onClick={logout}>Log out</a></li>
-                  </ul>
-                </div>
-              )}
-            </ButtonTriggeredDropdown>
-
-            <button
-              className={styles.contentButton}
-              onClick={() => this.setState({ searchOpen: !this.state.searchOpen })}
-            >
-              <i className='fa fa-search' />
-            </button>
-
-            <Modal
-              show={this.state.searchOpen}
-              onHide={() => this.setState({ searchOpen: false })}
-              backdropClassName={styles.backdrop}
-              backdrop
-            >
-              <Search
-                isOpen={this.state.searchOpen}
-                onCloseSearch={() => this.setState({ searchOpen: false })}
-              />
-            </Modal>
+          <div className='Header__navigation'>
+            <div>
+              <Link to='/events'>Arrangementer</Link>
+              <Link to=''>Karriere</Link>
+            </div>
+            <div className='SearchBox'>
+              <Icon name='search' />
+              <input placeholder='Søk...' className='SearchBox__input' />
+            </div>
+            <div>
+              <Link to=''>README</Link>
+              <Link to='/quotes'>Sitater</Link>
+            </div>
           </div>
         </div>
       </header>
--- app/routes/overview/components/Overview.js
+++ app/routes/overview/components/Overview.js
@@ -7,8 +7,36 @@ import colorForEvent from 'app/routes/events/colorForEvent';
 
 let id = 0;
 function getImage() {
-  return `http://lorempixel.com/800/400?${id++}`;
+  return `http://unplash.it/800/400?${id++}`;
 }
+
+const EVENT_TYPES = [
+  'Bedriftspresentasjon',
+  'Kurs',
+  'Fest',
+  'Annet',
+  'Arrangement'
+];
+
+const OverviewItem = ({ event }) => (
+  <div key={event.id} className='Overview__item'>
+    <h4 style={{ color: colorForEvent(event.eventType) }} className='Overview__item__type'>
+      {EVENT_TYPES[event.eventType]}
+    </h4>
+
+    <h2 className='Overview__item__title'>
+      <Link to={`/events/${event.id}`}>
+        {event.title}
+      </Link>
+    </h2>
+    <span className='Overview__item__moreInfo'>
+      <Time time={event.startTime} format='DD.MM HH:mm' />
+      <span> - </span>
+      <span>{event.location}</span>
+    </span>
+    <p className='Overview__item__description'>{event.description}</p>
+  </div>
+);
 
 export default class Overview extends Component {
 
@@ -21,41 +49,25 @@ export default class Overview extends Component {
   render() {
     const { events } = this.props;
     return (
-      <div className={styles.root}>
-        <div className='UpcomingEvents'>
-          {events.slice(0, 5).map((event) =>
-            <Link
-              key={event.id}
-              className='UpcomingEvents__item'
-              to={`/events/${event.id}`}
-              params={{ eventId: event.id }}
-              style={{ background: `url(${getImage()})` }}
-            >
-              <span
-                className='UpcomingEvents__item__title'
-                style={{
-                  borderBottom: `6px solid ${colorForEvent(event.eventType)}`
-                }}
-              >
-                {event.title}
-              </span>
+      <section className={styles.root}>
+        <div className='u-container Frontpage'>
+          <div className='Overview'>
+            {[0, 1].map(() => events.map((event) => <OverviewItem event={event} />))}
+          </div>
 
-              <div className='UpcomingEvents__item__moreInfo'>
-                <Icon name='clock-o' />&nbsp;
-                <Time time={event.startTime} format='DD.MM HH:mm' />&nbsp;
-                <Icon name='map-marker' />&nbsp;<span>{event.location}</span>
+          <div className='Sidebar'>
+            <div className='Profile'>
+              <img className='Profile__avatar' src='http://api.adorable.io/avatars/70/ekmartin.png'></img>
+              <div className='Profile__user'>
+                <h3>Martin Ek</h3>
+                <Icon name='chevron-down' />
               </div>
-            </Link>
-          )}
-        </div>
-
-        <div className='news'>
-          <h2 className='u-heading-with-bar'>Nyheter på tegnspråk</h2>
-          {events.map((event) =>
-            <div key={event.id}>
-              <Link to={`/events/${event.id}`}>{event.title} - {event.ingress}</Link>
             </div>
-          )}
+
+            <div className='Upcoming'>
+              <h2>Arrangementer</h2>
+            </div>
+          </div>
         </div>
       </div>
     );
