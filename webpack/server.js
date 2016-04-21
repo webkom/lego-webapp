0dd7f9058a6f357b81bb9c5afdac263c89476548
Martin Ek <mail@ekmartin.com>
2016-04-21 02:55:24 +0200

    Serve static files with webpack dev server

--- webpack/server.js
+++ webpack/server.js
@@ -1,23 +1,21 @@
 const path = require('path');
 const express = require('express');
 const config = require('./dev.config');
+const compiler = require('webpack')(config);
 
 const app = express();
 
 app.set('host', process.env.HOST || 'localhost');
 app.set('port', process.env.PORT || 3000);
 
-if (process.env.NODE_ENV !== 'production') {
-  const compiler = require('webpack')(config);
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
-  app.use(require('webpack-dev-middleware')(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath
-  }));
+app.use(require('webpack-hot-middleware')(compiler));
 
-  app.use(require('webpack-hot-middleware')(compiler));
-}
-
+app.use(express.static('public'));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
