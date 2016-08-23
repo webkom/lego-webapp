c0a8e357e7e418f58372632ed0748bbbf854e94a
Finn Julius Stephansen-Smith <finnjs@stud.ntnu.no>
2016-08-23 22:46:52 +0200

    Initial commit for Footer

---
+++ app/components/Footer/index.js
@@ -0,0 +1,28 @@
+import styles from './Footer.css';
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <footer className={styles.footer}>
+        <div className={styles.footerContent}>
+          <div className={styles.section}>
+            <h2>README</h2>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce semper nunc at ex consequat ultricies.
+          </div>
+          <div className={styles.section}>
+            <h2>Tilfeldig sitat</h2>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce semper nunc at ex consequat ultricies.
+          </div>
+          <div className={styles.section}>
+            <h2>Kontakt oss</h2>
+              <a href='mailto:abakus@abakus.no'>abakus@abakus.no</a> <br />
+              Abakus Linjeforening<br />
+              Sem Sælands vei 7-9<br />
+              7491 Trondheim
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
