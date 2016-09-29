a4c19c0d52dd75b65671c6d3fc5e9fcc7a9545e1
Hanse <hkri@koren.im>
2016-09-29 22:37:12 +0200

    List the eevents in groups

--- app/routes/events/components/EventList.js
+++ app/routes/events/components/EventList.js
@@ -1,25 +1,67 @@
 // @flow
 
 import React, { Component } from 'react';
+import { Link } from 'react-router';
+import moment from 'moment';
 import Toolbar from './Toolbar';
+import colorForEvent from '../colorForEvent';
 import styles from './EventList.css';
+
+// Kinda works
+function groupEvents(events) {
+  const now = moment().subtract(30, 'weeks');
+  const nextWeek = now.clone().add(1, 'week');
+
+  const groupers = {
+    currentWeek: (event) => moment(event.startTime).isSame(now, 'week'),
+    nextWeek: (event) => moment(event.startTime).isSame(nextWeek, 'week'),
+    later: (event) => moment(event.startTime).isAfter(nextWeek)
+  };
+
+  return events.reduce((result, event) => {
+    for (const groupName in groupers) {
+      if (groupers[groupName](event)) {
+        result[groupName] = (result[groupName] || []).concat([event]);
+        return result;
+      }
+    }
+
+    return result;
+  }, {});
+}
+
+function Events({ events = [] }) {
+  return (
+    <div className={styles.eventGroup}>
+      {events.map((event, i) => (
+        <div
+          key={i}
+          style={{ borderColor: colorForEvent(event.eventType) }}
+          className={styles.eventItem}
+        >
+          <Link to={`/events/${event.id}`}>
+            <h3 className={styles.eventItemTitle}>{event.title}</h3>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 class EventList extends Component {
   render() {
-    const { events } = this.props;
+    const events = groupEvents(this.props.events);
     return (
       <div className={styles.root}>
         <Toolbar />
-        <h2 className={styles.periodHeading}>This week</h2>
-        {events.slice(0, 2).map((event) => (
-          <div>
-            <pre>{JSON.stringify(event, null, 2)}</pre>
-          </div>
-        ))}
+        <h2 className={styles.heading}>This week</h2>
+        <Events events={events.currentWeek} />
 
-        <h2 className={styles.periodHeading}>Next Week</h2>
+        <h2 className={styles.heading}>Next Week</h2>
+        <Events events={events.nextWeek} />
 
-        <h2 className={styles.periodHeading}>Later</h2>
+        <h2 className={styles.heading}>Later</h2>
+        <Events events={events.later} />
       </div>
     );
   }
