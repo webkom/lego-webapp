024ab3b09e0c281e54fc217cd9b8cb35d1d1c913
Martin Hafskjold Thoresen <martinhath@gmail.com>
2016-10-12 00:07:51 +0200

    Make Tag a SFC

--- app/components/Tag/index.js
+++ app/components/Tag/index.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react';
+// @flow
+
+import React from 'react';
 import { Link } from 'react-router';
 import styles from './Tag.css';
 
-export default class Tag extends Component {
-
-  render() {
-    const { tag, small } = this.props;
-    let className = small ? styles.tagLinkSmall : styles.tagLink;
-
-    return (
-      <div className={styles.linkSpacing}>
-        <Link className={className} to={'/'}>
-          #{tag}
-        </Link>
-      </div>
-    );
-  }
+type Props = {
+  tag: string;
+  small?: boolean
 }
+
+function Tag({ tag, small }: Props) {
+  const className = small ? styles.tagLinkSmall : styles.tagLink;
+  return (
+    <div className={styles.linkSpacing}>
+      <Link className={className} to={'/'}>
+        #{tag}
+      </Link>
+    </div>
+  );
+}
+
+export default Tag;
