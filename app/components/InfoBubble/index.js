346b8396c1a9c55a0caa84050fc45fc3ef0bcebc
Finn Julius Stephansen-Smith <finnjs@stud.ntnu.no>
2016-10-11 19:11:57 +0200

    Use <Icon />, rename css, refactor to Stateless Functional Component

--- app/components/InfoBubble/index.js
+++ app/components/InfoBubble/index.js
@@ -1,29 +1,25 @@
-import React, { Component } from 'react';
-import styles from './infoBubble.css';
+import React from 'react';
+import styles from './InfoBubble.css';
+import Icon from 'app/components/Icon';
 
 type Props = {
   icon: string,
   data: string,
   meta: string,
-  style: Object
+  style?: Object
 };
 
-export default class InfoBubble extends Component {
-
-  props: Props;
-
-  render() {
-    const { icon, data, meta, style } = this.props;
-
-    return (
-      <div className={styles.infoBubble} style={style}>
-        <div className={styles.bubble}>
-          <span className={styles.circle}><i className='fa fa-circle'></i></span>
-          <span className={styles.icon}>{icon ? (<i className={`fa fa-${icon}`}></i>) : '-'}</span>
-        </div>
-        <span className={styles.data}>{data || '-'}</span>
-        <span className={styles.meta}>{meta || '-'}</span>
+function InfoBubble({ icon, data, meta, style }: Props) {
+  return (
+    <div className={styles.infoBubble} style={style}>
+      <div className={styles.bubble}>
+        <span className={styles.circle}><Icon name={'circle'} className={styles.abaRed}/></span>
+        <span className={styles.icon}>{icon ? (<i className={`fa fa-${icon}`}></i>) : '-'}</span>
       </div>
-    );
-  }
+      <span className={styles.data}>{data || '-'}</span>
+      <span className={styles.meta}>{meta || '-'}</span>
+    </div>
+  );
 }
+
+export default InfoBubble;
