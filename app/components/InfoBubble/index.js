51e1b90d826cd824b94953312959f5556d2e0df1
Finn Julius Stephansen-Smith <finnjs@stud.ntnu.no>
2016-11-08 16:59:48 +0100

    Initial commit for InfoBubble improvement

--- app/components/InfoBubble/index.js
+++ app/components/InfoBubble/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import styles from './InfoBubble.css';
 import Icon from 'app/components/Icon';
+import cx from 'classnames';
 
 type Props = {
   icon: string,
   data: string,
-  meta: string,
-  style?: Object
+  meta: string
 };
 
-function InfoBubble({ icon, data, meta, style }: Props) {
+function InfoBubble({ icon, data, meta, className, ...props }: Props) {
   return (
-    <div className={styles.infoBubble} style={style}>
+    <div
+      className={cx(styles.infoBubble, className)}
+      {...props}
+    >
       <div className={styles.bubble}><Icon name={icon} className={styles.icon} /></div>
       <span className={styles.data}>{data || '-'}</span>
       <span className={styles.meta}>{meta || '-'}</span>
