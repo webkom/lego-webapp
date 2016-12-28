33a9d16fadc21e07bacf22f757fb57a121bfb66e
Martin Ek <mail@ekmartin.com>
2016-12-28 14:24:55 +0100

    lint: fix config and errors

--- app/components/Editor/KeyBindings.js
+++ app/components/Editor/KeyBindings.js
@@ -5,6 +5,4 @@ import { getDefaultKeyBinding } from 'draft-js';
 Emits various key commands to be used by `handleKeyCommand` in `Editor` based
 on various key combos.
 */
-export default (e) => {
-  return getDefaultKeyBinding(e);
-};
+export default (e) => getDefaultKeyBinding(e);
