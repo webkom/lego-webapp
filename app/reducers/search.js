4d0fba2d4f960d873b6f5e0cd5700c4d341cd047
Kristoffer K Larsen <kristoffer@larsen.so>
2017-01-12 11:54:41 +0100

    Order search result according to content-type

--- app/reducers/search.js
+++ app/reducers/search.js
@@ -36,6 +36,7 @@ export default function search(state = initialState, action) {
     case Search.TOGGLE_OPEN:
       return {
         ...state,
+        results: [],
         open: !state.open
       };
 
