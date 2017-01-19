804727d1b2901fab2e1f9d3db32be06c67074c94
hbjornoy <havard.bjornoy@gmail.com>
2017-01-19 20:11:45 +0100

    removed mutate

--- app/reducers/companies.js
+++ app/reducers/companies.js
@@ -4,13 +4,6 @@ import createEntityReducer from 'app/utils/createEntityReducer';
 export default createEntityReducer({
   key: 'companies',
   types: {
-    fetch: Bdb.FETCH,
-    mutate: Bdb.ADD
-  },
-  mutate(state, action) {
-    switch (action.type) {
-      default:
-        return state;
-    }
+    fetch: Bdb.FETCH
   }
 });
