f93856e7ff22ae8f63cf0506c478650db0262375
hbjornoy <havard.bjornoy@gmail.com>
2017-02-23 20:23:31 +0100

    temporary infobubblestuff

--- app/components/InfoBubble/index.js
+++ app/components/InfoBubble/index.js
@@ -21,16 +21,23 @@ const withLinkIcon = (link, icon, bubbleClass, iconClass) => (
   <a href={httpCheck(link)}>{noLinkIcon(icon, bubbleClass, iconClass)}</a>
 );
 
-const noLinkData = (dataClass, data, isMeta = false) => (
-  <span className={cx(isMeta ? styles.meta : styles.data, dataClass)}>{data || '-'}</span>
+const noLinkData = (dataClass, data) => (
+  <span className={cx(styles.data, dataClass)}>{data || '-'}</span>
 );
 
 const withLinkData = (link, dataClass, data) => (
   <a href={httpCheck(link)} style={{ margin: '0 auto' }}>{noLinkData(dataClass, data)}</a>
 );
 
-function InfoBubble({ icon, data, meta, className, bubbleClass, dataClass,
-  metaClass, iconClass, link, ...props }: Props) {
+function InfoBubble({ icon, data, meta, className, small = false,
+  link, ...props }: Props) {
+  const bubbleClass = small ? styles.smallBubble : styles.bubble;
+  const iconClass = small ? styles.smallIcon : styles.icon;
+  const dataClass = small ? styles.smallData : styles.data;
+  const metaClass = small ? styles.smallMeta : styles.meta;
+
+  // const Component = link ? 'a' : 'span';
+
   return (
     <div
       className={cx(styles.infoBubble, className)}
@@ -39,7 +46,7 @@ function InfoBubble({ icon, data, meta, className, bubbleClass, dataClass,
       {link ? withLinkIcon(link, icon, bubbleClass, iconClass) :
         noLinkIcon(icon, bubbleClass, iconClass)}
       {link ? withLinkData(link, dataClass, data) : noLinkData(dataClass, data)}
-      {meta && noLinkData(metaClass, meta, true)}
+      {meta && <span className={cx(styles.meta, metaClass)}>{data || '-'}</span>}
     </div>
   );
 }
