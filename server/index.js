8049b73ad7c76396350cc0569ed430f7baa7f653
Hanse <hkri@koren.im>
2017-03-05 00:28:45 +0100

    Make hot reloading server work

---
+++ server/index.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-console */
+
+import http from 'http';
+import app from './server';
+
+const server = http.createServer(app);
+
+let currentApp = app;
+
+app.listen(app.get('port'), app.get('host'), (err) => {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log(`App is running on port ${app.get('port')}`);
+});
+
+if (module.hot) {
+  module.hot.accept('./server', () => {
+    server.removeListener('request', currentApp);
+    server.on('request', app);
+    currentApp = app;
+  });
+}
