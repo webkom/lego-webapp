95cffbe72c9c85348bab93bc10eddaba8be7597e
Finn Julius Stephansen-Smith <finnjs@stud.ntnu.no>
2017-04-27 22:48:16 +0200

    Initial commit for attempt at frontpage design changes

--- app/routes/overview/components/Overview.js
+++ app/routes/overview/components/Overview.js
@@ -9,9 +9,14 @@ import truncateString from 'app/utils/truncateString';
 import { Content, Flex } from 'app/components/Layout';
 import LatestReadme from './LatestReadme';
 import Feed from './Feed';
+import CompactEvents from './CompactEvents';
+import { EVENT_TYPE_TO_STRING } from 'app/routes/events/utils';
+import Button from 'app/components/Button';
 
+const TITLE_MAX_LENGTH = 50;
 const DESCRIPTION_MAX_LENGTH = 150;
-const IMAGE_HEIGHT = 180;
+const IMAGE_HEIGHT = 192;
+const IMAGE_WIDTH = 350;
 
 function PrimaryItem({ event }) {
   return (
@@ -32,9 +37,18 @@ function PrimaryItem({ event }) {
           </h2>
 
           <span className={styles.itemInfo}>
-            <Time time={event.startTime} format="DD.MM HH:mm" />
-            <span> · </span>
-            <span>{event.location}</span>
+            {event.startTime &&
+              <Time time={event.startTime} format="DD.MM HH:mm" />}
+            {event.location !== '-' &&
+              <span>
+                <span className={styles.dot}> · </span>
+                <span>{event.location}</span>
+              </span>}
+            {event.eventType &&
+              <span>
+                <span className={styles.dot}> · </span>
+                <span>{EVENT_TYPE_TO_STRING(event.eventType)}</span>
+              </span>}
           </span>
         </div>
       </Flex>
@@ -44,42 +58,66 @@ function PrimaryItem({ event }) {
 
 const OverviewItem = ({ event, showImage }) => (
   <Flex column className={styles.item}>
-    <Flex column className={styles.inner}>
-      {showImage &&
-        <Link
-          to={`/events/${event.id}`}
-          style={{ height: IMAGE_HEIGHT, display: 'block' }}
+    <Flex row className={styles.inner}>
+      <Flex column>
+        {showImage &&
+          <Link
+            to={`/events/${event.id}`}
+            style={{
+              width: IMAGE_WIDTH,
+              height: IMAGE_HEIGHT,
+              display: 'block'
+            }}
+          >
+            <Image className={styles.image} src={event.cover} />
+          </Link>}
+      </Flex>
+      <Flex column className={styles.innerRight}>
+        <div className={styles.heading}>
+          <h2 className={styles.itemTitle}>
+            <Link to={`/events/${event.id}`}>
+              {truncateString(event.title, TITLE_MAX_LENGTH)}
+            </Link>
+          </h2>
+
+          <span className={styles.itemInfo}>
+            {event.startTime &&
+              <Time time={event.startTime} format="DD.MM HH:mm" />}
+            {event.location !== '-' &&
+              <span>
+                <span className={styles.dot}> · </span>
+                <span>{event.location}</span>
+              </span>}
+            {event.eventType &&
+              <span>
+                <span className={styles.dot}> · </span>
+                <span>{EVENT_TYPE_TO_STRING(event.eventType)}</span>
+              </span>}
+          </span>
+        </div>
+
+        <p
+          className={styles.itemDescription}
+          style={{
+            borderTop: `3px solid ${colorForEvent(event.eventType)}`
+          }}
         >
-          <Image className={styles.image} src={event.cover} />
-        </Link>}
-
-      <div className={styles.heading}>
-        <h2 className={styles.itemTitle}>
-          <Link to={`/events/${event.id}`}>
-            {event.title}
-          </Link>
-        </h2>
-
-        <span className={styles.itemInfo}>
-          <Time time={event.startTime} format="DD.MM HH:mm" />
-          <span> · </span>
-          <span>{event.location}</span>
-        </span>
-      </div>
-
-      <p
-        className={styles.itemDescription}
-        style={{
-          borderTop: `3px solid ${colorForEvent(event.eventType)}`
-        }}
-      >
-        {truncateString(event.description, DESCRIPTION_MAX_LENGTH)}
-      </p>
+          {truncateString(event.description, DESCRIPTION_MAX_LENGTH)}
+        </p>
+      </Flex>
     </Flex>
   </Flex>
 );
 
 export default class Overview extends Component {
+  state = {
+    eventsToShow: 4
+  };
+
+  increaseEventsToShow = () => {
+    this.setState({ eventsToShow: this.state.eventsToShow * 2 });
+  };
+
   render() {
     const { events } = this.props;
 
@@ -90,19 +128,36 @@ export default class Overview extends Component {
     return (
       <Content>
         <Helmet title="Hjem" />
-        <Flex wrap>
-          <PrimaryItem event={events[0]} />
+        <Flex row style={{ justifyContent: 'space-between' }}>
+          <Flex column style={{ width: '65%' }}>
+            <CompactEvents events={events} />
+            <PrimaryItem event={events[0]} />
+          </Flex>
           <Feed />
         </Flex>
+        <Flex />
         <Flex padding={10}>
           <LatestReadme />
         </Flex>
         <Flex wrap>
           {events
-            .slice(1)
+            .slice(1, this.state.eventsToShow)
             .map(event => (
-              <OverviewItem key={event.id} event={event} showImage />
+              <OverviewItem
+                key={event.id}
+                event={event}
+                showImage
+                increaseEventsToShow={this.increaseEventsToShow}
+              />
             ))}
+          <Button
+            style={{ width: '100%', marginTop: '10px' }}
+            onClick={() =>
+              this.setState({ eventsToShow: this.state.eventsToShow ** 2 })}
+          >
+            Vis flere arrangementer&nbsp;
+            <i className="fa fa-angle-double-down " />
+          </Button>
         </Flex>
       </Content>
     );
