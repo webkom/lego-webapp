ebe9c2a6b2cf2cf507511ca2af4d4252bce69efb
Finn Julius Stephansen-Smith <finnjs@stud.ntnu.no>
2017-04-27 20:44:47 +0200

    Only attempt fetching quote if user is logged in

--- app/components/RandomQuote/index.js
+++ app/components/RandomQuote/index.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 import styles from './RandomQuote.css';
 import { fetchRandomQuote } from 'app/actions/QuoteActions';
 import { connect } from 'react-redux';
+import fetchOnUpdate from 'app/utils/fetchOnUpdate';
+import { compose } from 'redux';
 
 type Props = {
-  fetchRandomQuote: () => void
+  fetchRandomQuote: () => void,
+  loggedIn: boolean
 };
 
 class RandomQuote extends Component {
@@ -15,7 +18,11 @@ class RandomQuote extends Component {
   };
 
   componentDidMount() {
-    this.refreshQuote();
+    if (this.props.loggedIn) this.refreshQuote();
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (newProps.loggedIn) this.refreshQuote();
   }
 
   refreshQuote = () => {
@@ -33,15 +40,29 @@ class RandomQuote extends Component {
       <div>
         <h2 className={styles.heading}>
           <span>Tilfelding sitat</span>
-          <i onClick={this.refreshQuote} className="fa fa-refresh" />
+          {this.props.loggedIn &&
+            <i onClick={this.refreshQuote} className="fa fa-refresh" />}
         </h2>
-        <div className={styles.quoteText}>{currentQuote.text}</div>
-        <div className={styles.quoteSource}>-{currentQuote.source}</div>
+        {this.props.loggedIn
+          ? <div>
+              <div className={styles.quoteText}>{currentQuote.text}</div>
+              <div className={styles.quoteSource}>-{currentQuote.source}</div>
+            </div>
+          : 'Logg inn for å se sitater.'}
       </div>
     );
   }
 }
 
+function mapStateToProps(state, props) {
+  return {
+    loggedIn: state.auth.token !== null
+  };
+}
+
 const mapDispatchToProps = { fetchRandomQuote };
 
-export default connect(null, mapDispatchToProps)(RandomQuote);
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  fetchOnUpdate(['loggedIn'], () => null)
+)(RandomQuote);
