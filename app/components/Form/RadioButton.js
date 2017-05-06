8396a5d59aeabf0495d7fc68e3155286eea3752b
Hans-Wilhelm Warlo <hw@warlo.no>
2017-05-06 22:11:35 +0200

    Remove radiobutton style from bdb to component and fix optionsbox

--- app/components/Form/RadioButton.js
+++ app/components/Form/RadioButton.js
@@ -7,15 +7,26 @@ import styles from './RadioButton.css';
 type Props = {
   type?: string,
   label?: string,
-  className?: string
+  className?: string,
+  inputValue?: string,
+  value?: string,
+  checked?: boolean
 };
 
-function RadioButton({ id, label, inputValue, className, ...props }: Props) {
+function RadioButton({
+  id,
+  label,
+  inputValue,
+  value,
+  checked,
+  className,
+  ...props
+}: Props) {
   return (
     <div className={styles.box}>
       <input
         {...props}
-        checked={inputValue === props.value}
+        checked={inputValue === value || checked}
         type="radio"
         id={id}
         value={inputValue}
