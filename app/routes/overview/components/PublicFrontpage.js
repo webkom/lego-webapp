062f96c58d6424f4de7b36fc3e5b72e14e32dcba
Finn Julius Stephansen-Smith <finnjs@stud.ntnu.no>
2017-05-06 16:17:28 +0200

    Implement non-scale css and add bottomContent with useful links and fb feed

--- app/routes/overview/components/PublicFrontpage.js
+++ app/routes/overview/components/PublicFrontpage.js
@@ -5,8 +5,20 @@ import styles from './PublicFrontpage.css';
 
 function PublicFrontpage() {
   return (
-    <Content style={{ paddingBottom: '20px' }}>
+    <Content>
       <Flex justifyContent="space-between" className={styles.root}>
+        <div className={styles.smallWelcomeBox}>
+          <Flex
+            component="h2"
+            justifyContent="space-between"
+            alignItems="center"
+            className="u-mb"
+            style={{ whiteSpace: 'nowrap' }}
+          >
+            Logg inn <a href="" className="u-small">Jeg er ny →</a>
+          </Flex>
+          <LoginForm />
+        </div>
         <div className={styles.bigWelcomeBox}>
           <h2 className="u-mb">Velkommen til Abakus</h2>
           <p>
@@ -21,21 +33,51 @@ function PublicFrontpage() {
             fremme kontakten med næringslivet og bidra med sosiale aktiviteter.
           </p>
         </div>
-        <div className={styles.smallWelcomeBox}>
-          <Flex
-            component="h2"
-            justifyContent="space-between"
-            alignItems="center"
-            className="u-mb"
-            style={{ whiteSpace: 'nowrap' }}
-          >
-            Logg inn{' '}
-            <a href="" className="u-small">
-              Jeg er ny →
-            </a>
-          </Flex>
-          <LoginForm />
+      </Flex>
+
+      <Flex className={styles.bottomContainer}>
+        <div className={styles.usefulLinks}>
+          <h2 className="u-mb">Nyttige linker</h2>
+          <ul>
+            <li>
+              <a href="https://www.ntnu.no/innsida/">
+                <i className="fa fa-caret-right" /> innsida
+              </a>
+              <div className={styles.linkDescription}>
+                Innsida er kjappeste vei inn til andre system og tjenester. Logger man seg på Innsida, blir man samtidig pålogget andre system.
+              </div>
+            </li>
+            <li>
+              <a href="https://www.ntnu.no/studier/mtdt">
+                <i className="fa fa-caret-right" /> Om Datateknikk
+              </a>
+              <div className={styles.linkDescription}>
+                Datateknologi er en helt sentral del av alle fremtidsrettede teknologier, som for eksempel kunstig intelligens, medisinsk teknologi og søkemotorteknologi.
+              </div>
+            </li>
+            <li>
+              <a href="http://www.ntnu.no/studier/mtkom">
+                <i className="fa fa-caret-right" /> Om Kommunikasjonsteknologi
+              </a>
+              <div className={styles.linkDescription}>
+                Vi bruker stadig mer av livene våre på nett, på jobb som i fritid. Kommunikasjonsteknologi brukes etter hvert av alle og overalt.
+              </div>
+            </li>
+          </ul>
         </div>
+
+        <iframe
+          src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FAbakusNTNU%2F&amp;tabs=timeline&amp;width=420&amp;small_header=true&amp;adapt_container_width=true&amp;hide_cover=false&amp;show_facepile=true&amp;appId=1717809791769695"
+          style={{
+            border: 'none',
+            overflow: 'hidden',
+            height: '500px',
+            width: '420px'
+          }}
+          scrolling="no"
+          frameBorder="0"
+          allowTransparency="true"
+        />
       </Flex>
 
     </Content>
