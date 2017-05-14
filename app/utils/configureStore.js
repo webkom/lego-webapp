f48882f83abd0a05f3a6dc395d6b53eff94a1bac
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2017-05-14 12:55:56 +0200

    Report errors to sentry
    We should also fix the source-maps and post them to sentry.

--- app/utils/configureStore.js
+++ app/utils/configureStore.js
@@ -5,10 +5,17 @@ import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import { browserHistory } from 'react-router';
 import { routerMiddleware } from 'react-router-redux';
+import Raven from 'raven-js';
+import createRavenMiddleware from 'raven-for-redux';
 import { addNotification } from 'app/actions/NotificationActions';
 import promiseMiddleware from './promiseMiddleware';
 import createErrorMiddleware from './errorMiddleware';
 import type { State, Store } from 'app/types';
+import config from 'app/config';
+
+Raven.config(config.ravenDSN, {
+  release: config.RELEASE
+}).install();
 
 const loggerMiddleware = createLogger({
   level: 'info',
@@ -24,6 +31,7 @@ export default function configureStore(initialState: State): Store {
     routerMiddleware(browserHistory),
     thunkMiddleware,
     promiseMiddleware,
+    createRavenMiddleware(Raven),
     errorMiddleware
   ];
 
--- config/webpack.client.js
+++ config/webpack.client.js
@@ -52,7 +52,10 @@ module.exports = {
         'process.env.API_URL': JSON.stringify(process.env.API_URL),
         'process.env.WS_URL': JSON.stringify(process.env.WS_URL),
         'process.env.BASE_URL': JSON.stringify(process.env.BASE_URL),
-        'process.env.CAPTCHA_KEY': JSON.stringify(process.env.CAPTCHA_KEY)
+        'process.env.CAPTCHA_KEY': JSON.stringify(process.env.CAPTCHA_KEY),
+        'process.env.STRIPE_KEY': JSON.stringify(process.env.STRIPE_KEY),
+        'process.env.RAVEN_DSN': JSON.stringify(process.env.RAVEN_DSN),
+        'process.env.RELEASE': JSON.stringify(process.env.RELEASE),
       }),
 
       !isProduction && new FriendlyErrorsPlugin(),
