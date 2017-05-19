2f3c9c5db09c5247fc6bc45ff7e5e5b115879098
Orhan Hirsch <orhanhenrik@gmail.com>
2017-05-19 14:39:16 +0200

    fix flow types

--- app/routes/pages/components/PageList.js
+++ app/routes/pages/components/PageList.js
@@ -1,13 +1,10 @@
 // @flow
 
 import React from 'react';
-import classNames from 'classnames';
 import { Link } from 'react-router';
-import styles from './PageHierarchy.css';
-import Icon from 'app/components/Icon';
 
 type Props = {
-  pages: Object[]
+  pages: Object
 };
 
 const PageList = ({ pages }: Props) => {
