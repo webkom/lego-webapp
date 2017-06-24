751c127de9992a118c86ea314bce77e11f4f8d0e
Martin Farstad <martinrf@stud.ntnu.no>
2017-06-24 09:58:08 +0200

    add table for representing interested companies

--- app/routes/companyInterest/components/CompanyInterestList.js
+++ app/routes/companyInterest/components/CompanyInterestList.js
@@ -1,6 +1,5 @@
 import styles from './CompanyInterest.css';
 import React from 'react';
-import CompanyInterest from './CompanyInterest';
 import Button from 'app/components/Button';
 import { Link } from 'react-router';
 
@@ -8,10 +7,78 @@ export type Props = {
   CompanyInterestList: Array
 };
 
+const eventTypes = group => {
+  return [
+    {
+      value: group.name,
+      label: 'Bedriftsnavn'
+    },
+    {
+      value: group.contactPerson,
+      label: 'Kontaktperson'
+    },
+    {
+      value: group.mail,
+      label: 'Mail'
+    },
+    {
+      value: group.comment,
+      label: 'Kommentar'
+    },
+    {
+      value: group.companyPresentation,
+      label: 'Bedriftspresentasjon'
+    },
+    {
+      value: group.course,
+      label: 'Kurs'
+    },
+    {
+      value: group.lunchPresentation,
+      label: 'Lunsjpresentasjon'
+    },
+    {
+      value: group.readme,
+      label: 'Annonsere i readme'
+    },
+    {
+      value: group.collaboration,
+      label: 'Samarbeid med andre linjeforeninger'
+    },
+    {
+      value: group.itdagene,
+      label: 'Ønsker stand på itDAGENE'
+    }
+  ];
+};
+
 const CompanyInterestList = (props: Props) => {
-  const groups = props.CompanyInterestList.map((group, key) =>
-    <CompanyInterest group={group} key={key} />
+  const generateLabels = group => {
+    return eventTypes(group).map((event, key) => {
+      return <td className={styles.tableColumn}>{event.label}</td>;
+    });
+  };
+
+  const generateValues = group => {
+    return eventTypes(group).map((event, key) => {
+      let value;
+      switch (typeof event.value) {
+        case 'boolean':
+          value = event.value ? 'Ja' : '';
+          break;
+        case 'string':
+          value = event.value;
+          break;
+        default:
+      }
+      return <td className={styles.tableColumn}>{value}</td>;
+    });
+  };
+
+  const interests = props.CompanyInterestList.map((group, id) =>
+    <tr>{generateValues(group)}</tr>
   );
+
   return (
     <div className={styles.root}>
       <div className={styles.section}>
@@ -22,6 +89,7 @@ const CompanyInterestList = (props: Props) => {
             {' '}
             finner du all praktisk informasjon knyttet til
             bedriftsinteresser.
+            {console.log()}
           </p>
         </div>
         <Link
@@ -31,9 +99,12 @@ const CompanyInterestList = (props: Props) => {
           <Button>Opprett ny bedriftsinteresse</Button>
         </Link>
       </div>
-      <div className="groups">
-        {groups}
-      </div>
+      <table className="groups">
+        <tr className={styles.tableRow}>
+          {generateLabels(props.CompanyInterestList[0])}
+        </tr>
+        {interests}
+      </table>
     </div>
   );
 };
