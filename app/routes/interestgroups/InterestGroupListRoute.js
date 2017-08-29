fd0df103dc97fcd635cdab8cc9bfcc332406cbe0
Hans-Wilhelm Warlo <hw@warlo.no>
2017-08-29 11:09:05 +0200

    Add dispatched to interestgroup routes

--- app/routes/interestgroups/InterestGroupListRoute.js
+++ app/routes/interestgroups/InterestGroupListRoute.js
@@ -2,28 +2,23 @@
 
 import { compose } from 'redux';
 import { connect } from 'react-redux';
+import { dispatched } from 'react-prepare';
 import {
   fetchAll,
   createInterestGroup
 } from 'app/actions/InterestGroupActions';
 import InterestGroupList from './components/InterestGroupList';
-import fetchOnUpdate from 'app/utils/fetchOnUpdate';
 import { selectInterestGroups } from 'app/reducers/interestGroups';
 
-function loadData(params, props) {
-  props.fetchAll();
-}
-
-function mapStateToProps(state) {
-  const interestGroups = selectInterestGroups(state);
-  return {
-    interestGroups
-  };
-}
+const mapStateToProps = state => ({
+  interestGroups: selectInterestGroups(state)
+});
 
 const mapDispatchToProps = { fetchAll, createInterestGroup };
 
 export default compose(
-  connect(mapStateToProps, mapDispatchToProps),
-  fetchOnUpdate(['loggedIn'], loadData)
+  dispatched((props, dispatch) => dispatch(fetchAll()), {
+    componentWillReceiveProps: false
+  }),
+  connect(mapStateToProps, mapDispatchToProps)
 )(InterestGroupList);
