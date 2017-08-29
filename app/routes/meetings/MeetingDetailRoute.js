8e26282adb25fb46ff65a28668867406cfcea293
Hans-Wilhelm Warlo <hw@warlo.no>
2017-08-29 15:37:12 +0200

    Add meetingsToken reducer and adapt MeetingDetail for SSR with answerMeeting with token

--- app/routes/meetings/MeetingDetailRoute.js
+++ app/routes/meetings/MeetingDetailRoute.js
@@ -1,35 +1,63 @@
-// import { compose } from 'redux';
+import React from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
-import fetchOnUpdate from 'app/utils/fetchOnUpdate';
+import { dispatched } from 'react-prepare';
 import {
   fetchMeeting,
   setInvitationStatus,
-  deleteMeeting
+  deleteMeeting,
+  answerMeetingInvitation
 } from 'app/actions/MeetingActions';
 import { selectMeetingById } from 'app/reducers/meetings';
 import MeetingDetail from './components/MeetingDetail';
+import MeetingAnswer from './components/MeetingAnswer';
 
-function loadData({ meetingId }, props) {
-  props.fetchMeeting(meetingId);
-}
-
-function mapStateToProps(state, props) {
+const loadData = (props, dispatch) => {
   const { meetingId } = props.params;
+  const { action, token } = props.location.query;
+  const loggedIn = props.loggedIn;
+  if (!loggedIn && token) {
+    return dispatch(answerMeetingInvitation(action, token, loggedIn));
+  }
+  if (action && token) {
+    return dispatch(answerMeetingInvitation(action, token, loggedIn)).then(() =>
+      dispatch(fetchMeeting(meetingId))
+    );
+  }
+  return dispatch(fetchMeeting(meetingId));
+};
+
+const mapStateToProps = (state, props) => {
+  const { meetingId } = props.params;
+  const { action, token } = props.location.query;
+  const showAnswer = Boolean(action && token);
   const meeting = selectMeetingById(state, { meetingId });
+  const meetingsToken = state.meetingsToken;
   const userMe = state.auth.username
     ? state.users.byId[state.auth.username]
     : {};
   return {
     meeting,
+    meetingsToken,
     meetingId,
-    userMe
+    userMe,
+    showAnswer
   };
-}
+};
+
+const MeetingComponent = props => {
+  const { loggedIn, meetingsToken } = props;
+  if (!loggedIn) {
+    return <MeetingAnswer {...meetingsToken} />;
+  }
+  return <MeetingDetail {...props} />;
+};
 
 const mapDispatchToProps = { fetchMeeting, setInvitationStatus, deleteMeeting };
 
 export default compose(
-  connect(mapStateToProps, mapDispatchToProps),
-  fetchOnUpdate(['meetingId', 'loggedIn'], loadData)
-)(MeetingDetail);
+  dispatched(loadData, {
+    componentWillReceiveProps: false
+  }),
+  connect(mapStateToProps, mapDispatchToProps)
+)(MeetingComponent);
