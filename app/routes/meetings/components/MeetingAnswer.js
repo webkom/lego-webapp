67c8e7c66b626fa6298029c6c854470d524c5531
Hans-Wilhelm Warlo <hw@warlo.no>
2017-08-29 15:31:19 +0200

    Change MeetingAnswerResultRoute to a component

---
+++ app/routes/meetings/components/MeetingAnswer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import LoadingIndicator from 'app/components/LoadingIndicator';
+
+export const MeetingAnswer = ({ response, user, meeting, status }) => {
+  if (!response) {
+    return <LoadingIndicator loading />;
+  }
+
+  if (response === 'good') {
+    const statusText = ['', 'Delta', 'Ikke delta'][status];
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <h1> Du har nå svart på invitasjonen 😃 </h1>
+        <p>
+          {' '}{user.firstName} skal nå {statusText} på møtet!
+        </p>
+        <p>
+          Logg inn og sjekk møtet <a href={`/meetings/${meeting}/`}> her</a>
+        </p>
+      </div>
+    );
+  }
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1> Det har skjedd en feil :( </h1>
+      <p> Prøv å logg inn for å svare på invitasjonen </p>
+    </div>
+  );
+};
+
+export default MeetingAnswer;
