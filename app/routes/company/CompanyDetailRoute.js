09f3e3ac51143c858368411b10879f369efc43ff
Hans-Wilhelm Warlo <hw@warlo.no>
2017-08-29 10:52:30 +0200

    Add dispatched to company routes

--- app/routes/company/CompanyDetailRoute.js
+++ app/routes/company/CompanyDetailRoute.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
+import { dispatched } from 'react-prepare';
 import { fetch as fetchCompany } from 'app/actions/CompanyActions';
-import fetchOnUpdate from 'app/utils/fetchOnUpdate';
 import CompanyDetail from './components/CompanyDetail';
-
-function loadData({ companyId }, props) {
-  props.fetchCompany(Number(companyId));
-}
 
 type Props = {
   fetchCompany: () => {}
@@ -17,7 +13,7 @@ const CompanyDetailRoute = (props: Props) => {
   return <CompanyDetail {...props} />;
 };
 
-function mapStateToProps(state, props) {
+const mapStateToProps = (state, props) => {
   const companyId = props.params.companyId;
   const { query } = props.location;
   const company = state.companies.byId[companyId];
@@ -27,11 +23,16 @@ function mapStateToProps(state, props) {
     companyId,
     loggedIn: state.auth.token !== null
   };
-}
+};
 
 const mapDispatchToProps = { fetchCompany };
 
 export default compose(
-  connect(mapStateToProps, mapDispatchToProps),
-  fetchOnUpdate(['companyId', 'loggedIn'], loadData)
+  dispatched(
+    ({ params: { companyId } }, dispatch) => dispatch(fetchCompany(companyId)),
+    {
+      componentWillReceiveProps: false
+    }
+  ),
+  connect(mapStateToProps, mapDispatchToProps)
 )(CompanyDetailRoute);
