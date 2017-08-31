4f59fb2334313041a2187241402053a22bd9e06c
Hans-Wilhelm Warlo <hw@warlo.no>
2017-08-31 16:12:11 +0200

    Fix calendar route correctly fetching

--- app/routes/bdb/BdbRoute.js
+++ app/routes/bdb/BdbRoute.js
@@ -9,13 +9,10 @@ import BdbPage from './components/BdbPage';
 import { compose } from 'redux';
 import { selectCompanies } from 'app/reducers/companies';
 
-const mapStateToProps = (state, props) => {
-  console.log('companies', selectCompanies(state, props));
-  return {
-    companies: selectCompanies(state, props),
-    query: props.location
-  };
-};
+const mapStateToProps = (state, props) => ({
+  companies: selectCompanies(state, props),
+  query: props.location
+});
 
 const mapDispatchToProps = { fetchAll, editSemesterStatus, addSemesterStatus };
 
