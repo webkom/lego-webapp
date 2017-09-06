cfd732a21015cf56ea76a313901027d899d323ce
Hans-Wilhelm Warlo <hw@warlo.no>
2017-09-06 13:48:45 +0200

    Add registration component to frontpage

--- app/routes/overview/components/PublicFrontpage.js
+++ app/routes/overview/components/PublicFrontpage.js
@@ -1,108 +1,126 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Content, Flex } from 'app/components/Layout';
-import LoginForm from 'app/components/LoginForm';
+import { LoginForm, RegisterForm } from 'app/components/LoginForm';
 import styles from './PublicFrontpage.css';
 import { Link } from 'react-router';
 
-function PublicFrontpage() {
-  return (
-    <Content>
-      <Flex justifyContent="space-between" className={styles.root}>
-        <div className={styles.smallWelcomeBox}>
-          <Flex
-            component="h2"
-            justifyContent="space-between"
-            alignItems="center"
-            className="u-mb"
-            style={{ whiteSpace: 'nowrap' }}
-          >
-            Logg inn{' '}
-            <a href="" className="u-small">
-              Jeg er ny →
-            </a>
-          </Flex>
-          <LoginForm />
-        </div>
-        <div className={styles.bigWelcomeBox}>
-          <h2 className="u-mb">Velkommen til Abakus</h2>
-          <p>
-            Abakus er linjeforeningen for studentene ved Datateknologi og
-            Kommunikasjonsteknologi på NTNU, og drives av studenter ved disse
-            studiene.
-          </p>
-          <p>
-            Abakus
-            {"'"} formål er å gi disse studentene veiledning i
-            studiesituasjonen, arrangere kurs som utfyller fagtilbudet ved NTNU,
-            fremme kontakten med næringslivet og bidra med sosiale aktiviteter.
-          </p>
-        </div>
-      </Flex>
+class PublicFrontpage extends Component {
+  state = {
+    registerUser: false
+  };
 
-      <Flex className={styles.bottomContainer}>
-        <div className={styles.usefulLinks}>
-          <h2 className="u-mb">Nyttige linker</h2>
-          <ul>
-            <li>
-              <a href="https://www.ntnu.no/innsida/">
-                <i className="fa fa-caret-right" /> innsida
-              </a>
-              <div className={styles.linkDescription}>
-                Innsida er kjappeste vei inn til andre system og tjenester.
-                Logger man seg på Innsida, blir man samtidig pålogget andre
-                system.
-              </div>
-            </li>
-            <li>
-              <a href="https://www.ntnu.no/studier/mtdt">
-                <i className="fa fa-caret-right" /> Om Datateknologi
-              </a>
-              <div className={styles.linkDescription}>
-                Datateknologi er en helt sentral del av alle fremtidsrettede
-                teknologier, som for eksempel kunstig intelligens, medisinsk
-                teknologi og søkemotorteknologi.
-              </div>
-            </li>
-            <li>
-              <a href="http://www.ntnu.no/studier/mtkom">
-                <i className="fa fa-caret-right" /> Om Kommunikasjonsteknologi
-              </a>
-              <div className={styles.linkDescription}>
-                Vi bruker stadig mer av livene våre på nett, på jobb som i
-                fritid. Kommunikasjonsteknologi brukes etter hvert av alle og
-                overalt.
-              </div>
-            </li>
-            <li>
-              <Link to="/pages/for-companies">
-                <i className="fa fa-caret-right" /> For bedrifter
-              </Link>
-              <div className={styles.linkDescription}>
-                Her finner du som bedriftsrepresentant informasjon om Abakus
-                {"'"}
-                {} prosedyrer for bedriftspresentasjoner og andre nyttige fakta.
-              </div>
-            </li>
-          </ul>
-        </div>
+  toggleRegisterUser = () => {
+    this.setState(state => ({
+      registerUser: !state.registerUser
+    }));
+  };
 
-        <div className={styles.facebook}>
-          <iframe
-            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FAbakusNTNU%2F&amp;tabs=timeline&amp;width=420&amp;small_header=true&amp;adapt_container_width=true&amp;hide_cover=false&amp;show_facepile=true&amp;appId=1717809791769695"
-            style={{
-              border: 'none',
-              overflow: 'hidden',
-              height: '500px',
-              width: '420px'
-            }}
-            scrolling="no"
-            frameBorder="0"
-            allowTransparency="true"
-          />
-        </div>
-      </Flex>
-    </Content>
-  );
+  render() {
+    const { registerUser } = this.state;
+    return (
+      <Content>
+        <Flex justifyContent="space-between" className={styles.root}>
+          <div className={styles.smallWelcomeBox}>
+            <Flex
+              component="h2"
+              justifyContent="space-between"
+              alignItems="center"
+              className="u-mb"
+              style={{ whiteSpace: 'nowrap' }}
+            >
+              {registerUser ? 'Register bruker' : 'Logg inn'}
+              <button
+                onClick={this.toggleRegisterUser}
+                className={styles.toggleButton}
+              >
+                {registerUser ? '← Jeg har bruker' : 'Jeg er ny →'}
+              </button>
+            </Flex>
+            {registerUser ? <RegisterForm /> : <LoginForm />}
+          </div>
+          <div className={styles.bigWelcomeBox}>
+            <h2 className="u-mb">Velkommen til Abakus</h2>
+            <p>
+              Abakus er linjeforeningen for studentene ved Datateknologi og
+              Kommunikasjonsteknologi på NTNU, og drives av studenter ved disse
+              studiene.
+            </p>
+            <p>
+              Abakus
+              {"'"} formål er å gi disse studentene veiledning i
+              studiesituasjonen, arrangere kurs som utfyller fagtilbudet ved
+              NTNU, fremme kontakten med næringslivet og bidra med sosiale
+              aktiviteter.
+            </p>
+          </div>
+        </Flex>
+
+        <Flex className={styles.bottomContainer}>
+          <div className={styles.usefulLinks}>
+            <h2 className="u-mb">Nyttige linker</h2>
+            <ul>
+              <li>
+                <a href="https://www.ntnu.no/innsida/">
+                  <i className="fa fa-caret-right" /> innsida
+                </a>
+                <div className={styles.linkDescription}>
+                  Innsida er kjappeste vei inn til andre system og tjenester.
+                  Logger man seg på Innsida, blir man samtidig pålogget andre
+                  system.
+                </div>
+              </li>
+              <li>
+                <a href="https://www.ntnu.no/studier/mtdt">
+                  <i className="fa fa-caret-right" /> Om Datateknologi
+                </a>
+                <div className={styles.linkDescription}>
+                  Datateknologi er en helt sentral del av alle fremtidsrettede
+                  teknologier, som for eksempel kunstig intelligens, medisinsk
+                  teknologi og søkemotorteknologi.
+                </div>
+              </li>
+              <li>
+                <a href="http://www.ntnu.no/studier/mtkom">
+                  <i className="fa fa-caret-right" /> Om Kommunikasjonsteknologi
+                </a>
+                <div className={styles.linkDescription}>
+                  Vi bruker stadig mer av livene våre på nett, på jobb som i
+                  fritid. Kommunikasjonsteknologi brukes etter hvert av alle og
+                  overalt.
+                </div>
+              </li>
+              <li>
+                <Link to="/pages/for-companies">
+                  <i className="fa fa-caret-right" /> For bedrifter
+                </Link>
+                <div className={styles.linkDescription}>
+                  Her finner du som bedriftsrepresentant informasjon om Abakus
+                  {"'"}
+                  {} prosedyrer for bedriftspresentasjoner og andre nyttige
+                  fakta.
+                </div>
+              </li>
+            </ul>
+          </div>
+
+          <div className={styles.facebook}>
+            <iframe
+              src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FAbakusNTNU%2F&amp;tabs=timeline&amp;width=420&amp;small_header=true&amp;adapt_container_width=true&amp;hide_cover=false&amp;show_facepile=true&amp;appId=1717809791769695"
+              style={{
+                border: 'none',
+                overflow: 'hidden',
+                height: '500px',
+                width: '420px'
+              }}
+              scrolling="no"
+              frameBorder="0"
+              allowTransparency="true"
+            />
+          </div>
+        </Flex>
+      </Content>
+    );
+  }
 }
 
 export default PublicFrontpage;
