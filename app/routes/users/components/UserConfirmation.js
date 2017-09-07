2cf2f3e3b50f5ba662d8f02130fed2f19f0a7288
Hans-Wilhelm Warlo <hw@warlo.no>
2017-09-07 21:03:14 +0200

    Fix password regex

--- app/routes/users/components/UserConfirmation.js
+++ app/routes/users/components/UserConfirmation.js
@@ -98,7 +98,7 @@ const UserConfirmation = ({
     </Content>
   );
 };
-const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,72}$/;
 const validate = data => {
   const errors = {};
 
