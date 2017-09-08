18d7c1f67b6617c9f724f46d7ac8c3d443423068
Lise Hove <lisehove@hotmail.com>
2017-09-08 14:41:28 +0200

    Route to /announcements, fetch from backend

--- app/routes/announcements/AnnouncementsRoute.js
+++ app/routes/announcements/AnnouncementsRoute.js
@@ -8,13 +8,20 @@ import { fetchAll } from 'app/actions/AnnouncementsActions';
 
 const mapStateToProps = (state, props) => {
   const announcements = state.announcements;
+<<<<<<< HEAD
+=======
+  console.log('hei');
+>>>>>>> b319fe7... Route to /announcements, fetch from backend
   return {
     announcements
   };
 };
 
 const mapDispatchToProps = { fetchAll };
+<<<<<<< HEAD
 
+=======
+>>>>>>> b319fe7... Route to /announcements, fetch from backend
 export default compose(
   dispatched((props, dispatch) => dispatch(fetchAll()), {
     componentWillReceiveProps: false
