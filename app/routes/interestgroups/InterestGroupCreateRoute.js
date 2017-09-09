b9ec2873b88adbad81db7644319463ef98bbd0c8
Martin Hafskjold Thoresen <martinhath@gmail.com>
2017-09-09 17:54:06 +0200

    Implement interest group creation

--- app/routes/interestgroups/InterestGroupCreateRoute.js
+++ app/routes/interestgroups/InterestGroupCreateRoute.js
@@ -1,17 +1,31 @@
+// @flow
 import { compose } from 'redux';
 import { connect } from 'react-redux';
-import { createInterestGroup } from 'app/actions/InterestGroupActions';
+import { formValueSelector } from 'redux-form';
+import {
+  createInterestGroup,
+  joinInterestGroup
+} from 'app/actions/InterestGroupActions';
 import InterestGroupCreate from './components/InterestGroupCreate';
 import { LoginPage } from 'app/components/LoginForm';
 import replaceUnlessLoggedIn from 'app/utils/replaceUnlessLoggedIn';
+import { uploadFile } from 'app/actions/FileActions';
 
-const mapDispatchToProps = { createInterestGroup };
+const mapDispatchToProps = {
+  createInterestGroup,
+  joinInterestGroup,
+  uploadFile
+};
 
-const mapStateToProps = (state, props) => ({
-  initialValues: {
-    descriptionLong: '<p></p>'
-  }
-});
+const mapStateToProps = (state, props) => {
+  const valueSelector = formValueSelector('interestGroupCreate');
+  return {
+    initialValues: {
+      descriptionLong: '<p></p>'
+    },
+    invitedMembers: valueSelector(state, 'members') || []
+  };
+};
 
 export default compose(
   replaceUnlessLoggedIn(LoginPage),
