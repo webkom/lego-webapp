27dc538757f97c849039bea7ccc58caf1827a212
Hans-Wilhelm Warlo <hw@warlo.no>
2017-09-10 15:13:32 +0200

    Avoid retries on ssr and let only client retry >500 errors. Fix client intially retrying <500 as well

--- app/reducers/fetchHistory.js
+++ app/reducers/fetchHistory.js
@@ -1,10 +1,24 @@
+import { FetchHistory } from 'app/actions/ActionTypes';
+
 export default function fetchHistory(state: State = {}, action: Action) {
   const success = action.meta && action.meta.success;
-  if (success && action.type === success) {
-    return {
-      ...state,
-      [action.meta.endpoint]: Date.now()
-    };
+  switch (action.type) {
+    case success: {
+      if (!success) {
+        return state;
+      }
+      return {
+        ...state,
+        [action.meta.endpoint]: Date.now()
+      };
+    }
+    case FetchHistory.SET_HISTORY: {
+      return {
+        ...state,
+        [action.payload]: Date.now()
+      };
+    }
+    default:
+      return state;
   }
-  return state;
 }
