3a810c1245d50151db21e61da878194cace14349
Odin Ugedal <odin@ugedal.com>
2017-09-13 17:34:11 +0200

    Fix autologin when not using SSR (#504)

--- app/index.js
+++ app/index.js
@@ -5,8 +5,10 @@ import 'babel-polyfill';
 import moment from 'moment';
 import { browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
+import { isEmpty } from 'lodash';
 import configureStore from 'app/utils/configureStore';
 import renderApp from './render';
+import { loginAutomaticallyIfPossible } from 'app/actions/UserActions';
 
 moment.locale('nb-NO');
 
@@ -19,6 +21,11 @@ const preloadedState = window.__PRELOADED_STATE__;
 delete window.__PRELOADED_STATE__;
 
 const store = configureStore(preloadedState);
+
+if (isEmpty(preloadedState)) {
+  store.dispatch(loginAutomaticallyIfPossible());
+}
+
 const history = syncHistoryWithStore(browserHistory, store);
 
 store.dispatch({ type: 'REHYDRATED' });
