cd253471465c81117a9befbbd6d30c054c012225
Martin Hafskjold Thoresen <martinhath@gmail.com>
2017-09-23 22:47:45 +0200

    Add component for interest group create

---
+++ app/routes/interestgroups/components/InterestGroupCreate.js
@@ -0,0 +1,39 @@
+// @flow
+import React, { Component } from 'react';
+import GroupForm from 'app/components/GroupForm';
+import { Flex, Content } from 'app/components/Layout';
+import { Link } from 'react-router';
+
+export default class InterestGroupEdit extends Component {
+  props: {
+    initialValues: Object,
+    removeInterestGroup: number => Promise<*>,
+    uploadFile: string => Promise<*>,
+    handleSubmitCallback: Object => Promise<*>
+  };
+
+  render() {
+    const { initialValues, uploadFile, handleSubmitCallback } = this.props;
+
+    return (
+      <Content>
+        <h2>
+          <Link to={`/interestGroups/`}>
+            <i className="fa fa-angle-left" />
+            Tilbake
+          </Link>
+        </h2>
+        <Flex justifyContent="space-between" alignItems="baseline">
+          <div>
+            <h1>Opprett gruppe</h1>
+          </div>
+        </Flex>
+        <GroupForm
+          handleSubmitCallback={handleSubmitCallback}
+          uploadFile={uploadFile}
+          initialValues={initialValues}
+        />
+      </Content>
+    );
+  }
+}
