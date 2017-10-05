4452348767250c1f27bb9a0a155297dcb7f6cb53
Odin Ugedal <odin@ugedal.com>
2017-10-05 20:53:29 +0200

    Add normalizer schema for meetingInvitations

--- app/routes/meetings/MeetingEditRoute.js
+++ app/routes/meetings/MeetingEditRoute.js
@@ -6,6 +6,8 @@ import MeetingEditor from './components/MeetingEditor';
 import { editMeeting, fetchMeeting } from 'app/actions/MeetingActions';
 import { formValueSelector } from 'redux-form';
 import { selectMeetingById } from 'app/reducers/meetings';
+import { selectUserById } from 'app/reducers/users';
+import { selectMeetingInvitationsForMeeting } from 'app/reducers/meetingInvitations';
 import { LoginPage } from 'app/components/LoginForm';
 import replaceUnlessLoggedIn from 'app/utils/replaceUnlessLoggedIn';
 
@@ -15,12 +17,24 @@ const mapStateToProps = (state, props) => {
   const { meetingId } = props.params;
   const meeting = selectMeetingById(state, { meetingId });
   const valueSelector = formValueSelector('meetingEditor');
+  const meetingInvitations =
+    meeting &&
+    selectMeetingInvitationsForMeeting(state, {
+      meetingId
+    });
+  const reportAuthor =
+    meeting && selectUserById(state, { userId: meeting.reportAuthor });
   return {
     user: props.currentUser,
     meeting,
-    initialValues: meeting,
+    initialValues: {
+      ...meeting,
+      reportAuthor: reportAuthor && reportAuthor.id,
+      report: meeting ? meeting.report : '<p></p>'
+    },
     invitingUsers: valueSelector(state, 'users') || [],
-    meetingId
+    meetingId,
+    meetingInvitations
   };
 };
 
