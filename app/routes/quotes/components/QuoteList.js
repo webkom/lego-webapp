54f24b924411f21ea700e251aabb2da010621182
Finn Julius Stephansen-Smith <finnjs@stud.ntnu.no>
2017-10-09 15:57:38 +0200

    Use NavigationTab and flow in quotes

--- app/routes/quotes/components/QuoteList.js
+++ app/routes/quotes/components/QuoteList.js
@@ -1,8 +1,14 @@
+// @flow
+
 import React, { Component } from 'react';
 import Quote from './Quote';
 
 type Props = {
-  quotes: Array<Object>
+  quotes: Array<Object>,
+  approve: number => void,
+  deleteQuote: number => void,
+  unapprove: number => void,
+  actionGrant: Array<string>
 };
 
 export default class QuoteList extends Component {
@@ -12,23 +18,26 @@ export default class QuoteList extends Component {
     displayAdminId: -1
   };
 
-  componentWillReceiveProps(newProps) {
+  componentWillReceiveProps(newProps: Object) {
     this.setState({ displayAdminId: -1 });
   }
 
-  setDisplayAdmin = id => {
+  setDisplayAdmin = (id: number) => {
     this.setState(state => ({
       displayAdminId: state.displayAdminId === id ? -1 : id
     }));
   };
 
   render() {
-    const { quotes } = this.props;
+    const { quotes, actionGrant, approve, unapprove, deleteQuote } = this.props;
     return (
       <ul>
         {quotes.map(quote => (
           <Quote
-            {...this.props}
+            actionGrant={actionGrant}
+            approve={approve}
+            unapprove={unapprove}
+            deleteQuote={deleteQuote}
             quote={quote}
             key={quote.id}
             setDisplayAdmin={this.setDisplayAdmin}
