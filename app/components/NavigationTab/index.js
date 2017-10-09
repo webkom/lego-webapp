279f4c31169df4fb0fb494fb17092a251d898b16
Finn Julius Stephansen-Smith <finnjs@stud.ntnu.no>
2017-10-09 18:39:37 +0200

    Meeting detail style update

--- app/components/NavigationTab/index.js
+++ app/components/NavigationTab/index.js
@@ -4,15 +4,17 @@ import React from 'react';
 
 import NavigationLink from './NavigationLink';
 import styles from './NavigationTab.css';
+import cx from 'classnames';
 
 type Props = {
   title: string,
+  className?: string,
   children: React$Element<*>
 };
 
 const NavigationTab = (props: Props) => {
   return (
-    <div className={styles.container}>
+    <div className={cx(styles.container, props.className)}>
       <h1 className={styles.header}>{props.title}</h1>
       <div className={styles.navigator}>{props.children}</div>
     </div>
