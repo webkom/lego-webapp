c9736e849f43d670aad62251cbb8600179dd23f7
Martin Ek <mail@ekmartin.com>
2017-10-15 02:24:46 +0200

    Update UserProfile tests

--- app/routes/users/components/__tests__/UserProfile.spec.js
+++ app/routes/users/components/__tests__/UserProfile.spec.js
@@ -16,25 +16,25 @@ const user = {
 
 describe('<UserProfile />', () => {
   it('should show a settings link if the user is me', () => {
-    const wrapper = shallow(<UserProfile user={user} isMe />);
+    const wrapper = shallow(<UserProfile user={user} showSettings />);
     expect(
       wrapper.containsMatchingElement(
-        <Link to="/users/me/settings/profile">Settings</Link>
+        <Link to="/users/webkom/settings/profile">Instillinger</Link>
       )
     ).toEqual(true);
   });
 
   it('should not show a settings link for other users', () => {
-    const wrapper = shallow(<UserProfile user={user} isMe={false} />);
+    const wrapper = shallow(<UserProfile user={user} showSettings={false} />);
     expect(
       wrapper.containsMatchingElement(
-        <Link to="/users/me/settings/profile">Settings</Link>
+        <Link to="/users/webkom/settings/profile">Instillinger</Link>
       )
     ).toEqual(false);
   });
 
   it('should render user info', () => {
-    const wrapper = shallow(<UserProfile user={user} isMe={false} />);
+    const wrapper = shallow(<UserProfile user={user} showSettings={false} />);
     expect(wrapper.containsMatchingElement(<h2>{user.fullName}</h2>)).toEqual(
       true
     );
