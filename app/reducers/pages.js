930417ae564678c0dcf1924e914b63d57cdde73a
Odin Ugedal <odin@ugedal.com>
2017-10-17 11:39:18 +0200

    Remove unused tree structure selectors in pages
    Also remove tests for the removed selectors

--- app/reducers/pages.js
+++ app/reducers/pages.js
@@ -1,7 +1,6 @@
 // @flow
 
 import { createSelector } from 'reselect';
-import values from 'lodash/values';
 import { Page } from '../actions/ActionTypes';
 import createEntityReducer from 'app/utils/createEntityReducer';
 import { selectGroups } from './groups';
@@ -28,57 +27,10 @@ export const selectPageBySlug = createSelector(
   (pagesBySlug, pageSlug) => pagesBySlug[pageSlug] || {}
 );
 
-const rootKey = 'root';
-/**
- * Maps parent PKs to a list of their children:
- */
-const selectParents = createSelector(
-  state => state.pages.byId,
-  pagesBySlug =>
-    Object.keys(pagesBySlug)
-      .map(key => pagesBySlug[key])
-      .reduce((total, page) => {
-        const parent = page.parent || rootKey;
-        const existing = total[parent] || [];
-        return {
-          ...total,
-          [parent]: [...existing, page]
-        };
-      }, {})
-);
-
-/**
- * Finds the siblings of the given parent PK
- * (includes self)
- */
-export const selectSiblings = createSelector(
-  selectParents,
-  (state, props) => props.parentPk || rootKey,
-  (pagesByParent, parentPk) => pagesByParent[parentPk]
-);
-
-/**
- * Finds the children of the given PK
- */
-export const selectChildren = createSelector(
-  state => values(state.pages.byId),
-  (state, props) => props.parentPk,
-  (pages, parentPk) => pages.filter(page => page.parent === parentPk)
-);
-
 export const selectPages = createSelector(
   state => state.pages.byId,
   (pagesBySlug, pageSlug) =>
     Object.keys(pagesBySlug).map(slug => pagesBySlug[slug])
-);
-
-/**
- * Finds the page with the given parent PK.
- */
-export const selectParent = createSelector(
-  state => values(state.pages.byId),
-  (state, props) => props.parentPk,
-  (pages, parentPk) => pages.find(page => page.pk === parentPk)
 );
 
 export const selectPagesForHierarchy = createSelector(
@@ -92,6 +44,7 @@ export const selectPagesForHierarchy = createSelector(
     }))
   })
 );
+
 export const selectGroupsForHierarchy = createSelector(
   state => selectGroups(state),
   (state, props) => props.title,
