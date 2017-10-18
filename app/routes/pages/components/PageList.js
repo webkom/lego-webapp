935031e8bfa6cf54a912b768117d1aba83e8601c
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2017-10-18 21:39:22 +0200

    Wrap som pages in the Content component

--- app/routes/pages/components/PageList.js
+++ app/routes/pages/components/PageList.js
@@ -2,20 +2,23 @@
 
 import React from 'react';
 import { Link } from 'react-router';
+import { Content } from 'app/components/Layout';
 
 type Props = {
-  pages: Object[]
+  pages: Array<*>
 };
 
 const PageList = ({ pages }: Props) => {
   return (
-    <ul>
-      {Object.values(pages).map(page => (
-        <li key={page.pk}>
-          <Link to={`/pages/${page.slug}`}>{page.title}</Link>
-        </li>
-      ))}
-    </ul>
+    <Content>
+      <ul>
+        {Object.values(pages).map(page => (
+          <li key={page.pk}>
+            <Link to={`/pages/${page.slug}`}>{page.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </Content>
   );
 };
 
