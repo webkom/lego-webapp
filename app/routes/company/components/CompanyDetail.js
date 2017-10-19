c71582d1cd5432b506058e5c71119c432b970ab2
Martin Farstad <farstad2k@gmail.com>
2017-10-19 22:05:26 +0200

    fix detail style

--- app/routes/company/components/CompanyDetail.js
+++ app/routes/company/components/CompanyDetail.js
@@ -6,20 +6,23 @@ import LoadingIndicator from 'app/components/LoadingIndicator';
 import { Image } from 'app/components/Image';
 import InfoBubble from 'app/components/InfoBubble';
 import { Link } from 'react-router';
-import Button from 'app/components/Button';
-import Icon from 'app/components/Icon';
+import truncateString from 'app/utils/truncateString';
+import { eventTypes } from 'app/routes/events/utils';
+import Time from 'app/components/Time';
+import NavigationTab from 'app/components/NavigationTab';
+import NavigationLink from 'app/components/NavigationTab/NavigationLink';
 
 type Props = {
-  company: Object
+  company: Object,
+  companyEvents: Array<Object>
 };
 
 function insertInfoBubbles(company) {
   const infos = [
-    ['phone', company.phone],
-    ['home', company.website],
-    ['twitter', company.twitter],
-    ['facebook', company.facebook],
-    ['github', company.github]
+    ['call', company.phone, 'Telefon'],
+    ['at', company.website, 'Nettside'],
+    ['home', company.address, 'Addresse'],
+    ['briefcase', company.companyType, 'Type bedrift']
   ];
 
   return (
@@ -29,6 +32,7 @@ function insertInfoBubbles(company) {
           key={info[0]}
           icon={info[0]}
           data={info[1]}
+          meta={info[2]}
           style={{ order: i }}
           link={info[1] && info[1].includes('.') ? info[1] : undefined}
           small
@@ -40,38 +44,64 @@ function insertInfoBubbles(company) {
   );
 }
 
-const CompanyDetail = ({ company }: Props) => {
+const CompanyDetail = (props: Props) => {
+  const { company, companyEvents } = props;
   if (!company) {
     return <LoadingIndicator loading />;
   }
+  const events =
+    companyEvents &&
+    companyEvents
+      .sort((a, b) => new Date(b.startTime) - new Date(a.startTime))
+      .map((event, i) => (
+        <tr key={i}>
+          <td>
+            <Link to={`events/${event.id}`}>{event.title}</Link>
+          </td>
+          <td>{eventTypes[event.eventType]}</td>
+          <td>
+            <Time time={event.startTime} format="DD.MM.YYYY" />
+          </td>
+          <td>{truncateString(event.location, 50)}</td>
+          <td>{truncateString(event.description, 70)}</td>
+        </tr>
+      ));
 
   return (
     <div className={styles.root}>
-      <div className={styles.coverImage}>
+      <div className={styles.companyLogoDetail}>
         <Image src={company.logo} className={styles.image} />
       </div>
 
-      <div className={styles.titleFlex}>
-        <h1 className={styles.title} style={{ order: 1 }}>
-          {company.name}
-        </h1>
-        <Link
-          to={'/companies'}
-          className={styles.editLink}
-          style={{ order: 2 }}
-        >
-          <Button>
-            <Icon name="pencil" />
-            Endre
-          </Button>
-        </Link>
-      </div>
-
-      {insertInfoBubbles(company)}
+      <NavigationTab title={company.name}>
+        <NavigationLink to="/companies">Tilbake til liste</NavigationLink>
+      </NavigationTab>
 
       <div className={styles.description}>
         <p>{company.description}</p>
       </div>
+
+      {insertInfoBubbles(company)}
+
+      <h3>Bedriftens arrangementer</h3>
+      {events.length > 0 ? (
+        <div className={styles.companyList}>
+          <table className={styles.companyEventTable}>
+            <thead className={styles.categoryHeader}>
+              <tr>
+                <th>Tittel</th>
+                <th>Arrangementstype</th>
+                <th>Når</th>
+                <th>Hvor</th>
+                <th>Hva</th>
+              </tr>
+            </thead>
+            <tbody>{events}</tbody>
+          </table>
+        </div>
+      ) : (
+        <i>Ingen arrangementer.</i>
+      )}
     </div>
   );
 };
