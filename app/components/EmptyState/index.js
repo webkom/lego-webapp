01a575291f4e21f2624afdc73a0d04ac47973d7c
Finn Julius Stephansen-Smith <finnjs@stud.ntnu.no>
2017-10-19 22:48:50 +0200

    Center the emptyState if there is an icon

--- app/components/EmptyState/index.js
+++ app/components/EmptyState/index.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import styles from './EmptyState.css';
 import Icon from '../Icon';
+import cx from 'classnames';
 
 type Props = {
   /** name of icon */
@@ -17,7 +18,7 @@ type Props = {
  *
  */
 const EmptyState = ({ icon, size = 88, children }: Props) => (
-  <div className={styles.container}>
+  <div className={cx(styles.container, icon && styles.centered)}>
     {icon && <Icon className={styles.icon} size={size} name={icon} />}
     {children}
   </div>
