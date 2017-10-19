0a04409d4b25569f6805c471ea9d49a6853a73b3
Finn Julius Stephansen-Smith <finnjs@stud.ntnu.no>
2017-10-19 20:17:48 +0200

    Alternative user profile design

--- app/routes/users/components/UserProfile.js
+++ app/routes/users/components/UserProfile.js
@@ -57,70 +57,72 @@ export default class UserProfile extends Component {
         <Helmet title={`${user.firstName} ${user.lastName}`} />
 
         <Flex wrap className={styles.header}>
-          <div className={cx(styles.sidebar, styles.picture)}>
+          <Flex className={styles.left}>
             <ProfilePicture user={user} size={150} />
-          </div>
-          <Flex column className={styles.rightContent}>
-            <h2>{user.fullName}</h2>
-            <Flex wrap>
-              {(abakusGroups['withoutLogo'] || []).map(group => (
-                <Pill key={group.id} style={{ margin: '5px' }}>
-                  {group.name}
-                </Pill>
-              ))}
-            </Flex>
-            <Flex>
-              {(abakusGroups['withLogo'] || []).map(group => (
-                <Tooltip key={group.id} content={group.name}>
-                  <CircularPicture
-                    src={group.logo}
-                    size={50}
-                    style={{ margin: '10px 5px' }}
-                  />
-                </Tooltip>
-              ))}
+
+            <Flex column className={styles.primaryInfo}>
+              <h2 style={{ marginBottom: '15px' }}>{user.fullName}</h2>
+
+              <Flex wrap>
+                {(abakusGroups['withoutLogo'] || []).map(group => (
+                  <Pill key={group.id} style={{ margin: '5px' }}>
+                    {group.name}
+                  </Pill>
+                ))}
+              </Flex>
+              <Flex>
+                {(abakusGroups['withLogo'] || []).map(group => (
+                  <Tooltip key={group.id} content={group.name}>
+                    <CircularPicture
+                      src={group.logo}
+                      size={50}
+                      style={{ margin: '10px 5px' }}
+                    />
+                  </Tooltip>
+                ))}
+              </Flex>
             </Flex>
           </Flex>
+          <Card style={{ maxWidth: '300px' }}>
+            {this.renderFields()}
+            {showSettings ? (
+              <Link to={`/users/${user.username}/settings/profile`}>
+                Innstillinger
+              </Link>
+            ) : (
+              ''
+            )}
+          </Card>
+        </Flex>
+
+        <Flex>
+          {this.props.isMe &&
+            this.props.user.email !== this.props.user.emailAddress && (
+              <Card style={{ marginTop: '20px' }}>
+                <h3>Google GSuite</h3>
+                <p>
+                  Din konto er linket opp mot Abakus sitt domene i Google
+                  GSuite. E-post sendes til denne brukeren og ikke til e-posten
+                  du har oppgitt i din profil.
+                </p>
+
+                <ul>
+                  <li>
+                    <b>URL:</b>{' '}
+                    <a href="http://mail.abakus.no">mail.abakus.no</a>
+                  </li>
+                  <li>
+                    <b>E-post:</b> {this.props.user.emailAddress}
+                  </li>
+                  <li>
+                    <b>Passord:</b> <i>Ditt abakus passord</i>
+                  </li>
+                </ul>
+              </Card>
+            )}
         </Flex>
 
         <Flex wrap className={styles.content}>
-          <div className={styles.sidebar}>
-            <Card>
-              {this.renderFields()}
-              {showSettings ? (
-                <Link to={`/users/${user.username}/settings/profile`}>
-                  Innstillinger
-                </Link>
-              ) : (
-                ''
-              )}
-            </Card>
-            {this.props.isMe &&
-              this.props.user.email !== this.props.user.emailAddress && (
-                <Card style={{ marginTop: '20px' }}>
-                  <h3>Google GSuite</h3>
-                  <p>
-                    Din konto er linket opp mot Abakus sitt domene i Google
-                    GSuite. E-post sendes til denne brukeren og ikke til
-                    e-posten du har oppgitt i din profil.
-                  </p>
-
-                  <ul>
-                    <li>
-                      <b>URL:</b>{' '}
-                      <a href="http://mail.abakus.no">mail.abakus.no</a>
-                    </li>
-                    <li>
-                      <b>E-post:</b> {this.props.user.emailAddress}
-                    </li>
-                    <li>
-                      <b>Passord:</b> <i>Ditt abakus passord</i>
-                    </li>
-                  </ul>
-                </Card>
-              )}
-          </div>
-
           <div className={styles.feed}>
             <h2>Nylig Aktivitet</h2>
             {feed ? (
