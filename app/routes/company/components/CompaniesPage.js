daf04b5f9aa6e368fa268180024bf4883cda7664
Martin Farstad <farstad2k@gmail.com>
2017-10-19 21:32:36 +0200

    add style for companyList

--- app/routes/company/components/CompaniesPage.js
+++ app/routes/company/components/CompaniesPage.js
@@ -4,10 +4,42 @@ import React from 'react';
 import styles from './Company.css';
 import LoadingIndicator from 'app/components/LoadingIndicator';
 import { Link } from 'react-router';
+import Image from 'app/components/Image';
+import truncateString from 'app/utils/truncateString';
 
 type Props = {
   companies: Array<Object>
 };
+
+export function CompanyItem({ company }: any) {
+  return (
+    <div className={styles.companyItem}>
+      <div>
+        <Link key={company.id} to={`/companies/${company.id}`}>
+          <h3 className={styles.companyItemTitle}>{company.name}</h3>
+        </Link>
+        <div className={styles.website}>
+          {`${company.website} • ${company.address}`
+            ? company.website || company.address
+            : ''}
+        </div>
+        <p>{truncateString(company.description, 300)}</p>
+      </div>
+      <div className={styles.companyLogo}>
+        {company.thumbnail && <Image src={company.thumbnail} />}
+      </div>
+    </div>
+  );
+}
+
+function CompanyList({ name, companies = [] }) {
+  return (
+    <div className={styles.companies}>
+      <h2 className={styles.heading}>{name}</h2>
+      {companies.map((company, i) => <CompanyItem key={i} company={company} />)}
+    </div>
+  );
+}
 
 const CompaniesPage = ({ companies }: Props) => {
   if (companies.length < 1) {
@@ -16,14 +48,7 @@ const CompaniesPage = ({ companies }: Props) => {
 
   return (
     <div className={styles.root}>
-      <h1>Companies</h1>
-      <p>
-        {companies.map((company, id) => (
-          <Link key={id} to={`/companies/${company.id}`}>
-            {company.name}
-          </Link>
-        ))}
-      </p>
+      <CompanyList name="Bedrifter" companies={companies} />
     </div>
   );
 };
