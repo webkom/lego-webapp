3b65f786fe77b44fd4695b0f8ad71400a5439791
Martin Hafskjold Thoresen <martinhath@gmail.com>
2017-10-19 14:51:05 +0200

    Implement group permission admin
    So far it is possible to add permissions. No validation of the
    permission is done in the frontend.
    
    Also, the `editGroup` action redirected to `interestgroups`, for some
    reason. My guess is that we tried to remove the interest group acitons,
    in order to unite all group actions, but seing as we're still using
    them, we probably gave up. This is probably for the better, since there
    is some special handling of the actions of interest groups.

---
+++ app/routes/admin/groups/components/AddGroupPermission.js
@@ -0,0 +1,51 @@
+// @flow
+import React from 'react';
+import { reduxForm, Field } from 'redux-form';
+import type { FieldProps } from 'redux-form';
+import { Button, Form } from 'app/components/Form';
+import TextInput from 'app/components/Form/TextInput';
+import { createValidator, required } from 'app/utils/validation';
+import { omit } from 'lodash';
+
+type Props = FieldProps & {
+  group: Object
+};
+
+const AddGroupPermission = ({
+  editGroup,
+  submitting,
+  group,
+  handleSubmit
+}: Props) => {
+  const onSubmit = handleSubmit(({ permission }) =>
+    editGroup({
+      ...omit(group || {}, 'logo'),
+      permissions: group.permissions.concat(permission)
+    })
+  );
+
+  return (
+    <Form onSubmit={onSubmit}>
+      <h3>Legg til ny rettighet</h3>
+      <Field
+        label="Rettighet"
+        name="permission"
+        placeholder="/sudo/samfundet/øl/drikk"
+        component={TextInput.Field}
+      />
+
+      <Button submit disabled={submitting}>
+        Legg til rettighet
+      </Button>
+    </Form>
+  );
+};
+
+const validate = createValidator({
+  permission: [required()]
+});
+
+export default reduxForm({
+  form: 'add-permission',
+  validate
+})(AddGroupPermission);
