f76515b7177f3f6693e78b407aa73639a3bfad0c
Martin Ek <mail@ekmartin.com>
2017-10-20 04:00:34 +0200

    Use callAPI directly

--- app/actions/FrontpageActions.js
+++ app/actions/FrontpageActions.js
@@ -5,16 +5,13 @@ import { Frontpage } from './ActionTypes';
 import { frontpageSchema } from 'app/reducers';
 
 export function fetch() {
-  return dispatch =>
-    dispatch(
-      callAPI({
-        types: Frontpage.FETCH,
-        endpoint: '/frontpage/',
-        schema: frontpageSchema,
-        meta: {
-          errorMessage: 'Klarte ikke hente forsiden!'
-        },
-        propagateError: true
-      })
-    );
+  return callAPI({
+    types: Frontpage.FETCH,
+    endpoint: '/frontpage/',
+    schema: frontpageSchema,
+    meta: {
+      errorMessage: 'Klarte ikke hente forsiden!'
+    },
+    propagateError: true
+  });
 }
