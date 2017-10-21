5ac380dd5dea6dd08c5b8ab14d6a4ce26b599b81
Odin Ugedal <odin@ugedal.com>
2017-10-21 23:25:20 +0200

    Add 'loggedIn' as a default watchProp in prepare

--- app/routes/events/EventListRoute.js
+++ app/routes/events/EventListRoute.js
@@ -3,7 +3,7 @@
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import { dispatched } from '@webkom/react-prepare';
+import prepare from 'app/utils/prepare';
 import { fetchList } from 'app/actions/EventActions';
 import EventList from './components/EventList';
 import { selectSortedEvents } from 'app/reducers/events';
@@ -45,8 +45,6 @@ const mapDispatchToProps = {
 };
 
 export default compose(
-  dispatched((props, dispatch) => dispatch(fetchData()), {
-    componentWillReceiveProps: false
-  }),
+  prepare((props, dispatch) => dispatch(fetchData())),
   connect(mapStateToProps, mapDispatchToProps)
 )(EventList);
