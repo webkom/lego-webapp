a68fc608b8501b2c7f7368bd7514e1a1d4273a1f
Odin Ugedal <odin@ugedal.com>
2017-11-07 23:31:17 +0100

    Use test recaptcha sitekey
    Use the testing sitekeys for recaptcha. More info can be found here:
    - https://developers.google.com/recaptcha/docs/faq

--- config/env.js
+++ config/env.js
@@ -4,7 +4,8 @@ module.exports = {
   baseUrl: process.env.BASE_URL || 'http://127.0.0.1:8000',
   segmentWriteKey:
     process.env.SEGMENT_WRITE_KEY || 'AwzecATYd1qiH21IY0hruqg63Q4QwZtO',
-  captchaKey: process.env.CAPTCHA_KEY,
+  captchaKey:
+    process.env.CAPTCHA_KEY || '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
   stripeKey: process.env.STRIPE_KEY || 'pk_test_VWJmJ3yOunhMBkG71SXyjdqk',
   ravenDSN: process.env.RAVEN_DSN,
   release: process.env.RELEASE,
