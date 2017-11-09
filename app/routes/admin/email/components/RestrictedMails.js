abaefd544c1426643d05d042cc487c1a66235b68
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2017-11-09 16:00:49 +0100

    Document how email works (#970)
    * Document how email works
    
    * Typos and clarification
    
    * Typos and clarification

--- app/routes/admin/email/components/RestrictedMails.js
+++ app/routes/admin/email/components/RestrictedMails.js
@@ -51,16 +51,38 @@ export default class RestrictedMails extends Component<Props> {
     ];
 
     return (
-      <Table
-        infiniteScroll
-        columns={columns}
-        onFetch={() => {
-          this.props.fetch({ next: true });
-        }}
-        hasMore={this.props.hasMore}
-        loading={this.props.fetching}
-        data={this.props.restrictedMails}
-      />
+      <div>
+        <p>
+          Begrenset epost benyttes om det ønskes å sende en målrettet epost til
+          en kombinasjon av grupper, brukere, møter, arrangementer eller
+          definerte adresser. En begrenset epost kan kun benyttes en gang, og må
+          komme fra en forhåndsdefinert adresse. For ytterligere sikkerhet må en fil
+          legges som vedlegg for verifisering av avsender. Denne filen vil bli
+          validert og fjernet før eposten sendes til mottakere. Begrenset epost
+          kan sendes med skult avsender, mottakere vil da ikke kunne svare på
+          eposten.
+          <ul style={{ listStyleType: 'circle', listStylePosition: 'inside' }}>
+            <li>Opprett en begrenset epost</li>
+            <li>Last ned epost token</li>
+            <li>Opprett en epost med oppgitt avsender</li>
+            <li>
+              Mottaker settes til <b>restricted@abakus.no</b>
+            </li>
+            <li>Legg inn epost token som vedlegg</li>
+            <li>Send eposten</li>
+          </ul>
+        </p>
+        <Table
+          infiniteScroll
+          columns={columns}
+          onFetch={() => {
+            this.props.fetch({ next: true });
+          }}
+          hasMore={this.props.hasMore}
+          loading={this.props.fetching}
+          data={this.props.restrictedMails}
+        />
+      </div>
     );
   }
 }
