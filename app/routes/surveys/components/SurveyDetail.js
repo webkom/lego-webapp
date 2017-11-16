af480dd123072db460c932e3dd5b809e65b9ef80
Finn Julius Stephansen-Smith <finnjs@stud.ntnu.no>
2017-11-16 18:56:07 +0100

    Use content banner in survey detail

--- app/routes/surveys/components/SurveyDetail.js
+++ app/routes/surveys/components/SurveyDetail.js
@@ -3,7 +3,6 @@
 import React from 'react';
 import { Link } from 'react-router';
 import Time from 'app/components/Time';
-import { Image } from 'app/components/Image';
 import styles from './surveyDetail.css';
 import type { SurveyEntity } from 'app/reducers/surveys';
 import LoadingIndicator from 'app/components/LoadingIndicator';
@@ -21,28 +20,22 @@ const SurveyDetail = (props: Props) => {
   if (!survey.event) return <LoadingIndicator />;
 
   return (
-    <div>
-      <div className={styles.coverImage}>
-        <Image src={survey.event.cover} />
+    <Content className={styles.surveyDetail} banner={survey.event.cover}>
+      <DetailNavigation
+        title={survey.title}
+        surveyId={Number(survey.id)}
+        deleteFunction={deleteSurvey}
+      />
+
+      <div className={styles.surveyTime}>
+        Spørreundersøkelse for{' '}
+        <Link to={`/surveys/${survey.event}`}>{survey.event.title}</Link>
       </div>
 
-      <Content className={styles.surveyDetail}>
-        <DetailNavigation
-          title={survey.title}
-          surveyId={Number(survey.id)}
-          deleteFunction={deleteSurvey}
-        />
-
-        <div className={styles.surveyTime}>
-          Spørreundersøkelse for{' '}
-          <Link to={`/surveys/${survey.event}`}>{survey.event.title}</Link>
-        </div>
-
-        <div className={styles.surveyTime}>
-          Aktiv fra <Time time={survey.activeFrom} format="ll HH:mm" />
-        </div>
-      </Content>
-    </div>
+      <div className={styles.surveyTime}>
+        Aktiv fra <Time time={survey.activeFrom} format="ll HH:mm" />
+      </div>
+    </Content>
   );
 };
 
