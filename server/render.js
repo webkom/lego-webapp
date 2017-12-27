378b2572f7763f58472449e49f1425444af44f90
Odin Ugedal <odin@ugedal.com>
2017-12-27 18:36:15 +0100

    Add required assets to client build

--- server/render.js
+++ server/render.js
@@ -13,13 +13,6 @@ import configureStore from '../app/utils/configureStore';
 import config from '../config/env';
 
 import manifest from '../app/assets/manifest.json';
-require('../app/assets/favicon.png');
-require('../app/assets/icon-48x48.png');
-require('../app/assets/icon-96x96.png');
-require('../app/assets/icon-192x192.png');
-require('../app/assets/icon-256x256.png');
-require('../app/assets/icon-384x384.png');
-require('../app/assets/icon-512x512.png');
 
 const serverSideTimeoutInMs = 4000;
 
