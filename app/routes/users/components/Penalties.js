050ba833c1e5aa6c47e6fbce6c405d0d57f3e724
cherrybeans <sigridkvamme@gmail.com>
2018-01-11 23:43:24 +0100

    Prettify penalties

--- app/routes/users/components/Penalties.js
+++ app/routes/users/components/Penalties.js
@@ -2,14 +2,13 @@
 
 import React from 'react';
 import moment from 'moment-timezone';
-import Time, { FormatTime } from 'app/components/Time';
-
-
+import { FormatTime } from 'app/components/Time';
 
 type Penalty = {
   id: number,
   reason: string,
-  weight: number
+  weight: number,
+  exactExpiration: string
 };
 
 type Props = {
@@ -26,15 +25,17 @@ function Penalties({ penalties }: Props) {
       {penalties.map(penalty => {
         const word = penalty.weight > 1 ? 'prikker' : 'prikk';
         return (
-          <li key={penalty.id} style={{paddingBottom: "10px"}}>
+          <li key={penalty.id} style={{ paddingBottom: '10px' }}>
             <strong>
               Du har fått {penalty.weight} {word}
             </strong>
-            <br/>
+            <br />
             Begrunnelse: <i>{penalty.reason}</i>
-            <br/>
-            Utgår: <i><FormatTime time={moment(penalty.exactExpiration)}/></i>
-
+            <br />
+            Utgår:{' '}
+            <i>
+              <FormatTime time={moment(penalty.exactExpiration)} />
+            </i>
           </li>
         );
       })}
