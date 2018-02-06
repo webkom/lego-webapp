12b9f4cf747ecd889cb52b083946f18576e44bbe
Odin Ugedal <odin@ugedal.com>
2018-02-06 13:08:18 +0100

    Add better flow annotations to helmet util

--- app/utils/helmet.js
+++ app/utils/helmet.js
@@ -1,31 +1,38 @@
 // @flow
 
-import React, { createElement } from 'react';
+import * as React from 'react';
 import { Helmet } from 'react-helmet';
 import config from 'app/config';
+import { type ComponentType, createElement } from 'react';
 
 /**
  * A higher order component that wraps the given component in
  * LoadingIndicator while `props[loadingProp]` is being fetched.
  */
-export default function helmet(
-  properties: (
-    props: Object,
-    config?: Object
-  ) => Array<{
-    property?: string,
-    content?: string,
-    element?: string,
-    children?: string,
-    rel?: string,
-    href?: string
-  }>
-) {
-  // $FlowFixMe React.Node in >= 0.53
-  return (Component: React.Class<*, *, *>) => (props: Object) => [
+
+type PropertyGenerator = (
+  props: Object,
+  config?: Object
+) => Array<{
+  property?: string,
+  content?: string,
+  element?: string,
+  children?: string,
+  rel?: string,
+  href?: string
+}>;
+
+export default function helmet<T>(propertyGenerator: PropertyGenerator) {
+  return (Component: ComponentType<T>) => ({
+    PropertyGenerator,
+    ...props
+  }: T & {
+    propertyGenerator: PropertyGenerator
+  }) => [
     <Helmet key="helmet">
-      {properties(props, config).map(({ element, children, ...props }) =>
-        createElement(element || 'meta', { ...props }, children)
+      {propertyGenerator(props, config).map(
+        ({ element, children, ...props }, index) =>
+          createElement(element || 'meta', { key: index, ...props }, children)
       )}
     </Helmet>,
     <Component key="component" {...props} />
