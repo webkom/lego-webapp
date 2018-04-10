99e59eda2515108b5dc2182460107fc0c48f66cd
Finn Julius Stephansen-Smith <finnjs@stud.ntnu.no>
2018-04-10 14:02:28 +0200

    Survey pagination

--- app/routes/surveys/SurveyRoute.js
+++ app/routes/surveys/SurveyRoute.js
@@ -1,30 +1,28 @@
 import { connect } from 'react-redux';
 import prepare from 'app/utils/prepare';
-import { fetchAll, addSurvey, deleteSurvey } from '../../actions/SurveyActions';
+import { fetchAll } from '../../actions/SurveyActions';
 import SurveyPage from './components/SurveyList/SurveyPage';
 import { compose } from 'redux';
 import { selectSurveys } from 'app/reducers/surveys';
 import { LoginPage } from 'app/components/LoginForm';
 import replaceUnlessLoggedIn from 'app/utils/replaceUnlessLoggedIn';
 import { push } from 'react-router-redux';
-import loadingIndicator from 'app/utils/loadingIndicator';
 
 const loadData = (props, dispatch) => dispatch(fetchAll());
 
 const mapStateToProps = (state, props) => ({
   surveys: selectSurveys(state, props).filter(survey => !survey.templateType),
-  notFetching: !state.surveys.fetching
+  fetching: state.surveys.fetching,
+  hasMore: state.surveys.hasMore
 });
 
 const mapDispatchToProps = {
-  addSurvey,
-  deleteSurvey,
+  fetchAll,
   push
 };
 
 export default compose(
   replaceUnlessLoggedIn(LoginPage),
   prepare(loadData),
-  connect(mapStateToProps, mapDispatchToProps),
-  loadingIndicator(['notFetching'])
+  connect(mapStateToProps, mapDispatchToProps)
 )(SurveyPage);
