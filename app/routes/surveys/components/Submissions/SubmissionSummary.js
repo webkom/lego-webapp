72e39a1cf1be1fc4d62dfff1312d7235eb867a88
Finn Julius Stephansen-Smith <finnjs@stud.ntnu.no>
2018-04-14 18:40:06 +0200

    Add event info and pie charts to the survey result page

--- app/routes/surveys/components/Submissions/SubmissionSummary.js
+++ app/routes/surveys/components/Submissions/SubmissionSummary.js
@@ -4,7 +4,10 @@ import React from 'react';
 import styles from '../surveys.css';
 import type { SubmissionEntity } from 'app/reducers/surveySubmissions';
 import type { SurveyEntity } from 'app/reducers/surveys';
-import { QuestionTypes } from '../../utils';
+import { QuestionTypes, CHART_COLORS } from '../../utils';
+import InfoBubble from 'app/components/InfoBubble';
+import { VictoryPie, VictoryTheme } from 'victory';
+import type { QuestionEntity } from 'app/reducers/surveys';
 
 type Props = {
   submissions: Array<SubmissionEntity>,
@@ -27,10 +30,68 @@ const SubmissionSummary = ({ submissions, deleteSurvey, survey }: Props) => {
     return texts.length === 0 ? <i>Ingen svar.</i> : texts;
   };
 
+  const generateQuestionData = (question: QuestionEntity) => {
+    const questionData = [];
+
+    question.options.map(option => {
+      const selectedCount = submissions
+        .map(
+          submission =>
+            submission.answers.find(
+              answer => answer.question.id === question.id
+            ) || {}
+        )
+        .filter(answer =>
+          (answer.selectedOptions || []).find(o => o === option.id)
+        ).length;
+
+      questionData.push({
+        option: option.optionText,
+        selections: selectedCount
+      });
+    });
+    return questionData;
+  };
+
+  const graphData = {};
+  survey.questions.map(question => {
+    graphData[question.id] = generateQuestionData(question);
+  });
+
   return (
-    <ul className={styles.summary}>
-      {survey.questions.map(question => {
-        return (
+    <div>
+      <div className={styles.eventSummary}>
+        <h3>Arrangementet</h3>
+        <div className={styles.infoBubbles}>
+          <InfoBubble
+            icon="person"
+            data={String(survey.event.registrationCount)}
+            meta="Påmeldte"
+            style={{ order: 0 }}
+          />
+          <InfoBubble
+            icon="checkmark"
+            data={String(survey.event.attendedCount)}
+            meta="Møtte opp"
+            style={{ order: 1 }}
+          />
+          <InfoBubble
+            icon="list"
+            data={String(survey.event.waitingRegistrationCount)}
+            meta="På venteliste"
+            style={{ order: 2 }}
+          />
+          <InfoBubble
+            icon="chatboxes"
+            data={String(submissions.length)}
+            meta="Har svart"
+            style={{ order: 3 }}
+          />
+        </div>
+      </div>
+
+      <ul className={styles.summary}>
+        {survey.questions.map(question => (
           <li key={question.id}>
             <h3>{question.questionText}</h3>
 
@@ -39,31 +100,44 @@ const SubmissionSummary = ({ submissions, deleteSurvey, survey }: Props) => {
                 {textAnswers(submissions, question)}
               </ul>
             ) : (
-              <ul className={styles.detailOptions}>
-                {question.options.map(option => {
-                  const selectedCount = submissions
-                    .map(
-                      submission =>
-                        submission.answers.find(
-                          answer => answer.question.id === question.id
-                        ) || {}
-                    )
-                    .filter(answer =>
-                      (answer.selectedOptions || []).find(o => o === option.id)
-                    ).length;
+              <div className={styles.questionResults}>
+                <div style={{ width: '300px' }}>
+                  <VictoryPie
+                    data={graphData[question.id]}
+                    x="option"
+                    y="selections"
+                    theme={VictoryTheme.material}
+                    colorScale={CHART_COLORS}
+                    labels={d => d.y}
+                    labelRadius={60}
+                    padding={{ left: 0, top: 40, right: 30, bottom: 30 }}
+                    style={{
+                      labels: { fill: 'white', fontSize: 20 }
+                    }}
+                  />
+                </div>
 
-                  return (
-                    <li key={option.id}>
-                      {option.optionText}: {String(selectedCount)}
+                <ul className={styles.graphData}>
+                  {graphData[question.id].map((dataPoint, i) => (
+                    <li key={i}>
+                      <span
+                        className={styles.colorBox}
+                        style={{ backgroundColor: CHART_COLORS[i] }}
+                      >
+                        &nbsp;
+                      </span>
+                      <span style={{ marginTop: '-5px' }}>
+                        {dataPoint.option}
+                      </span>
                     </li>
-                  );
-                })}
-              </ul>
+                  ))}
+                </ul>
+              </div>
             )}
           </li>
-        );
-      })}
-    </ul>
+        ))}
+      </ul>
+    </div>
   );
 };
 
