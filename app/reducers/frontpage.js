a0abf6b63572189fe3e3c06cf6c89e621958cb40
Odin Ugedal <odin@ugedal.com>
2018-09-23 14:25:10 +0200

    Make event sorting stable and deterministic

--- app/reducers/frontpage.js
+++ app/reducers/frontpage.js
@@ -17,6 +17,7 @@ export const selectFrontpage = createSelector(
       documentType: 'article'
     }));
     events = events.map(event => ({ ...event, documentType: 'event' }));
+    const now = moment();
     return sortBy(articles.concat(events), [
       // Always sort pinned items first:
       item => !item.pinned,
@@ -24,7 +25,7 @@ export const selectFrontpage = createSelector(
         // For events we care about when the event starts, whereas for articles
         // we look at when it was written:
         const timeField = item.eventType ? item.startTime : item.createdAt;
-        return Math.abs(moment().diff(timeField));
+        return Math.abs(now.diff(timeField));
       },
       item => item.id
     ]);
