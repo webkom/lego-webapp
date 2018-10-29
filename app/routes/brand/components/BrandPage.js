a6be228d50d77e3e6e1b4a2adcde0114f22f1039
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-10-29 20:32:22 +0100

    Add paragraph about usage

--- app/routes/brand/components/BrandPage.js
+++ app/routes/brand/components/BrandPage.js
@@ -9,11 +9,18 @@ const BrandPage = () => {
       <NavigationTab title="Brand Guidelines" />
       <section>
         <div className={styles.root}>
-          <p>Hei, vi i PR har laget noen retningslinjer for å hjelpe deg å
+          <p>
+            Hei, vi i PR har laget noen retningslinjer for å hjelpe deg å
             bruke vårt brand og design. For å bruke logoen og designet på
             en måte som ikke er dekket av disse retningslinjene
-            kontakt oss på <a href="mailto:abakus@abakus.no">pr@abakus.no</a> og
-            legg ved en mockup av hvordan du har tenkt til å bruke det.
+            kontakt oss på <a href="mailto:abakus@abakus.no">pr@abakus.no</a>
+            og legg ved en mockup av hvordan du har tenkt til å bruke det.
+          </p>
+          <h2>Bruk</h2>
+          <p>
+            Abakus' merker inkluderer Abakus-navnet, logoen og ord som
+            identifiserer oss. Vær så snill å ikke modifiser merkene eller bruk
+            dem på en forvirrende måte.
           </p>
         </div>
       </section>
