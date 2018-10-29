f2e94638e5950a295aff15633b14c79f886da527
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-10-29 16:05:47 +0100

    Refactor to stateless component

--- app/routes/brand/components/BrandPage.js
+++ app/routes/brand/components/BrandPage.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Content } from 'app/components/Content';
 import NavigationTab from 'app/components/NavigationTab';
 import styles from './BrandPage.css';
 
-class BrandPage extends Component {
-  render() {
-    return (
-      <Content>
-        <NavigationTab title="Brand Guidelines" />
-        <section>
-          <div className={styles.root}>HTML CONTENT GOES HERE</div>
-        </section>
-      </Content>
-    );
-  }
+const BrandPage = () => {
+  return (
+    <Content>
+      <NavigationTab title="Brand Guidelines" />
+      <section>
+        <div className={styles.root}>HTML CONTENT GOES HERE</div>
+      </section>
+    </Content>
+  );
 }
+
 
 export default BrandPage;
