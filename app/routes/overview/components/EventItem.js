7d236068497fbb401e61e9eb037ea5f7b00c5eba
Peder Smith <smith.peder@gmail.com>
2019-01-15 21:58:13 +0100

    Fix Ingen påmelding tag

--- app/routes/overview/components/EventItem.js
+++ app/routes/overview/components/EventItem.js
@@ -9,6 +9,7 @@ import { Link } from 'react-router';
 import { Flex } from 'app/components/Layout';
 import { colorForEvent } from 'app/routes/events/utils';
 import styles from './EventItem.css';
+import moment from 'moment-timezone';
 
 type Props = {
   item: Event | Article,
@@ -20,7 +21,16 @@ class EventItem extends Component<Props, *> {
   render() {
     const { item, url, meta } = this.props;
     const TITLE_MAX_LENGTH = 50;
-    const { registrationCount, totalCapacity } = item;
+    const { registrationCount, totalCapacity, activationTime } = item;
+
+    // This value will less then 0 if activationTime has yet to come
+    const future = moment().diff(activationTime) < 0;
+    const info = future
+      ? `Åpner ${moment(activationTime).format('dddd D MMM HH:mm')}`
+      : totalCapacity == 0
+      ? 'Åpent arrangement'
+      : `${registrationCount}/${totalCapacity}`;
+
     return (
       <div className={styles.body}>
         <Link to={url} className={styles.link}>
@@ -29,13 +39,7 @@ class EventItem extends Component<Props, *> {
               {item.cover && (
                 <Image className={styles.image} src={item.cover} />
               )}
-              {registrationCount && totalCapacity ? (
-                <span className={styles.count}>
-                  {registrationCount}/{totalCapacity}
-                </span>
-              ) : (
-                <span className={styles.count}>Ingen påmelding</span>
-              )}
+              <span className={styles.info}>{info}</span>
             </Flex>
             <div
               className={styles.right}
