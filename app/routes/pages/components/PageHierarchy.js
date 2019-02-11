689083972abb5baa50d5cb8ca153e0ecbdfe1b31
Sigrid Marita Kvamme <sigridkvamme@gmail.com>
2019-02-11 20:26:05 +0100

    Make categories in sidebar collapsible (dropdown)

--- app/routes/pages/components/PageHierarchy.js
+++ app/routes/pages/components/PageHierarchy.js
@@ -1,9 +1,10 @@
 // @flow
 import { readmeIfy } from 'app/components/ReadmeLogo';
 
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router';
 import styles from './PageHierarchy.css';
+import Icon from 'app/components/Icon';
 
 export type HierarchyEntity = {
   title: string,
@@ -27,26 +28,47 @@ const HierarchySection = ({
   hierarchySection: HierarchySectionEntity,
   currentUrl: string
 }) => (
-  <ul className={styles.pageList}>
+  <nav className={styles.pageList}>
     {items.length > 0 && (
-      <li key="title">
-        <p className={styles.header}>{title}</p>
-      </li>
-    )}{' '}
-    {items.map((item, key) => (
-      <li key={key} className={styles.links}>
-        <Link
-          className={
-            item.url === currentUrl ? styles.selected : styles.nonSelected
-          }
-          to={item.url}
-        >
-          {readmeIfy(item.title)}
-        </Link>
-      </li>
-    ))}
-  </ul>
+      <AccordionContainer title={title}>
+        {items.map((item, key) => (
+          <div key={key} className={styles.links}>
+            <Link
+              className={
+                item.url === currentUrl ? styles.selected : styles.nonSelected
+              }
+              to={item.url}
+            >
+              {readmeIfy(item.title)}
+            </Link>
+          </div>
+        ))}
+      </AccordionContainer>
+    )}
+  </nav>
 );
+
+class AccordionContainer extends Component {
+  state = {
+    isOpen: true
+  };
+
+  handleClick = () => {
+    this.setState(state => ({ isOpen: !state.isOpen }));
+  };
+
+  render() {
+    const { title, children } = this.props;
+    return (
+      <div>
+        <button className={styles.dropdownBtn} onClick={this.handleClick}>
+          {title} <Icon className={styles.dropdownIcon} name="arrow-down" />
+        </button>{' '}
+        {this.state.isOpen ? <div>{children}</div> : null}
+      </div>
+    );
+  }
+}
 
 const PageHierarchy = ({ pageHierarchy, currentUrl }: Props) => {
   return (
