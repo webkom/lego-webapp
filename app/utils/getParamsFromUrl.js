b6f251c7cb200208911a39fa2f75bd50fc440bb7
Martin Farstad <farstad2k@gmail.com>
2019-03-12 10:45:42 +0100

    Fix edge case where video id is given in different format

--- app/utils/getParamsFromUrl.js
+++ app/utils/getParamsFromUrl.js
@@ -1,9 +1,20 @@
 // Convert params from url into JSON object
 export default function getParamsFromUrl(url) {
   const params = {};
-  const { searchParams } = new URL(url);
-  for (const [key, value] of searchParams.entries()) {
-    params[key] = value;
+  const urlObj = new URL(url);
+  const { searchParams, hostname } = urlObj;
+  if (hostname === 'youtu.be') {
+    const { pathname, search } = urlObj;
+    const videoId = pathname.substring(1);
+    const videoStartTime = search.substring(3);
+    params['v'] = videoId;
+    if (!isNaN(videoStartTime)) {
+      params['t'] = videoStartTime;
+    }
+  } else {
+    for (const [key, value] of searchParams.entries()) {
+      params[key] = value;
+    }
   }
   return params;
 }
