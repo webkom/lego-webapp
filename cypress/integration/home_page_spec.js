1059f1fd0223c2aadb1c923f32027e4fe7b75548
Orhan Hirsch <orhanhenrik@gmail.com>
2019-03-14 16:32:04 +0100

    Work on speeding up tests

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,6 +1,18 @@
-describe('The Home Page', function() {
-  it('successfully loads', function() {
+describe('The Home Page', () => {
+  it('successfully loads', () => {
     cy.visit('/');
-    cy.screenshot();
+    cy.contains('Velkommen');
+    cy.contains('Om Abakus');
+  });
+  it('can log in from homepage', () => {
+    cy.get('[name=username]').type('webkom');
+    cy.get('[name=password]').type('webkom{enter}');
+  });
+
+  it('can see upcoming bedpres and kurs', () => {
+    cy.contains('Bedpres og Kurs');
+  });
+  it('can see upcoming events', () => {
+    cy.contains('Arrangementer');
   });
 });
