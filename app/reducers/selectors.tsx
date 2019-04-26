587cb709bf5197a56aed2d30eb2d98a1b0c97698
Peder Smith <smith.peder@gmail.com>
2019-04-26 19:58:12 +0200

    Delete all .tsx reducers

--- app/reducers/selectors.tsx
+++
@@ -1,4 +0,0 @@
-import { get } from 'lodash';
-
-export const selectPagination = (entityName, { queryString }) => state =>
-  get(state, [entityName, 'pagination', queryString, 'nextPage']) !== null;
