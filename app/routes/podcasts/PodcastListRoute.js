0a4c599615657035a1ee0c97e179950d788501ba
kotlarz <marius.kotlarz@gmail.com>
2019-08-24 16:21:50 +0200

    Replace SoundCloud token with a currently working token

--- app/routes/podcasts/PodcastListRoute.js
+++ app/routes/podcasts/PodcastListRoute.js
@@ -1,10 +1,17 @@
+// @flow
+
+import React, { Component } from 'react';
+import Podcast from './components/Podcast.js';
+import { Content } from 'app/components/Content';
+import NavigationTab, { NavigationLink } from 'app/components/NavigationTab';
+import Icon from 'app/components/Icon';
+import styles from './components/PodcastList.css';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 
 import { selectPodcasts } from 'app/reducers/podcasts';
 import { fetchPodcasts } from 'app/actions/PodcastAction';
 import prepare from 'app/utils/prepare';
-import PodcastList from './components/PodcastList';
 
 const mapStateToProps = (state, props) => {
   return {
@@ -12,6 +19,54 @@ const mapStateToProps = (state, props) => {
     actionGrant: state.podcasts.actionGrant
   };
 };
+
+type Props = {
+  podcasts: Array<Object>,
+  actionGrant: Array<String>
+};
+
+type State = {
+  items: number
+};
+
+class PodcastList extends Component<Props, State> {
+  state = {
+    items: 5
+  };
+
+  showMore = () => {
+    this.setState(state => ({ items: state.items + 5 }));
+  };
+
+  render() {
+    const { podcasts, actionGrant } = this.props;
+    const elements = podcasts
+      .map(podcast => {
+        return (
+          <Podcast key={podcast.id} {...podcast} actionGrant={actionGrant} />
+        );
+      })
+      .reverse()
+      .splice(0, this.state.items);
+
+    return (
+      <Content>
+        <NavigationTab title="Podcasts">
+          {actionGrant.includes('create') && (
+            <NavigationLink to="/podcasts/create">Legg til ny</NavigationLink>
+          )}
+        </NavigationTab>
+
+        {elements}
+        {podcasts.length > this.state.items && (
+          <div className={styles.showMore}>
+            <Icon onClick={this.showMore} size={40} name="arrow-dropdown" />
+          </div>
+        )}
+      </Content>
+    );
+  }
+}
 
 export default compose(
   prepare((props, dispatch) => dispatch(fetchPodcasts())),
