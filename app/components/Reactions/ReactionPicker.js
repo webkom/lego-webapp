e6f3c5ceaa97e827f45f39a6bc87f91cfeb7bc1d
kotlarz <marius.kotlarz@gmail.com>
2019-09-04 17:21:41 +0200

    Add a search function that correctly searches and sorts emojis

--- app/components/Reactions/ReactionPicker.js
+++ app/components/Reactions/ReactionPicker.js
@@ -2,6 +2,7 @@
 
 // $FlowFixMe
 import React, { useMemo, useState, useCallback } from 'react';
+import fuzzy from 'fuzzy';
 import type { EmojiEntity } from 'app/reducers/emojis';
 import type { ID } from 'app/models';
 import styles from './ReactionPicker.css';
@@ -19,6 +20,120 @@ type Props = {
   }) => Promise<*>,
   deleteReaction: ({ reactionId: ID, contentTarget: string }) => Promise<*>,
   contentTarget: string
+};
+
+const searchEmojis = (emojis, searchString) => {
+  const matchingEmojis = [];
+  let currentEmojis = emojis;
+
+  /*
+   * STEP 1
+   * First look for exact matches for searchString in shortCode.
+   *
+   * Examples (searchString = "car"):
+   *   - ":car:" (match = ":<car>:")
+   */
+  currentEmojis = currentEmojis.filter(emoji => {
+    if (`:${searchString}:` === emoji.shortCode.toLowerCase()) {
+      matchingEmojis.push(emoji);
+      return false;
+    }
+    {
+      return true;
+    }
+  });
+
+  /*
+   * STEP 2
+   * Then look if shortCode start with, ends with or contains the searchString in the middle.
+   *
+   * Examples (searchString = "car"):
+   *   - ":car_test:" (match = ":<car_>test:")
+   *   - ":test_car:" (match = ":test<_car>:")
+   *   - ":good_car_test:" (match = ":good<_car_>test:")
+   */
+  currentEmojis = currentEmojis.filter(emoji => {
+    const startsWithExact = emoji.shortCode
+      .toLowerCase()
+      .startsWith(`:${searchString}_`);
+    const endsWithExact = emoji.shortCode
+      .toLowerCase()
+      .endsWith(`_${searchString}:`);
+    const contains = emoji.shortCode
+      .toLowerCase()
+      .includes(`_${searchString}_`);
+    if (startsWithExact || endsWithExact || contains) {
+      matchingEmojis.push(emoji);
+      return false;
+    } else {
+      return true;
+    }
+  });
+
+  /*
+   * STEP 3
+   * Then look if shortCode start with, ends with or contains the searchString in the middle.
+   *
+   * Examples (searchString = "car"):
+   *   - ":car2:" (match = ":<car>2:")
+   *   - ":carrot:" (match = ":<car>rot")
+   *   - ":2car:" (match = ":2<car>:")
+   */
+  currentEmojis = currentEmojis.filter(emoji => {
+    const startsWith = emoji.shortCode
+      .toLowerCase()
+      .startsWith(`:${searchString}`);
+    const endsWith = emoji.shortCode.toLowerCase().endsWith(`${searchString}:`);
+    if (startsWith || endsWith) {
+      matchingEmojis.push(emoji);
+      return false;
+    } else {
+      return true;
+    }
+  });
+
+  /*
+   * STEP 3
+   * Look if the keywords for the emoji match exactly
+   *
+   * Examples (searchString = "vehicle"):
+   *   - ":taxi:" (match = "vehicle", keywords = ["uber", "vehicle", "cars", "transportation"])
+   *   - ":tractor:" (match = "vehicle", keywords = ["vehicle", "car", "farming", "argiculture"])
+   *   - ":oncoming_automobile:" (match = "vehicle", keywords = ["car", "vehicle", "transportation"])
+   */
+  currentEmojis = currentEmojis.filter(emoji => {
+    if (
+      emoji.keywords.find(keyword => keyword.toLowerCase() == searchString) !==
+      undefined
+    ) {
+      matchingEmojis.push(emoji);
+    } else {
+      return true;
+    }
+  });
+
+  /*
+   * STEP 4
+   * Perform a fuzzy search at the end for the remaining emojis.
+   *
+   * Examples (searchString = "car"):
+   *   - ":woman_cart_wheeling:" (match = ":woman_<car>t_wheeling:")
+   *   - ":costa_rica:" (match = ":<c>ost<a>_<r>ica")
+   *   - ":christmas_tree:" (match = ":<c>hristm<a>s_t<r>ee")
+   */
+  var results = fuzzy.filter(searchString, currentEmojis, {
+    pre: '<',
+    post: '>',
+    extract: emoji => {
+      return emoji.shortCode;
+    }
+  });
+
+  currentEmojis = results.forEach(result => {
+    matchingEmojis.push(result.original);
+  });
+
+  return matchingEmojis;
 };
 
 const ReactionPicker = ({
@@ -58,14 +173,7 @@ const ReactionPicker = ({
     if (searchString === null || searchString === '') {
       return null;
     }
-    return emojis.filter(emoji => {
-      const idMatches = emoji.shortCode.toLowerCase().includes(searchString);
-      const keywordsMatches =
-        emoji.keywords.filter(keyword =>
-          keyword.toLowerCase().includes(searchString)
-        ).length > 0;
-      return idMatches || keywordsMatches;
-    });
+    return searchEmojis(emojis, searchString);
   }, [searchString, emojis]);
 
   const onCategoryClick = useCallback(category => {
