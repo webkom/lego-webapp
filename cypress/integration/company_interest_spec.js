3eb75ab6e350b311e43f0ff2b82b7c3b2f52d934
Martin Farstad <farstad2k@gmail.com>
2019-09-25 22:47:28 +0200

    Add cypress tests for company interest create, delete and edit

---
+++ cypress/integration/company_interest_spec.js
@@ -0,0 +1,86 @@
+import { field } from '../support/utils.js';
+
+const createCompanyInterest = () => {
+  cy.visit('/interesse');
+
+  field('companyName')
+    .click()
+    .type('webkom consulting');
+
+  field('contactPerson')
+    .click()
+    .type('webkom');
+
+  field('mail')
+    .click()
+    .type('webkom@webkom.no');
+
+  field('semesters[0].checked').check();
+  field('events[0].checked').check();
+  field('otherOffers[0].checked').check();
+
+  field('comment').type('random comment');
+
+  cy.contains('Opprett bedriftsinteresse').click();
+
+  cy.url().should('include', `/companyInterest`);
+};
+
+describe('Create company interest', () => {
+  beforeEach(() => {
+    cy.resetDb();
+    cy.cachedLogin();
+  });
+
+  it('should be able to create and delete interest', () => {
+    createCompanyInterest();
+
+    cy.contains('webkom');
+    cy.contains('webkom@webkom.no');
+    cy.contains('Slett')
+      .click()
+      .click();
+
+    cy.should('not.contain', 'webkom');
+  });
+
+  it('should not be able to create if invalid input', () => {
+    cy.visit('/interesse');
+
+    field('companyName')
+      .click()
+      .type('webkom consulting');
+
+    field('contactPerson')
+      .click()
+      .type('webkom');
+
+    field('mail')
+      .click()
+      .type('webkom@webko');
+
+    field('comment').type('random comment');
+
+    cy.contains('Opprett bedriftsinteresse').click();
+
+    cy.url().should('not.include', `/companyInterest`);
+  });
+
+  it('should be able to edit company interest', () => {
+    createCompanyInterest();
+    cy.contains('webkom consulting').click();
+    cy.url().should('include', `edit`);
+    cy.contains('webkom');
+    cy.contains('webkom@webkom.no');
+    cy.contains('random comment');
+
+    field('companyName').type('plebkom');
+
+    field('semesters[0].checked').should('have.attr', 'checked');
+    field('events[0].checked').should('have.attr', 'checked');
+    field('otherOffers[0].checked').should('have.attr', 'checked');
+
+    cy.contains('Oppdater bedriftsinteresse').click();
+    cy.contains('plebkom');
+  });
+});
