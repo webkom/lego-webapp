ac9930c3fdcf698576624d73b173bf8eec0887ae
karolbon <karolbon@stud.ntnu.no>
2019-11-06 19:50:30 +0100

    Add empty select

--- app/routes/surveys/components/Submissions/Results.js
+++ app/routes/surveys/components/Submissions/Results.js
@@ -3,9 +3,13 @@
 import React from 'react';
 import styles from '../surveys.css';
 import type { SurveyEntity, QuestionEntity } from 'app/reducers/surveys';
+import { SelectInput, legoForm } from 'app/components/Form';
+import { Field } from 'redux-form';
+
 import { QuestionTypes, CHART_COLORS } from '../../utils';
 import InfoBubble from 'app/components/InfoBubble';
 import { VictoryPie, VictoryTheme } from 'victory';
+import { createValidator, required } from 'app/utils/validation';
 
 type Props = {
   survey: SurveyEntity,
@@ -67,77 +71,102 @@ const Results = ({
 
   return (
     <div>
-      <div className={styles.eventSummary}>
-        <h3>Arrangementet</h3>
-        <div className={styles.infoBubbles}>
-          <EventData info={info} />
+      <form>
+        <div className={styles.eventSummary}>
+          <h3>Arrangementet</h3>
+          <div className={styles.infoBubbles}>
+            <EventData info={info} />
+          </div>
         </div>
-      </div>
 
-      <ul className={styles.summary}>
-        {survey.questions.map(question => {
-          const colorsToRemove = [];
-          const pieData = graphData[question.id].filter((dataPoint, i) => {
-            if (dataPoint.selections === 0) {
-              colorsToRemove.push(i);
-              return false;
-            }
-            return true;
-          });
-          const pieColors = CHART_COLORS.filter(
-            (color, i) => !colorsToRemove.includes(i)
-          );
-          const labelRadius = pieData.length === 1 ? -10 : 60;
+        <ul className={styles.summary}>
+          {survey.questions.map(question => {
+            const colorsToRemove = [];
+            const pieData = graphData[question.id].filter((dataPoint, i) => {
+              if (dataPoint.selections === 0) {
+                colorsToRemove.push(i);
+                return false;
+              }
+              return true;
+            });
+            const pieColors = CHART_COLORS.filter(
+              (color, i) => !colorsToRemove.includes(i)
+            );
+            const labelRadius = pieData.length === 1 ? -10 : 60;
 
-          return (
-            <li key={question.id}>
-              <h3>{question.questionText}</h3>
+            return (
+              <li key={question.id}>
+                <h3>{question.questionText}</h3>
 
-              {question.questionType === QuestionTypes('text') ? (
-                <ul className={styles.textAnswers}>
-                  {generateTextAnswers(question)}
-                </ul>
-              ) : (
-                <div className={styles.questionResults}>
-                  <div style={{ width: '300px' }}>
-                    <VictoryPie
-                      data={pieData}
-                      x="option"
-                      y="selections"
-                      theme={VictoryTheme.material}
-                      colorScale={pieColors}
-                      labels={d => d.y}
-                      labelRadius={labelRadius}
-                      padding={{ left: 0, top: 40, right: 30, bottom: 30 }}
-                      style={{
-                        labels: { fill: 'white', fontSize: 20 }
-                      }}
-                    />
-                  </div>
-
-                  <ul className={styles.graphData}>
-                    {graphData[question.id].map((dataPoint, i) => (
-                      <li key={i}>
-                        <span
-                          className={styles.colorBox}
-                          style={{ backgroundColor: CHART_COLORS[i] }}
-                        >
-                          &nbsp;
-                        </span>
-                        <span style={{ marginTop: '-5px' }}>
-                          {dataPoint.option}
-                        </span>
-                      </li>
-                    ))}
+                {question.questionType === QuestionTypes('text') ? (
+                  <ul className={styles.textAnswers}>
+                    {generateTextAnswers(question)}
                   </ul>
-                </div>
-              )}
-            </li>
-          );
-        })}
-      </ul>
+                ) : (
+                  <div className={styles.questionResults}>
+                    <Field
+                      name={`testquestionType`}
+                      simpleValue
+                      component={SelectInput.Field}
+                      options={[]}
+                      clearable={false}
+                      backspaceRemoves={false}
+                      searchable={false}
+                    />
+
+                    <div style={{ width: '300px' }}>
+                      <VictoryPie
+                        data={pieData}
+                        x="option"
+                        y="selections"
+                        theme={VictoryTheme.material}
+                        colorScale={pieColors}
+                        labels={d => d.y}
+                        labelRadius={labelRadius}
+                        padding={{ left: 0, top: 40, right: 30, bottom: 30 }}
+                        style={{
+                          labels: { fill: 'white', fontSize: 20 }
+                        }}
+                      />
+                    </div>
+
+                    <ul className={styles.graphData}>
+                      {graphData[question.id].map((dataPoint, i) => (
+                        <li key={i}>
+                          <span
+                            className={styles.colorBox}
+                            style={{ backgroundColor: CHART_COLORS[i] }}
+                          >
+                            &nbsp;
+                          </span>
+                          <span style={{ marginTop: '-5px' }}>
+                            {dataPoint.option}
+                          </span>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+              </li>
+            );
+          })}
+        </ul>
+      </form>
     </div>
   );
 };
 
-export default Results;
+const validate = createValidator({
+  title: [required()],
+  event: [required()]
+});
+
+const onSubmit = (formContent: Object, dispatch, props: Props) => {
+  console.log(props);
+};
+
+export default legoForm({
+  form: 'surveyEditor',
+  validate,
+  onSubmit
+})(Results);
