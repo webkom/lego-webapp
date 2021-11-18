027a53457b6135df2a81ed24c9d917287bac0b7b
Mithunan Sivakumar <mithunan2522@gmail.com>
2021-11-18 18:35:11 +0100

    Removed everything related to redux in announcementinline component"

--- app/components/AnnouncementInLine/index.js
+++ app/components/AnnouncementInLine/index.js
@@ -1,13 +1,7 @@
 // @flow
 import { Link } from 'react-router-dom';
-import { compose } from 'redux';
-import { connect } from 'react-redux';
-import cx from 'classnames';
 import { Component } from 'react';
 import Button from 'app/components/Button';
-import { Form, TextArea } from 'app/components/Form';
-import { reduxForm, Field, reset } from 'redux-form';
-import { createAnnouncement } from 'app/actions/AnnouncementsActions';
 import styles from './AnnouncementInLine.css';
 import type { ID } from 'app/models';
 
@@ -24,42 +18,19 @@ type Props = {
   className?: string,
 };
 
-type State = {
-  hidden: boolean,
-  sent: boolean,
-};
-
-class AnnouncementInLine extends Component<Props, State> {
-  state = {
-    hidden: true,
-    sent: false,
-  };
-
+class AnnouncementInLine extends Component<Props> {
   render() {
-    const {
-      actionGrant,
-      handleSubmit,
-      placeholder,
-      event,
-      meeting,
-      group,
-      button,
-      className,
-    } = this.props;
-
-    const showButton = button && this.state.hidden && !this.state.sent;
+    const { actionGrant, event, meeting, group } = this.props;
 
     return (
       <div>
         {actionGrant && (event || meeting || group) && (
           <div>
-            {showButton && (
-              <Link to={'/announcements'} state={{ event, meeting }}>
-                <Button className={styles.announcementButton}>
-                  Ny kunngjøring
-                </Button>
-              </Link>
-            )}
+            <Link to={'/announcements'} state={{ event, meeting }}>
+              <Button className={styles.announcementButton}>
+                Ny kunngjøring
+              </Button>
+            </Link>
           </div>
         )}
       </div>
@@ -67,27 +38,4 @@ class AnnouncementInLine extends Component<Props, State> {
   }
 }
 
-const resetForm = (result, dispatch) => {
-  dispatch(reset('announcementInline'));
-};
-
-const mapStateToProps = (state) => ({
-  actionGrant: state.allowed.announcements,
-});
-
-const mapDispatchToProps = { createAnnouncement };
-
-export default compose(
-  connect(mapStateToProps, mapDispatchToProps),
-  reduxForm({
-    form: 'announcementInline',
-    onSubmitSuccess: resetForm,
-    validate(values) {
-      const errors = {};
-      if (!values.message) {
-        errors.message = 'Du må skrive en melding';
-      }
-      return errors;
-    },
-  })
-)(AnnouncementInLine);
+export default AnnouncementInLine;
