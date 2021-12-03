26005c3612aab1089fb13282b3d9468e50b4edb6
erlingfn <erling.nesset@gmail.com>
2021-12-03 17:49:27 +0100

    Remove immutable-js dependency

--- config/webpack.server.js
+++ config/webpack.server.js
@@ -64,7 +64,6 @@ module.exports = (env, argv) => {
         lodash: 'node_modules/lodash-es',
         'moment-timezone':
           'moment-timezone/builds/moment-timezone-with-data-2012-2022.min',
-        immutable: 'node_modules/immutable',
       },
     },
 
