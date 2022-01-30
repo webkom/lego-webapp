18a6283b0896b650145325b7f1160d0d6507b7b0
Peder Smith <smith.peder@gmail.com>
2022-01-30 16:26:22 +0100

    Add qoute preview

--- app/routes/quotes/QuoteEditorRoute.js
+++ app/routes/quotes/QuoteEditorRoute.js
@@ -4,10 +4,14 @@ import { addQuotes } from '../../actions/QuoteActions';
 import AddQuote from './components/AddQuote';
 import { LoginPage } from 'app/components/LoginForm';
 import replaceUnlessLoggedIn from 'app/utils/replaceUnlessLoggedIn';
+import { formValueSelector } from 'redux-form';
 
 const mapStateToProps = (state, props) => {
+  const valueSelector = formValueSelector('addQuote');
   return {
     actionGrant: state.quotes.actionGrant,
+    text: valueSelector(state, 'text'),
+    source: valueSelector(state, 'source'),
   };
 };
 
