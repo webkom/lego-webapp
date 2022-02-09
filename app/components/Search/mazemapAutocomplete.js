3dbfc443f3ea73c4854450030ececb80609a3bdd
Olav Kihle <olavki@stud.ntnu.no>
2022-02-09 12:02:41 +0100

    Fix ssr bug in autocomplete field

--- app/components/Search/mazemapAutocomplete.js
+++ app/components/Search/mazemapAutocomplete.js
@@ -1,10 +1,9 @@
 // @flow
 
-import { Component, type ComponentType } from 'react';
+import { Component,  type ComponentType } from 'react';
 import { debounce } from 'lodash';
 import { stripHtmlTags } from './utils.js';
 import 'node_modules/mazemap/mazemap.min.css';
-import * as Mazemap from 'mazemap';
 
 type InjectedProps = {
   mazemapSearch: (query: string) => Promise<*>,
@@ -24,16 +23,6 @@ function mazemapAutocomplete<Props>({
   const displayName =
     WrappedComponent.displayName || WrappedComponent.name || 'Unknown';
 
-  const mazemapSearchController = new Mazemap.Search.SearchController({
-    campusid: 1,
-    rows: 10,
-    withpois: true,
-    withbuilding: false,
-    withtype: false,
-    withcampus: false,
-    resultsFormat: 'geojson',
-  });
-
   const mapRoomAndBuildingToResult = (
     poiName: string,
     buildingName: string,
@@ -51,67 +40,64 @@ function mazemapAutocomplete<Props>({
     state = {
       searching: false,
       result: [], //first result fetched on mount
+      Mazemap: undefined,
     };
 
     _isMounted = false;
 
     componentDidMount() {
+      import('mazemap').then((mazemap) => this.setState({Mazemap: mazemap}));
       this._isMounted = true;
-      if (this.props.meta.initial) {
-        Mazemap.Data.getPoi(this.props.meta.initial.value).then((poi) =>
-          this.setState({
-            result: [
-              mapRoomAndBuildingToResult(
-                poi.properties.title,
-                poi.properties.buildingName,
-                this.props.meta.initial.value
-              ),
-            ],
-          })
-        );
-      }
     }
 
     componentWillUnmount() {
       this._isMounted = false;
     }
 
-    handleSearch = (query: string): void => {
-      if (!query) {
-        return;
-      }
-      this.setState({
-        searching: true,
-      });
-
-      mazemapSearchController
-        .search(query)
-        .then((results) => {
-          if (this._isMounted) {
-            this.setState({
-              result: results.results.features.map((result) =>
-                mapRoomAndBuildingToResult(
-                  result.properties.dispPoiNames[0],
-                  result.properties.dispBldNames[0],
-                  result.properties.poiId
-                )
-              ),
-            });
-          }
-        })
-        .finally(() => {
-          if (this._isMounted) {
-            this.setState({ searching: false });
-          }
-        });
-    };
-
     render() {
+      const {Mazemap} = this.state;
+      const handleSearch = (query: string): void => {
+        if (!query || !Mazemap) {
+          return;
+        }
+        const mazemapSearchController = new Mazemap.Search.SearchController({
+          campusid: 1,
+          rows: 10,
+          withpois: true,
+          withbuilding: false,
+          withtype: false,
+          withcampus: false,
+          resultsFormat: 'geojson',
+        });
+        this.setState({
+          searching: true,
+        });
+        mazemapSearchController
+          .search(query)
+          .then((results) => {
+            if (this._isMounted) {
+              this.setState({
+                result: results.results.features.map((result) =>
+                  mapRoomAndBuildingToResult(
+                    result.properties.dispPoiNames[0],
+                    result.properties.dispBldNames[0],
+                    result.properties.poiId
+                  )
+                ),
+              });
+            }
+          })
+          .finally(() => {
+            if (this._isMounted) {
+              this.setState({ searching: false });
+            }
+          });
+      };
       return (
         <WrappedComponent
           {...this.props}
           options={this.state.result}
-          onSearch={debounce((query) => this.handleSearch(query), 300)}
+          onSearch={debounce((query) => handleSearch(query), 300)}
           fetching={this.state.searching}
         />
       );
