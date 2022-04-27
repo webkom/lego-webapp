263e12a8160d44c4c796376b30e49e1bfd4213d7
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2022-04-27 23:08:41 +0200

    import zxcvbn dynamically in PasswordStrengthMeter

--- app/routes/users/components/PasswordStrengthMeter.js
+++ app/routes/users/components/PasswordStrengthMeter.js
@@ -1,9 +1,7 @@
 // @flow
 
-import { Component, Fragment } from 'react';
-import { pick } from 'lodash';
+import loadable from '@loadable/component';
 import moment from 'moment-timezone';
-import zxcvbn from 'zxcvbn';
 import Bar from '@webkom/react-meter-bar';
 import '@webkom/react-meter-bar/dist/Bar.css';
 import styles from './PasswordStrengthMeter.css';
@@ -19,60 +17,82 @@ type Props = {
   user: UserEntity,
 };
 
-class PasswordStrengthMeter extends Component<Props> {
-  render() {
-    const { password, user } = this.props;
-    const zxcvbnValue = zxcvbn(
-      password,
-      //$FlowFixMe[incompatible-call]
-      Object.values(pick(user, ['username', 'firstName', 'lastName']))
-    );
-    let tips = zxcvbnValue.feedback.suggestions;
-    tips.push(zxcvbnValue.feedback.warning);
-    tips = tips.map((tip) => passwordFeedbackMessages[tip]).filter(Boolean);
+const Zxcvbn = loadable.lib(() => import('zxcvbn'), { ssr: false });
 
-    const crackTimeSec = Number(
-      zxcvbnValue.crack_times_seconds.offline_slow_hashing_1e4_per_second
-    );
-    const crackTimeDuration = moment
-      .duration(crackTimeSec, 'seconds')
-      .humanize();
-    const crackTime =
-      crackTimeSec > 2 ? crackTimeDuration : crackTimeSec + ' sekunder';
+const PasswordStrengthMeter = ({ password, user }: Props) => {
+  return (
+    <Zxcvbn
+      fallback={
+        <>
+          <PasswordStrengthBar strengthScore={0} />
+          <span>
+            <strong>Kalkulerer passordstyrke</strong>
+          </span>
+        </>
+      }
+    >
+      {({ default: zxcvbn }) => {
+        const zxcvbnValue = zxcvbn(password, [
+          user.username,
+          user.firstName,
+          user.lastName,
+        ]);
 
-    return (
-      <Fragment>
-        <div className={styles.removeLabels}>
-          <Bar
-            labels={[1, 2, 3, 4, 5]}
-            labelColor="#000"
-            progress={zxcvbnValue.score * 25}
-            barColor={barColor[zxcvbnValue.score]}
-            seperatorColor="#fff"
-          />
-        </div>
-        {password && (
-          <Fragment>
-            <span>
-              <strong>Passordstyrke: </strong>{' '}
-              {passwordLabel[zxcvbnValue.score]}
-            </span>
-            <p>
-              Dette passordet hadde tatt en maskin {crackTime} å knekke @ 10^4
-              Hash/s.
-            </p>
-          </Fragment>
-        )}
-        {password && zxcvbnValue.score < 3 && (
-          <ul className={styles.tipsList}>
-            {tips.map((value, key) => {
-              return <li key={key}>{value}</li>;
-            })}
-          </ul>
-        )}
-      </Fragment>
-    );
-  }
-}
+        let tips = zxcvbnValue.feedback?.suggestions ?? [];
+        tips.push(zxcvbnValue.feedback?.warning);
+        tips = tips.map((tip) => passwordFeedbackMessages[tip]).filter(Boolean);
+
+        const crackTimeSec = Number(
+          zxcvbnValue.crack_times_seconds?.offline_slow_hashing_1e4_per_second
+        );
+        const crackTimeDuration = moment
+          .duration(crackTimeSec, 'seconds')
+          .humanize();
+
+        const crackTime =
+          crackTimeSec > 2 ? crackTimeDuration : crackTimeSec + ' sekunder';
+
+        return (
+          <>
+            <PasswordStrengthBar strengthScore={zxcvbnValue.score} />
+            {password && (
+              <>
+                <span>
+                  <strong>Passordstyrke: </strong>{' '}
+                  {passwordLabel[zxcvbnValue.score]}
+                </span>
+                <p>
+                  Dette passordet hadde tatt en maskin {crackTime} å knekke @
+                  10⁴ Hash/s.
+                </p>
+              </>
+            )}
+            {password && zxcvbnValue.score < 3 && (
+              <ul className={styles.tipsList}>
+                {tips.map((value, key) => {
+                  return <li key={key}>{value}</li>;
+                })}
+              </ul>
+            )}
+          </>
+        );
+      }}
+    </Zxcvbn>
+  );
+};
+
+const PasswordStrengthBar = ({ strengthScore }: { strengthScore: number }) => {
+  return (
+    <div className={styles.removeLabels}>
+      <Bar
+        labels={[1, 2, 3, 4, 5]}
+        labelColor="#000"
+        progress={strengthScore * 25}
+        barColor={barColor[strengthScore]}
+        seperatorColor="#fff"
+      />
+    </div>
+  );
+};
 
 export default PasswordStrengthMeter;
