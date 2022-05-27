c34b831a98b3c0530d269694fa9b5df381dadd23
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2022-05-27 15:27:19 +0200

    fix AnnouncementInLine (flow++)

--- app/components/AnnouncementInLine/index.js
+++ app/components/AnnouncementInLine/index.js
@@ -1,41 +1,37 @@
 // @flow
 import { Link } from 'react-router-dom';
-import { Component } from 'react';
 import Button from 'app/components/Button';
 import styles from './AnnouncementInLine.css';
 import type { ID } from 'app/models';
+import { useSelector } from 'react-redux';
 
 type Props = {
-  placeholder?: string,
   event?: ID,
   meeting?: ID,
   group?: ID,
-  createAnnouncement: (CreateAnnouncement) => Promise<*>,
-  handleSubmit: (Function) => void,
-  actionGrant: boolean,
-  hidden?: boolean,
-  button?: boolean,
-  className?: string,
 };
 
-class AnnouncementInLine extends Component<Props> {
-  render() {
-    const { actionGrant, event, meeting, group } = this.props;
+const AnnouncementInLine = ({ event, meeting, group }: Props) => {
+  const actionGrant = useSelector((state) => state.allowed.announcements);
 
-    return (
-      <div>
-        {actionGrant && (event || meeting || group) && (
-          <div>
-            <Link to={'/announcements'} state={{ event, meeting }}>
-              <Button className={styles.announcementButton}>
-                Ny kunngjøring
-              </Button>
-            </Link>
-          </div>
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      {actionGrant && (event || meeting || group) && (
+        <div>
+          <Link
+            to={{
+              pathname: '/announcements',
+              state: { event, meeting, group },
+            }}
+          >
+            <Button className={styles.announcementButton}>
+              Ny kunngjøring
+            </Button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default AnnouncementInLine;
