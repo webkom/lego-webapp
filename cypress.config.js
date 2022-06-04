49507e2dbd900e90fa9998417e4a9ea111821067
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2022-06-04 15:47:57 +0200

    Remove import of empty plugin

--- cypress.config.js
+++ cypress.config.js
@@ -12,11 +12,6 @@ module.exports = defineConfig({
     openMode: 0,
   },
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config);
-    },
     baseUrl: 'http://localhost:3000',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
