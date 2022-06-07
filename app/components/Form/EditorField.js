85025a9ca05a3323a279d121d9888e836912ef48
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2022-06-07 13:36:44 +0200

    replace react-redux connect() with useDispatch hook

--- app/components/Form/EditorField.js
+++ app/components/Form/EditorField.js
@@ -1,5 +1,5 @@
 // @flow
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import cx from 'classnames';
 import Editor from '@webkom/lego-editor';
 import '@webkom/lego-editor/dist/style.css';
@@ -15,7 +15,6 @@ type Props = {
   meta: any,
   name: string,
   initialized: boolean,
-  uploadFile: (file: Blob) => Promise<*>,
 };
 
 class NoSSRError {
@@ -25,30 +24,23 @@ class NoSSRError {
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    uploadFile: async (file) => {
-      const response = await dispatch(uploadFile({ file, isPublic: true }));
-      return { fileKey: response.meta.fileKey };
-    },
-  };
-};
-
 /*
  * The reason for the initialized prop is an issue(https://github.com/redux-form/redux-form/issues/621) in redux form that causes all fields to be initially rendered with an empty string as value,
  * due to the form not being initialized. Because the editor state is immutable, the editor field does not update once it is passed the correct initial value on the second render.
  * The initialized prop "solves" the issue by enabling the editor field to only render once the form has been initialized.
  */
-const EditorFieldComponent = ({
-  className,
-  name,
-  initialized,
-  uploadFile,
-  ...props
-}: Props) => {
+const EditorField = ({ className, name, initialized, ...props }: Props) => {
   if (!__CLIENT__) {
     throw new NoSSRError('Cannot SSR editor');
   }
+
+  const dispatch = useDispatch();
+
+  const imageUpload = async (file: File) => {
+    const response = await dispatch(uploadFile({ file, isPublic: true }));
+    return { fileKey: response.meta.fileKey };
+  };
+
   return (
     <div name={name}>
       {initialized && (
@@ -57,25 +49,19 @@ const EditorFieldComponent = ({
           {...props}
           {...props.input}
           {...props.meta}
-          imageUpload={uploadFile}
+          imageUpload={imageUpload}
         />
       )}
     </div>
   );
 };
 
-const EditorField = connect(null, mapDispatchToProps)(EditorFieldComponent);
-
 EditorField.AutoInitialized = (props) =>
-  EditorFieldComponent({
+  EditorField({
     initialized: !!(props.value || props.input?.value),
     ...props,
   });
 
-// $FlowFixMe
-EditorField.Field = connect(
-  null,
-  mapDispatchToProps
-)(createField(EditorField.AutoInitialized, { noLabel: true }));
+EditorField.Field = createField(EditorField.AutoInitialized, { noLabel: true });
 
 export default EditorField;
