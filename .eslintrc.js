8ffc3aae262265f0fdb3bd6094aeaff5f063bc60
Ivar Nakken <ivarnakken1441@gmail.com>
2022-07-08 15:59:28 +0200

    Enforce import sorting according to eslint guidelines
    Employing simple-import-sort, but overriding ruleset to match our standards.
    
    Rules have increased severity and are auto-fixable through a `--fix` flag, e.g. `yarn lint:js --fix`.

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,6 +39,7 @@ module.exports = {
   plugins: [
     'cypress',
     'import',
+    'simple-import-sort',
     'jsx-a11y',
     'promise',
     'react',
@@ -47,12 +48,57 @@ module.exports = {
   ],
   ignorePatterns: ['mazemap/mazemap.min.*', '**/vendor/*.js'],
   rules: {
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
     'react-app/react/react-in-jsx-scope': 'off',
     'jest/valid-describe': 'off', // valid-describe was replaced by valid-describe-callback, but still needs its rule ...
     'jest/valid-describe-callback': 'error',
   },
+  overrides: [
+    {
+      files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+      rules: {
+        'simple-import-sort/imports': [
+          'error',
+          {
+            /**
+             * Each `import` is matched against all regexes on the `from` string. The import ends up
+             * in the group with the longest match. In case of a tie, the first matching group wins.
+             * Read more here: https://github.com/lydell/eslint-plugin-simple-import-sort#custom-grouping
+             */
+            groups: [
+              // Node.js builtins.
+              [
+                '^(assert|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|https|module|net|os|path|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|tty|url|util|vm|zlib|freelist|v8|process|async_hooks|http2|perf_hooks)(/.*|$)',
+              ],
+              // Packages.`react` related packages come first.
+              ['^react', '^@?\\w'],
+              [
+                // Internal packages.
+                '^(app|config|mazemap|schema|server)(/.*|$)',
+                // Side effect imports.
+                '^\\u0000',
+                // Parent imports. Put `..` last.
+                '^\\.\\.(?!/?$)',
+                '^\\.\\./?$',
+                // Other relative imports. Put same-folder imports and `.` last.
+                '^\\./(?=.*/)(?!/?$)',
+                '^\\.(?!/?$)',
+                '^\\./?$',
+              ],
+              // Style imports.
+              ['^.+\\.s?css$'],
+            ],
+          },
+        ],
+      },
+    },
+  ],
   settings: {
     'import/resolver': {
       node: {
