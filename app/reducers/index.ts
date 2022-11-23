caf7487b99aef2bfe40bfeef7eeb4c5e06cec476
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2022-11-23 10:50:44 +0100

    remove unnecessary schema typecasting

--- app/reducers/index.ts
+++ app/reducers/index.ts
@@ -2,81 +2,73 @@ import { schema } from 'normalizr';
 import { followersKeyGen } from './followers';
 import { getMeetingInvitationId } from './meetingInvitations';
 
-import type { Schema } from 'normalizr';
-
-export const restrictedMailSchema = new schema.Entity(
-  'restrictedMails'
-) as Schema as Schema;
-export const groupSchema = new schema.Entity('groups') as Schema;
-export const penaltySchema = new schema.Entity('penalties', {}) as Schema;
+export const restrictedMailSchema = new schema.Entity('restrictedMails');
+export const groupSchema = new schema.Entity('groups');
+export const penaltySchema = new schema.Entity('penalties', {});
 export const userSchema = new schema.Entity('users', {
   abakusGroups: [groupSchema],
   penalties: [penaltySchema],
-}) as Schema;
+});
 export const emailUserSchema = new schema.Entity('emailUsers', {
   user: userSchema,
-}) as Schema;
+});
 export const emailListSchema = new schema.Entity('emailLists', {
   users: [userSchema],
   groups: [groupSchema],
-}) as Schema;
+});
 export const registrationSchema = new schema.Entity('registrations', {
   user: userSchema,
-}) as Schema;
+});
 export const poolSchema = new schema.Entity('pools', {
   registrations: [registrationSchema],
-}) as Schema;
-export const commentSchema = new schema.Entity('comments') as Schema;
+});
+export const commentSchema = new schema.Entity('comments');
 export const eventSchema = new schema.Entity('events', {
   pools: [poolSchema],
   comments: [commentSchema],
   waitingRegistrations: [registrationSchema],
   pendingRegistration: registrationSchema,
-}) as Schema;
+});
 export const eventAdministrateSchema = new schema.Entity('events', {
   pools: [poolSchema],
   unregistered: [registrationSchema],
   waitingRegistrations: [registrationSchema],
-}) as Schema;
-export const reactionSchema = new schema.Entity('reactions') as Schema;
+});
+export const reactionSchema = new schema.Entity('reactions');
 export const articleSchema = new schema.Entity('articles', {
   comments: [commentSchema],
   reactions: [reactionSchema],
   author: userSchema,
-}) as Schema;
+});
 export const galleryPictureSchema = new schema.Entity('galleryPictures', {
   comments: [commentSchema],
-}) as Schema;
-export const gallerySchema = new schema.Entity('galleries') as Schema;
-export const quoteSchema = new schema.Entity('quotes') as Schema;
-export const pollSchema = new schema.Entity('polls') as Schema;
+});
+export const gallerySchema = new schema.Entity('galleries');
+export const quoteSchema = new schema.Entity('quotes');
+export const pollSchema = new schema.Entity('polls');
 export const pageSchema = new schema.Entity(
   'pages',
   {},
   {
     idAttribute: 'slug',
   }
-) as Schema;
-export const companySemesterSchema = new schema.Entity(
-  'companySemesters'
-) as Schema;
+);
+export const companySemesterSchema = new schema.Entity('companySemesters');
 export const companyInterestSchema = new schema.Entity('companyInterest', {
   semesters: [companySemesterSchema],
-}) as Schema;
+});
 export const companySchema = new schema.Entity('companies', {
   studentContact: userSchema,
   comments: [commentSchema],
-}) as Schema;
-export const joblistingsSchema = new schema.Entity('joblistings') as Schema;
-export const announcementsSchema = new schema.Entity('announcements') as Schema;
-export const feedActivitySchema = new schema.Entity('feedActivities') as Schema;
-export const oauth2ApplicationSchema = new schema.Entity(
-  'oauth2Application'
-) as Schema;
-export const oauth2GrantSchema = new schema.Entity('oauth2Grant') as Schema;
+});
+export const joblistingsSchema = new schema.Entity('joblistings');
+export const announcementsSchema = new schema.Entity('announcements');
+export const feedActivitySchema = new schema.Entity('feedActivities');
+export const oauth2ApplicationSchema = new schema.Entity('oauth2Application');
+export const oauth2GrantSchema = new schema.Entity('oauth2Grant');
 export const membershipSchema = new schema.Entity('memberships', {
   user: userSchema,
-}) as Schema;
+});
 export const meetingInvitationSchema = new schema.Entity(
   'meetingInvitations',
   {
@@ -86,50 +78,50 @@ export const meetingInvitationSchema = new schema.Entity(
     idAttribute: (invite) =>
       getMeetingInvitationId(invite.meeting, invite.user.username),
   }
-) as Schema;
+);
 export const meetingSchema = new schema.Entity('meetings', {
   invitations: [meetingInvitationSchema],
   reportAuthor: userSchema,
   createdBy: userSchema,
   comments: [commentSchema],
-}) as Schema;
+});
 export const frontpageSchema = new schema.Entity('frontpage', {
   events: [eventSchema],
   articles: [articleSchema],
   poll: pollSchema,
-}) as Schema;
+});
 export const emojiSchema = new schema.Entity(
   'emojis',
   {},
   {
     idAttribute: 'shortCode',
   }
-) as Schema;
+);
 export const surveySchema = new schema.Entity('surveys', {
   event: eventSchema,
-}) as Schema;
+});
 export const surveySubmissionSchema = new schema.Entity('surveySubmissions', {
   user: userSchema,
-}) as Schema;
+});
 export const tagSchema = new schema.Entity(
   'tags',
   {},
   {
     idAttribute: 'tag',
   }
-) as Schema;
+);
 export const followersEventSchema = new schema.Entity(
   followersKeyGen('event'),
   {
     follower: userSchema,
   }
-) as Schema;
+);
 export const followersCompanySchema = new schema.Entity(
   followersKeyGen('company'),
   {
     follower: userSchema,
   }
-) as Schema;
+);
 export const followersUserSchema = new schema.Entity(followersKeyGen('user'), {
   follower: userSchema,
-}) as Schema;
+});
