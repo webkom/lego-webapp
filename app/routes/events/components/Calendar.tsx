0c4ee655a12a42f604c8ded9f12270baaf276ced
haukkagu <falk.bruskeland@gmail.com>
2023-01-24 19:04:09 +0100

    Improve styling of the calendar

--- app/routes/events/components/Calendar.tsx
+++ app/routes/events/components/Calendar.tsx
@@ -1,6 +1,6 @@
-import { Component } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Link } from 'react-router-dom';
+import Icon from 'app/components/Icon';
 import type { ActionGrant, IcalToken } from 'app/models';
 import createMonthlyCalendar from 'app/utils/createMonthlyCalendar';
 import styles from './Calendar.css';
@@ -9,55 +9,58 @@ import EventFooter from './EventFooter';
 import Toolbar from './Toolbar';
 
 const WEEKDAYS = ['Man', 'Tir', 'Ons', 'Tor', 'Fre', 'Lør', 'Søn'];
+
+const pathForPrevMonth = (date: moment.Moment) => {
+  const prevMonth = date.clone().subtract(1, 'months');
+  return `${prevMonth.year()}/${prevMonth.month() + 1}`;
+};
+
+const pathForNextMonth = (date: moment.Moment) => {
+  const nextMonth = date.clone().add(1, 'months');
+  return `${nextMonth.year()}/${nextMonth.month() + 1}`;
+};
+
 type Props = {
   weekOffset: number;
-  date: moment$Moment;
+  date: moment.Moment;
   icalToken: IcalToken;
   actionGrant: ActionGrant;
 };
 
-function pathForPrevMonth(date: moment$Moment) {
-  const newDate = date.clone().subtract(1, 'months');
-  return `${newDate.year()}/${newDate.month() + 1}`;
-}
+const Calendar = (props: Props) => {
+  const { actionGrant, date, icalToken, weekOffset } = props;
+  return (
+    <div className={styles.root}>
+      <Helmet title="Kalender" />
+      <Toolbar actionGrant={actionGrant} />
 
-function pathForNextMonth(date: moment$Moment) {
-  const newDate = date.clone().add(1, 'months');
-  return `${newDate.year()}/${newDate.month() + 1}`;
-}
+      <h2 className={styles.header}>
+        <Link to={`/events/calendar/${pathForPrevMonth(date)}`}>
+          <Icon name="arrow-back" />
+        </Link>
+        <span>{date.format('MMMM YYYY')}</span>
+        <Link to={`/events/calendar/${pathForNextMonth(date)}`}>
+          <Icon name="arrow-forward" />
+        </Link>
+      </h2>
 
-export default class Calendar extends Component<Props> {
-  static defaultProps = {
-    weekOffset: 0,
-  };
-
-  render() {
-    const { actionGrant, date, icalToken } = this.props;
-    return (
-      <div className={styles.root}>
-        <Helmet title="Kalender" />
-        <Toolbar actionGrant={actionGrant} />
-
-        <h2 className={styles.header}>
-          <Link to={`/events/calendar/${pathForPrevMonth(date)}`}>«</Link>
-          <span>{date.format('MMMM YYYY')}</span>
-          <Link to={`/events/calendar/${pathForNextMonth(date)}`}>»</Link>
-        </h2>
-
-        <div className={styles.grid}>
-          {WEEKDAYS.map((d) => (
-            <div key={d} className={styles.headingItem}>
-              {d}
-            </div>
-          ))}
-          {createMonthlyCalendar(date, this.props.weekOffset).map(
-            (dateProps, i) => (
-              <CalendarCell key={i} {...(dateProps as Record<string, any>)} />
-            )
-          )}
-        </div>
-        <EventFooter icalToken={icalToken} />
+      <div className={styles.grid}>
+        {WEEKDAYS.map((d) => (
+          <div key={d} className={styles.weekdayHeading}>
+            {d}
+          </div>
+        ))}
+        {createMonthlyCalendar(date, weekOffset).map((dateProps) => (
+          <CalendarCell
+            key={dateProps.day.format('DD/MM/YY')}
+            day={dateProps.day}
+            prevOrNextMonth={dateProps.prevOrNextMonth}
+          />
+        ))}
       </div>
-    );
-  }
-}
+      <EventFooter icalToken={icalToken} />
+    </div>
+  );
+};
+
+export default Calendar;
