ca7ccbce5d2ce8e28c0ca00f1ccdd44e382321ce
Adrian Andersen <adrianandersen@protonmail.com>
2023-02-06 14:46:48 +0100

    refactor(LatestReadme): class component => functional

--- app/routes/overview/components/LatestReadme.tsx
+++ app/routes/overview/components/LatestReadme.tsx
@@ -1,69 +1,50 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import Card from 'app/components/Card';
 import Icon from 'app/components/Icon';
 import { Image } from 'app/components/Image';
 import { Flex } from 'app/components/Layout';
 import { readmeIfy } from 'app/components/ReadmeLogo';
+import type { Readme } from 'app/models';
 import styles from './LatestReadme.css';
 
 type Props = {
-  expanded: boolean;
-  readmes: Array<Record<string, any>>;
-};
-type State = {
-  expanded: boolean;
+  expandedInitially: boolean;
+  readmes: Array<Readme>;
 };
 
-class LatestReadme extends Component<Props, State> {
-  state = {
-    expanded: this.props.expanded,
-  };
+const LatestReadme = ({ readmes, expandedInitially }: Props) => {
+  const [expanded, setExpanded] = useState(expandedInitially);
 
-  // eslint-disable-next-line
-  componentWillReceiveProps(nextProps: Props) {
-    if (nextProps.expanded !== this.state.expanded) {
-      this.setState({
-        expanded: nextProps.expanded || false,
-      });
-    }
-  }
+  useEffect(() => {
+    setExpanded(expandedInitially);
+  }, [expandedInitially]);
 
-  render() {
-    const { expanded } = this.state;
+  return (
+    <Card className={styles.latestReadme}>
+      <button className={styles.heading} onClick={() => setExpanded(!expanded)}>
+        <Flex justifyContent="space-between" alignItems="center">
+          {readmeIfy('readme')}
+          <Icon name={expanded ? 'close' : 'arrow-down'} />
+        </Flex>
+      </button>
 
-    const toggle = () =>
-      this.setState((state) => ({
-        expanded: !state.expanded,
-      }));
-
-    const { readmes = [] } = this.props;
-    return (
-      <Card className={styles.latestReadme}>
-        <button className={styles.heading} onClick={toggle}>
-          <Flex justifyContent="space-between" alignItems="center">
-            {readmeIfy('readme')}
-            <Icon name={expanded ? 'close' : 'arrow-down'} />
-          </Flex>
-        </button>
-
-        {expanded && (
-          <Flex
-            wrap
-            justifyContent="space-around"
-            style={{
-              paddingTop: 20,
-            }}
-          >
-            {readmes.slice(0, 4).map(({ image, pdf, title }) => (
-              <a key={title} href={pdf} className={styles.thumb}>
-                <Image src={image} alt={`Cover of ${title}`} />
-              </a>
-            ))}
-          </Flex>
-        )}
-      </Card>
-    );
-  }
-}
+      {expanded && (
+        <Flex
+          wrap
+          justifyContent="space-around"
+          style={{
+            paddingTop: 20,
+          }}
+        >
+          {readmes.slice(0, 4).map(({ image, pdf, title }) => (
+            <a key={title} href={pdf} className={styles.thumb}>
+              <Image src={image} alt={`Cover of ${title}`} />
+            </a>
+          ))}
+        </Flex>
+      )}
+    </Card>
+  );
+};
 
 export default LatestReadme;
