dd1bfe5bab61a60f1f1ba62eca7a3285ed993016
Mattias Tofte <mattiastofte@gmail.com>
2023-02-11 15:50:03 +0100

    Improve page sidebar layout and design

--- app/routes/pages/components/Sidebar.tsx
+++ app/routes/pages/components/Sidebar.tsx
@@ -45,9 +45,6 @@ class Sidebar extends Component<Props, State> {
                 <Icon name="close" size={50} />
               </button>
               <h3 className={styles.sidebarHeader}>Om Abakus</h3>
-              <h4 className={styles.sidebarSubtitle}>
-                {categorySelected ? categorySelected : 'Generelt'}
-              </h4>
             </div>
 
             <div className={styles.sidebarBottom}>
