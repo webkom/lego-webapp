c33029c74ae3bcbd9ad6c14f8e6b8c897d0d4e6e
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2023-04-14 17:49:37 +0200

    Use a shared component for login/register/forgot-password section

---
+++ app/components/AuthSection/AuthSection.tsx
@@ -0,0 +1,82 @@
+import { useState } from 'react';
+import { Flex } from 'app/components/Layout';
+import {
+  ForgotPasswordForm,
+  LoginForm,
+  RegisterForm,
+} from 'app/components/LoginForm';
+import styles from './AuthSection.css';
+import type { ReactNode, MouseEvent } from 'react';
+
+enum AuthMode {
+  LOGIN,
+  REGISTER,
+  FORGOT_PASSWORD,
+}
+
+const titles: { [M in AuthMode]: string } = {
+  [AuthMode.LOGIN]: 'Logg inn',
+  [AuthMode.REGISTER]: 'Registrer bruker',
+  [AuthMode.FORGOT_PASSWORD]: 'Glemt passord',
+};
+
+const forms: { [M in AuthMode]: ReactNode } = {
+  [AuthMode.LOGIN]: <LoginForm />,
+  [AuthMode.REGISTER]: <RegisterForm />,
+  [AuthMode.FORGOT_PASSWORD]: <ForgotPasswordForm />,
+};
+
+const AuthSection = () => {
+  const [authMode, setAuthMode] = useState<AuthMode>(AuthMode.LOGIN);
+
+  const title = titles[authMode];
+  const form = forms[authMode];
+
+  const createModeSelector = (mode: AuthMode) => (e: MouseEvent) => {
+    setAuthMode(mode);
+    e.stopPropagation();
+  };
+
+  return (
+    <>
+      <Flex
+        component="h2"
+        justifyContent="space-between"
+        alignItems="center"
+        className="u-mb"
+        style={{
+          whiteSpace: 'nowrap',
+        }}
+      >
+        {title}
+        {authMode === AuthMode.LOGIN ? (
+          <div>
+            <button
+              onClick={createModeSelector(AuthMode.FORGOT_PASSWORD)}
+              className={styles.toggleButton}
+            >
+              Glemt passord
+            </button>
+            <span className={styles.toggleButton}>&bull;</span>
+            <button
+              onClick={createModeSelector(AuthMode.REGISTER)}
+              className={styles.toggleButton}
+            >
+              Jeg er ny
+            </button>
+          </div>
+        ) : (
+          <button
+            onClick={createModeSelector(AuthMode.LOGIN)}
+            className={styles.toggleButton}
+          >
+            Tilbake
+          </button>
+        )}
+      </Flex>
+      {form}
+    </>
+  );
+};
+
+export default AuthSection;
--- app/components/LoginForm/LoginPage.tsx
+++ app/components/LoginForm/LoginPage.tsx
@@ -1,86 +1,10 @@
-import { useState } from 'react';
+import AuthSection from 'app/components/AuthSection/AuthSection';
 import { Content } from 'app/components/Content';
-import { Flex } from 'app/components/Layout';
-import {
-  LoginForm,
-  RegisterForm,
-  ForgotPasswordForm,
-} from 'app/components/LoginForm';
-import styles from './Login.css';
-import type { ReactNode, MouseEvent } from 'react';
 
-const LoginPage = () => {
-  const [mode, setMode] = useState<'login' | 'register' | 'forgotPassword'>(
-    'login'
-  );
-  const isLogin = mode === 'login';
-
-  const toggleRegisterUser = (e: MouseEvent) => {
-    setMode('register');
-    e.stopPropagation();
-  };
-  const toggleForgotPassword = (e: MouseEvent) => {
-    setMode('forgotPassword');
-    e.stopPropagation();
-  };
-  const toggleBack = (e: MouseEvent) => {
-    setMode('login');
-    e.stopPropagation();
-  };
-
-  let title: string;
-  let form: ReactNode;
-
-  switch (mode) {
-    case 'login': {
-      title = 'Logg inn';
-      form = <LoginForm />;
-      break;
-    }
-    case 'register': {
-      title = 'Register';
-      form = <RegisterForm />;
-      break;
-    }
-    case 'forgotPassword': {
-      title = 'Glemt passord';
-      form = <ForgotPasswordForm />;
-      break;
-    }
-    default:
-      break;
-  }
-
-  return (
-    <Content>
-      <Flex justifyContent="space-between" alignItems="center">
-        <h2>{title}</h2>
-        {isLogin && (
-          <div>
-            <button
-              onClick={toggleForgotPassword}
-              className={styles.toggleButton}
-            >
-              Glemt passord
-            </button>
-            <span className={styles.toggleButton}>&bull;</span>
-            <button
-              onClick={toggleRegisterUser}
-              className={styles.toggleButton}
-            >
-              Jeg er ny
-            </button>
-          </div>
-        )}
-        {!isLogin && (
-          <button onClick={toggleBack} className={styles.toggleButton}>
-            Tilbake
-          </button>
-        )}
-      </Flex>
-      {form}
-    </Content>
-  );
-};
+const LoginPage = () => (
+  <Content>
+    <AuthSection />
+  </Content>
+);
 
 export default LoginPage;
