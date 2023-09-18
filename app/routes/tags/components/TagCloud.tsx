5632ebbf4f660d3430674ea2e5ea92be17e3fea8
Ivar Nakken <ivarnakken1441@gmail.com>
2023-09-19 00:44:16 +0200

    Refactor tags route to `react-router` v6

--- app/routes/tags/components/TagCloud.tsx
+++ app/routes/tags/components/TagCloud.tsx
@@ -1,67 +1,75 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Link } from 'react-router-dom';
 import { TagCloud as Cloud } from 'react-tagcloud';
+import { fetchAll } from 'app/actions/TagActions';
 import { Content } from 'app/components/Content';
+import { selectTags } from 'app/reducers/tags';
+import { useAppDispatch, useAppSelector } from 'app/store/hooks';
+import type { Tag } from 'app/store/models/Tag';
 
-type Props = {
-  tags: Array<Record<string, any>>;
-  fetching: boolean;
-  hasMore: boolean;
-  fetchAll: (arg0: { next: boolean }) => void;
+type CloudTag = {
+  value: string;
+  count: number;
 };
 
-class TagCloud extends Component<Props> {
-  tagRenderer = (tag: Record<string, any>, size: string, color: string) => (
-    <Link
-      key={tag.value}
-      to={`/tags/${tag.value}`}
-      style={{
-        fontSize: `${size}px`,
-        color: color,
-        margin: '3px',
-        display: 'inline-block',
-      }}
-    >
-      {tag.value}
-    </Link>
-  );
+const tagRenderer = (tag: CloudTag, size: string, color: string) => (
+  <Link
+    key={tag.value}
+    to={`/tags/${tag.value}`}
+    style={{
+      fontSize: `${size}px`,
+      color: color,
+      margin: '3px',
+      display: 'inline-block',
+    }}
+  >
+    {tag.value}
+  </Link>
+);
 
-  componentDidUpdate() {
-    const { fetchAll, fetching, hasMore } = this.props;
+const TagCloud = () => {
+  const tags = useAppSelector((state) => selectTags(state));
+  const fetching = useAppSelector((state) => state.tags.fetching);
+  const hasMore = useAppSelector((state) => state.tags.hasMore);
 
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAll());
+  }, [dispatch]);
+
+  useEffect(() => {
     if (hasMore && !fetching) {
-      fetchAll({
-        next: true,
-      });
+      dispatch(fetchAll({ next: true }));
     }
-  }
+  }, [dispatch, hasMore, fetching]);
 
-  render() {
-    const data = this.props.tags.map((tag) => {
-      return {
-        value: tag.tag,
-        count: tag.usages,
-      };
-    });
-    const options = {
-      hue: 'red',
+  const data: CloudTag[] = tags.map((tag: Tag) => {
+    return {
+      value: tag.tag,
+      count: tag.usages,
     };
-    return (
-      <Content>
-        <Helmet title="Tags" />
-        <h1>Tags</h1>
-        <Cloud
-          renderer={this.tagRenderer}
-          minSize={12}
-          maxSize={35}
-          tags={data}
-          shuffle={false}
-          colorOptions={options}
-        />
-      </Content>
-    );
-  }
-}
+  });
+
+  const options = {
+    hue: 'red',
+  };
+
+  return (
+    <Content>
+      <Helmet title="Tags" />
+      <h1>Tags</h1>
+      <Cloud
+        renderer={tagRenderer}
+        minSize={12}
+        maxSize={35}
+        tags={data}
+        shuffle={false}
+        colorOptions={options}
+      />
+    </Content>
+  );
+};
 
 export default TagCloud;
