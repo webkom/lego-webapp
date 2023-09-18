8a31202e44d971990f2d2ac62358ffc1d6f6229b
Ivar Nakken <ivarnakken1441@gmail.com>
2023-09-18 18:14:03 +0200

    Refactor announcement route to `react-router` v6
    co-authored-by: Eik Hvattum Røgeberg <eik@eikhr.no>

--- app/routes/announcements/index.tsx
+++ app/routes/announcements/index.tsx
@@ -1,34 +1,16 @@
-import { Route, Switch } from 'react-router-dom';
-import RouteWrapper from 'app/components/RouteWrapper';
-import { UserContext } from 'app/routes/app/AppRoute';
+import { useRouteMatch, Route, Switch } from 'react-router-dom';
 import PageNotFound from '../pageNotFound';
-import AnnouncementsRoute from './AnnouncementsRoute';
+import AnnouncementsList from './components/AnnouncementsList';
 
-const announcementsRoute = ({
-  match,
-}: {
-  match: {
-    path: string;
-  };
-}) => (
-  <UserContext.Consumer>
-    {({ currentUser, loggedIn }) => (
-      <Switch>
-        <RouteWrapper
-          exact
-          path={`${match.path}`}
-          passedProps={{
-            currentUser,
-            loggedIn,
-          }}
-          Component={AnnouncementsRoute}
-        />
-        <Route component={PageNotFound} />
-      </Switch>
-    )}
-  </UserContext.Consumer>
-);
+const AnnouncementsRoute = () => {
+  const { path } = useRouteMatch();
 
-export default function Announcements() {
-  return <Route path="/announcements" component={announcementsRoute} />;
-}
+  return (
+    <Switch>
+      <Route exact path={path} component={AnnouncementsList} />
+      <Route component={PageNotFound} />
+    </Switch>
+  );
+};
+
+export default AnnouncementsRoute;
