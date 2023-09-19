fca1e9b14b5fdd8bc853500bbee4a33da7c5793d
Ivar Nakken <ivarnakken1441@gmail.com>
2023-09-19 11:51:00 +0200

    Refactor polls routes to `react-router` v6

--- app/routes/polls/components/PollDetail.tsx
+++ app/routes/polls/components/PollDetail.tsx
@@ -1,80 +1,82 @@
-import { Button, Icon } from '@webkom/lego-bricks';
-import { Component } from 'react';
+import { Button, Icon, LoadingIndicator } from '@webkom/lego-bricks';
+import { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
+import { useParams } from 'react-router-dom-v5-compat';
+import { fetchPoll } from 'app/actions/PollActions';
 import { Content } from 'app/components/Content';
+import { LoginPage } from 'app/components/LoginForm';
 import NavigationTab from 'app/components/NavigationTab';
 import Poll from 'app/components/Poll';
+import { selectIsLoggedIn } from 'app/reducers/auth';
+import { selectPollById } from 'app/reducers/polls';
+import { useAppDispatch, useAppSelector } from 'app/store/hooks';
 import PollEditor from './PollEditor';
-import type { ActionGrant, ID } from 'app/models';
-import type { PollEntity } from 'app/reducers/polls';
 
-type Props = {
-  poll: PollEntity;
-  editPoll: (arg0: PollEntity) => Promise<any>;
-  deletePoll: (id: ID) => Promise<any>;
-  votePoll: (pollId: ID, optionId: ID) => Promise<any>;
-  fetching: boolean;
-  actionGrant: ActionGrant;
-  initialValues: PollEntity;
-};
-type State = {
-  editing: boolean;
-};
+const PollDetail = () => {
+  const loggedIn = useAppSelector(selectIsLoggedIn);
+  const [editing, setEditing] = useState(false);
 
-class PollDetail extends Component<Props, State> {
-  state = {
-    editing: false,
-  };
-  toggleEdit = () => {
-    this.setState({
-      editing: !this.state.editing,
-    });
+  const toggleEdit = () => {
+    setEditing(!editing);
   };
 
-  render() {
+  const { pollsId } = useParams();
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPoll(pollsId));
+  }, [dispatch, pollsId]);
+
+  const poll = useAppSelector((state) => selectPollById(state, pollsId));
+  const fetching = useAppSelector((state) => state.polls.fetching);
+  const actionGrant = poll?.actionGrant ?? [];
+
+  if (!loggedIn) {
+    return LoginPage;
+  }
+
+  if (!poll) {
     return (
       <Content>
-        <Helmet title={this.props.poll.title} />
-        <NavigationTab
-          title={this.props.poll.title}
-          back={{
-            label: 'Tilbake',
-            path: '/polls',
-          }}
-        >
-          {this.props.actionGrant.includes('edit') && (
-            <Button onClick={this.toggleEdit}>
-              {this.state.editing ? (
-                'Avbryt'
-              ) : (
-                <>
-                  <Icon name="create-outline" size={19} />
-                  Rediger
-                </>
-              )}
-            </Button>
-          )}
-        </NavigationTab>
-        {!this.state.editing && (
-          <Poll
-            poll={this.props.poll}
-            handleVote={this.props.votePoll}
-            allowedToViewHiddenResults={this.props.actionGrant.includes('edit')}
-            details
-          />
-        )}
-        {this.state.editing && (
-          <PollEditor
-            initialValues={this.props.initialValues}
-            editing
-            editOrCreatePoll={this.props.editPoll}
-            toggleEdit={this.toggleEdit}
-            deletePoll={() => this.props.deletePoll(this.props.poll.id)}
-          />
-        )}
+        <LoadingIndicator loading={fetching} />
       </Content>
     );
   }
-}
+
+  return (
+    <Content>
+      <Helmet title={poll.title} />
+      <NavigationTab
+        title={poll.title}
+        back={{
+          label: 'Tilbake',
+          path: '/polls',
+        }}
+      >
+        {actionGrant.includes('edit') && (
+          <Button onClick={toggleEdit}>
+            {editing ? (
+              'Avbryt'
+            ) : (
+              <>
+                <Icon name="create-outline" size={19} />
+                Rediger
+              </>
+            )}
+          </Button>
+        )}
+      </NavigationTab>
+      {!editing && (
+        <Poll
+          poll={poll}
+          allowedToViewHiddenResults={actionGrant.includes('edit')}
+          details
+        />
+      )}
+      {editing && <PollEditor poll={poll} editing toggleEdit={toggleEdit} />}
+    </Content>
+  );
+};
 
 export default PollDetail;
