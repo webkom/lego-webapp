bd72bcb883d741805b0921e2c1f3a442dac9767c
Ivar Nakken <ivarnakken1441@gmail.com>
2023-09-20 15:32:04 +0200

    Refactor email routes to `react-router` v6

--- app/routes/admin/email/components/RestrictedMailEditor.tsx
+++ app/routes/admin/email/components/RestrictedMailEditor.tsx
@@ -1,19 +1,24 @@
 import { Button, Icon } from '@webkom/lego-bricks';
-import { reduxForm, Form, Field } from 'redux-form';
-import { TextInput, SelectInput } from 'app/components/Form';
+import { useEffect } from 'react';
+import { Field } from 'react-final-form';
+import { useParams } from 'react-router-dom-v5-compat';
+import { push } from 'redux-first-history';
+import {
+  createRestrictedMail,
+  fetchRestrictedMail,
+} from 'app/actions/RestrictedMailActions';
+import {
+  TextInput,
+  Form,
+  SelectInput,
+  LegoFinalForm,
+} from 'app/components/Form';
 import CheckBox from 'app/components/Form/CheckBox';
 import config from 'app/config';
+import { selectRestrictedMailById } from 'app/reducers/restrictedMails';
+import { useAppDispatch, useAppSelector } from 'app/store/hooks';
 import { isEmail, createValidator, required } from 'app/utils/validation';
-import type { History } from 'history';
 
-export type Props = {
-  restrictedMailId?: number;
-  restrictedMail: Record<string, any>;
-  submitting: boolean;
-  handleSubmit: (arg0: (...args: Array<any>) => any) => void;
-  push: History['push'];
-  mutateFunction: (arg0: Record<string, any>) => Promise<any>;
-};
 const hiddenSenderLabel = (
   <div>
     <p
@@ -54,16 +59,51 @@ const restrictedMailLabel = (
   </div>
 );
 
-const RestrictedMailEditor = ({
-  restrictedMail,
-  restrictedMailId,
-  mutateFunction,
-  submitting,
-  push,
-  handleSubmit,
-}: Props) => {
-  const onSubmit = (data) => {
-    mutateFunction({
+const validate = createValidator({
+  fromAddress: [required(), isEmail()],
+});
+
+const RestrictedMailEditor = () => {
+  const { restrictedMailId } = useParams();
+  const restrictedMail = useAppSelector((state) =>
+    selectRestrictedMailById(state, { restrictedMailId })
+  );
+
+  const initialValues = {
+    ...restrictedMail,
+    groups: (restrictedMail?.groups || []).map((groups) => ({
+      label: groups.name,
+      value: groups.id,
+    })),
+    meetings: (restrictedMail?.meetings || []).map((meeting) => ({
+      label: meeting.name,
+      value: meeting.id,
+    })),
+    events: (restrictedMail?.events || []).map((event) => ({
+      label: event.title,
+      value: event.id,
+    })),
+    // Raw Sauce
+    rawAddresses: (restrictedMail?.rawAddresses || []).map((rawAddresses) => ({
+      label: rawAddresses,
+      value: rawAddresses,
+    })),
+    users: (restrictedMail?.users || []).map((user) => ({
+      label: user.fullName,
+      value: user.id,
+    })),
+  };
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (restrictedMailId) {
+      dispatch(fetchRestrictedMail(restrictedMailId));
+    }
+  }, [dispatch, restrictedMailId]);
+
+  const onSubmit = async (data) => {
+    const payload = {
       ...data,
       rawAddresses: (data.rawAddresses || []).map(
         (rawAddresses) => rawAddresses.value
@@ -72,112 +112,119 @@ const RestrictedMailEditor = ({
       events: (data.events || []).map((event) => event.value),
       meetings: (data.meetings || []).map((meeting) => meeting.id),
       users: (data.users || []).map((user) => user.id),
-    }).then(({ payload }) => {
-      if (!restrictedMailId) {
-        push(`/admin/email/restricted/${payload.result}`);
-      }
-    });
+    };
+
+    if (!restrictedMailId) {
+      const response = await dispatch(createRestrictedMail(payload));
+      dispatch(push(`/admin/email/restricted/${response.payload.result}`));
+    }
   };
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <Field
-        disabled={restrictedMailId}
-        required
-        placeholder="abakus@abakus.no"
-        name="fromAddress"
-        label="E-posten du ønsker å sende fra"
-        component={TextInput.Field}
-      />
-      <Field
-        disabled={restrictedMailId}
-        name="hideSender"
-        label={hiddenSenderLabel}
-        component={CheckBox.Field}
-        normalize={(v) => !!v}
-      />
-      <Field
-        disabled={restrictedMailId}
-        name="weekly"
-        label={restrictedMailLabel}
-        component={CheckBox.Field}
-        normalize={(v) => !!v}
-      />
+    <LegoFinalForm
+      onSubmit={onSubmit}
+      validate={validate}
+      initialValues={initialValues}
+    >
+      {({ handleSubmit, submitting, pristine }) => (
+        <Form onSubmit={handleSubmit}>
+          <Field
+            disabled={restrictedMailId}
+            required
+            placeholder="abakus@abakus.no"
+            name="fromAddress"
+            label="E-posten du ønsker å sende fra"
+            component={TextInput.Field}
+          />
+          <Field
+            disabled={restrictedMailId}
+            name="hideSender"
+            label={hiddenSenderLabel}
+            component={CheckBox.Field}
+            parse={(v) => !!v}
+          />
+          <Field
+            disabled={restrictedMailId}
+            name="weekly"
+            label={restrictedMailLabel}
+            component={CheckBox.Field}
+            parse={(v) => !!v}
+          />
 
-      <Field
-        disabled={restrictedMailId}
-        label="Brukere"
-        name="users"
-        isMulti
-        placeholder="Brukere du ønsker å sende e-post til"
-        filter={['users.user']}
-        component={SelectInput.AutocompleteField}
-      />
-      <Field
-        disabled={restrictedMailId}
-        label="Grupper"
-        name="groups"
-        isMulti
-        placeholder="Grupper du ønsker å sende e-post til"
-        filter={['users.abakusgroup']}
-        component={SelectInput.AutocompleteField}
-      />
-      <Field
-        disabled={restrictedMailId}
-        label="Arrangementer"
-        name="events"
-        isMulti
-        placeholder="Arrangementer du ønsker å sende e-post til"
-        filter={['events.event']}
-        component={SelectInput.AutocompleteField}
-      />
-      <Field
-        disabled={restrictedMailId}
-        label="Møter"
-        name="meetings"
-        isMulti
-        placeholder="Møter du ønsker å sende e-post til"
-        filter={['meetings.meeting']}
-        component={SelectInput.AutocompleteField}
-      />
-      <Field
-        disabled={restrictedMailId}
-        label="E-postadresser"
-        name="rawAddresses"
-        placeholder="Enkelte e-poster du ønsker å sende til"
-        component={SelectInput.Field}
-        tags
-        isMulti
-        isValidNewOption={({ label }: { label: string }) => isEmail()(label)[0]}
-        shouldKeyDownEventCreateNewOption={({ keyCode }: { keyCode: number }) =>
-          keyCode === 32 || keyCode === 13
-        }
-      />
+          <Field
+            disabled={restrictedMailId}
+            label="Brukere"
+            name="users"
+            isMulti
+            placeholder="Brukere du ønsker å sende e-post til"
+            filter={['users.user']}
+            component={SelectInput.AutocompleteField}
+          />
+          <Field
+            disabled={restrictedMailId}
+            label="Grupper"
+            name="groups"
+            isMulti
+            placeholder="Grupper du ønsker å sende e-post til"
+            filter={['users.abakusgroup']}
+            component={SelectInput.AutocompleteField}
+          />
+          <Field
+            disabled={restrictedMailId}
+            label="Arrangementer"
+            name="events"
+            isMulti
+            placeholder="Arrangementer du ønsker å sende e-post til"
+            filter={['events.event']}
+            component={SelectInput.AutocompleteField}
+          />
+          <Field
+            disabled={restrictedMailId}
+            label="Møter"
+            name="meetings"
+            isMulti
+            placeholder="Møter du ønsker å sende e-post til"
+            filter={['meetings.meeting']}
+            component={SelectInput.AutocompleteField}
+          />
+          <Field
+            disabled={restrictedMailId}
+            label="E-postadresser"
+            name="rawAddresses"
+            placeholder="Enkelte e-poster du ønsker å sende til"
+            component={SelectInput.Field}
+            tags
+            isMulti
+            isValidNewOption={({ label }: { label: string }) =>
+              isEmail()(label)[0]
+            }
+            shouldKeyDownEventCreateNewOption={({
+              keyCode,
+            }: {
+              keyCode: number;
+            }) => keyCode === 32 || keyCode === 13}
+          />
 
-      {!restrictedMailId && (
-        <Button submit disabled={submitting}>
-          Lag flaskepost
-        </Button>
+          {!restrictedMailId && (
+            <Button submit disabled={submitting || pristine}>
+              Lag flaskepost
+            </Button>
+          )}
+          {restrictedMailId && restrictedMail && (
+            <a
+              href={`${config.serverUrl}/restricted-mail/${restrictedMailId}/token?auth=${restrictedMail.tokenQueryParam}`}
+              download
+            >
+              <Button>
+                <Icon name="download-outline" size={19} />
+                Last ned e-post token
+              </Button>
+            </a>
+          )}
+        </Form>
       )}
-      {restrictedMailId && restrictedMail && (
-        <a
-          href={`${config.serverUrl}/restricted-mail/${restrictedMailId}/token?auth=${restrictedMail.tokenQueryParam}`}
-          download
-        >
-          <Button>
-            <Icon name="download-outline" size={19} />
-            Last ned e-post token
-          </Button>
-        </a>
-      )}
-    </Form>
+    </LegoFinalForm>
   );
 };
 
-export default reduxForm({
-  form: 'restrictedEmail',
-  enableReinitialize: true,
-  validate: createValidator({
-    fromAddress: [required(), isEmail()],
-  }),
-})(RestrictedMailEditor);
+export default RestrictedMailEditor;
