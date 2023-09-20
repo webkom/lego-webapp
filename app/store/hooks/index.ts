73e3776af67ad140edb02541ae0affce4e706286
Ivar Nakken <ivarnakken1441@gmail.com>
2023-09-20 22:26:49 +0200

    Implement a custom `useHelmet` hook
    The hook encapsulates the same logic as the `helmet` hoc, but can be be
    employed inside of components as a hook. It will manage the Helmet
    properties as a side effect.

--- app/store/hooks/index.ts
+++ app/store/hooks/index.ts
@@ -5,3 +5,5 @@ import type { TypedUseSelectorHook } from 'react-redux';
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export { default as useHelmet } from './useHelmet';
