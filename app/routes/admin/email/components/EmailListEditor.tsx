844cedda54213cf3c2a9c56a67e5173633af7fac
Ivar Nakken <ivarnakken1441@gmail.com>
2023-09-20 15:32:04 +0200

    Refactor email routes to `react-router` v6

--- app/routes/admin/email/components/EmailListEditor.tsx
+++ app/routes/admin/email/components/EmailListEditor.tsx
@@ -1,119 +1,176 @@
-import { Button } from '@webkom/lego-bricks';
-import { Form, Field } from 'redux-form';
+import { useEffect } from 'react';
+import { Field } from 'react-final-form';
+import { useParams } from 'react-router-dom-v5-compat';
+import { push } from 'redux-first-history';
+import {
+  createEmailList,
+  editEmailList,
+  fetchEmailList,
+} from 'app/actions/EmailListActions';
 import {
   TextInput,
+  Form,
   SelectInput,
   CheckBox,
-  handleSubmissionError,
-  legoForm,
+  LegoFinalForm,
 } from 'app/components/Form';
-import { roleOptions } from 'app/utils/constants';
+import SubmissionError from 'app/components/Form/SubmissionError';
+import { SubmitButton } from 'app/components/Form/SubmitButton';
+import { selectEmailListById } from 'app/reducers/emailLists';
+import { useAppDispatch, useAppSelector } from 'app/store/hooks';
+import { ROLES, type RoleType, roleOptions } from 'app/utils/constants';
 import { createValidator, required, EMAIL_REGEX } from 'app/utils/validation';
-import type { History } from 'history';
 
-export type Props = {
-  emailListId?: number;
-  submitting: boolean;
-  handleSubmit: (arg0: (...args: Array<any>) => any) => void;
-  push: History['push'];
-  mutateFunction: (arg0: Record<string, any>) => Promise<any>;
-};
+const validate = createValidator({
+  email: [
+    required(
+      'Skriv inn en gyldig e-postadresse. Legg merke til at @abakus.no ikke skal med.'
+    ),
+  ],
+  name: [required()],
+  additionalEmails: [
+    // Check if all emails entered are valid
+    (value) => [
+      !value || value.every((email) => EMAIL_REGEX.test(email.value)),
+      'Ugyldig e-post',
+    ],
+  ],
+});
 
-const EmailListEditor = ({ submitting, handleSubmit, emailListId }: Props) => (
-  <Form onSubmit={handleSubmit}>
-    <Field
-      required
-      placeholder="Abakus"
-      name="name"
-      label="Navnet på e-postliste"
-      component={TextInput.Field}
-    />
-    <Field
-      required
-      disabled={emailListId}
-      placeholder="abakus"
-      suffix="@abakus.no"
-      name="email"
-      label="E-post"
-      component={TextInput.Field}
-    />
-    <Field
-      label="Brukere"
-      name="users"
-      isMulti
-      placeholder="Inviter en ny bruker"
-      filter={['users.user']}
-      component={SelectInput.AutocompleteField}
-    />
-    <Field
-      label="Grupper"
-      name="groups"
-      isMulti
-      placeholder="Inviter en ny bruker"
-      filter={['users.abakusgroup']}
-      component={SelectInput.AutocompleteField}
-    />
-    <Field
-      label="Roller (hvis du lar denne stå tom betyr det at alle medlemmene i gruppene får e-post!)"
-      name="groupRoles"
-      isMulti
-      placeholder="Velg rolle"
-      options={roleOptions}
-      component={SelectInput.Field}
-    />
+const EmailListEditor = () => {
+  const { emailListId } = useParams();
+  const emailList = useAppSelector((state) =>
+    selectEmailListById(state, { emailListId })
+  );
 
-    <Field
-      label="Kun for for brukere med internmail (@abakus.no)"
-      description="Når denne er aktivert vil kun brukere med aktiv @abakus.no-adresse få e-post fra denne listen"
-      name="requireInternalAddress"
-      component={CheckBox.Field}
-      normalize={(v) => !!v}
-    />
+  const dispatch = useAppDispatch();
 
-    <Field
-      label="E-poster for medlemmer utenfor abakus"
-      description="Her kan du legge til e-postene til de som skal ha mailer fra gruppemailen, men ikke er medlem av abakus"
-      name="additionalEmails"
-      placeholder="Skriv inn e-post her"
-      component={SelectInput.Field}
-      isMulti
-      tags
-    />
-    <Button submit disabled={submitting}>
-      {emailListId ? 'Oppdater e-postliste' : 'Lag e-postliste'}
-    </Button>
-  </Form>
-);
+  useEffect(() => {
+    if (emailListId) {
+      dispatch(fetchEmailList(emailListId));
+    }
+  }, [dispatch, emailListId]);
 
-export default legoForm({
-  form: 'emailList',
-  enableReinitialize: true,
-  onSubmit: (data, dispatch, { mutateFunction, emailListId, push }: Props) =>
-    mutateFunction({
-      id: data.id,
-      email: data.email,
-      name: data.name,
-      requireInternalAddress: data.requireInternalAddress,
-      groupRoles: (data.groupRoles || []).map((groupRole) => groupRole.value),
-      groups: (data.groups || []).map((group) => group.value),
-      users: (data.users || []).map((user) => user.value),
-      additionalEmails: (data.additionalEmails || []).map(
+  const handleSubmit = async (values) => {
+    const payload = {
+      id: values.id,
+      email: values.email,
+      name: values.name,
+      requireInternalAddress: values.requireInternalAddress,
+      groupRoles: (values.groupRoles || []).map((groupRole) => groupRole.value),
+      groups: (values.groups || []).map((group) => group.value),
+      users: (values.users || []).map((user) => user.value),
+      additionalEmails: (values.additionalEmails || []).map(
         (email) => email.value
       ),
-    }).then(({ payload }) => {
-      if (!emailListId) {
-        push(`/admin/email/lists/${payload.result}`);
-      }
-    }, handleSubmissionError),
-  validate: createValidator({
-    email: [required()],
-    name: [required()],
-    additionalEmails: [
-      //check if all emails entered are valid
-      (value) => [
-        !value || value.every((email) => EMAIL_REGEX.test(email.value)),
-        'Ugyldig e-post',
-      ],
-    ],
-  }),
-})(EmailListEditor);
+    };
+
+    if (emailListId) {
+      dispatch(editEmailList(payload));
+    } else {
+      const response = await dispatch(createEmailList(payload));
+      dispatch(push(`/admin/email/lists/${response.payload.result}`));
+    }
+  };
+
+  const initialValues = {
+    ...emailList,
+    groups: (emailList?.groups || []).filter(Boolean).map((groups) => ({
+      label: groups.name,
+      value: groups.id,
+    })),
+    groupRoles: (emailList?.groupRoles || []).map((groupRoles: RoleType) => ({
+      label: ROLES[groupRoles],
+      value: groupRoles,
+    })),
+    users: (emailList?.users || []).filter(Boolean).map((user) => ({
+      label: user.fullName,
+      value: user.id,
+    })),
+    additionalEmails: (emailList?.additionalEmails || []).map(
+      (additionalEmail) => ({
+        label: additionalEmail,
+        value: additionalEmail,
+      })
+    ),
+    requireInternalAddress: emailList?.requireInternalAddress || false,
+  };
+
+  return (
+    <LegoFinalForm
+      onSubmit={handleSubmit}
+      validate={validate}
+      initialValues={initialValues}
+    >
+      {({ handleSubmit }) => (
+        <Form onSubmit={handleSubmit}>
+          <Field
+            required
+            placeholder="Abakus"
+            name="name"
+            label="Navnet på e-postliste"
+            component={TextInput.Field}
+          />
+          <Field
+            required
+            disabled={emailListId}
+            placeholder="abakus"
+            suffix="@abakus.no"
+            name="email"
+            label="E-post"
+            component={TextInput.Field}
+          />
+          <Field
+            label="Brukere"
+            name="users"
+            isMulti
+            placeholder="Inviter en ny bruker"
+            filter={['users.user']}
+            component={SelectInput.AutocompleteField}
+          />
+          <Field
+            label="Grupper"
+            name="groups"
+            isMulti
+            placeholder="Inviter en ny bruker"
+            filter={['users.abakusgroup']}
+            component={SelectInput.AutocompleteField}
+          />
+          <Field
+            label="Roller (hvis du lar denne stå tom betyr det at alle medlemmene i gruppene får e-post!)"
+            name="groupRoles"
+            isMulti
+            placeholder="Velg rolle"
+            options={roleOptions}
+            component={SelectInput.Field}
+          />
+
+          <Field
+            label="Kun for for brukere med internmail (@abakus.no)"
+            description="Når denne er aktivert vil kun brukere med aktiv @abakus.no-adresse få e-post fra denne listen"
+            name="requireInternalAddress"
+            component={CheckBox.Field}
+            normalize={(v) => !!v}
+          />
+
+          <Field
+            label="E-poster for medlemmer utenfor abakus"
+            description="Her kan du legge til e-postene til de som skal ha mailer fra gruppemailen, men ikke er medlem av abakus"
+            name="additionalEmails"
+            placeholder="Skriv inn e-post her"
+            component={SelectInput.Field}
+            isMulti
+            tags
+          />
+
+          <SubmissionError />
+          <SubmitButton>
+            {emailListId ? 'Oppdater e-postliste' : 'Lag e-postliste'}
+          </SubmitButton>
+        </Form>
+      )}
+    </LegoFinalForm>
+  );
+};
+
+export default EmailListEditor;
