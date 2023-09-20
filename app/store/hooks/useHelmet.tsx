1eb04d0a6a256c6e06bac85a1e2145bce73d496a
Ivar Nakken <ivarnakken1441@gmail.com>
2023-09-20 22:26:49 +0200

    Implement a custom `useHelmet` hook
    The hook encapsulates the same logic as the `helmet` hoc, but can be be
    employed inside of components as a hook. It will manage the Helmet
    properties as a side effect.

---
+++ app/store/hooks/useHelmet.tsx
@@ -0,0 +1,70 @@
+import { Helmet } from 'react-helmet-async';
+import config from 'app/config';
+import type Config from 'config/Config';
+
+type Property = {
+  property?: string;
+  content?: string;
+  element?: string;
+  children?: string;
+  rel?: string;
+  href?: string;
+};
+
+type PropertyGenerator<T> = (
+  props: T,
+  config?: Partial<Config>
+) => Property[] | null | undefined;
+
+/**
+ * Hook to manage Helmet properties for SEO and meta tags.
+ *
+ * @param propertyGenerator - A function that takes in props and config to generate an array of properties for Helmet.
+ * @param props - The props that will be passed to the propertyGenerator function to generate Helmet properties.
+ * @returns - Nothing. The hook takes care of setting the Helmet properties as a side effect.
+ *
+ * @example
+ 
+ * const propertyGenerator = (props, config) => {
+ *   if (!props.item) return;
+ *   return [
+ *     {
+ *       property: 'og:title',
+ *       content: props.item.title,
+ *     },
+ *     {
+ *       element: 'title',
+ *       children: props.item.title,
+ *     },
+ *   ];
+ * };
+ * 
+ * const YourComponent = ({ item }) => {
+ *   useHelmet(propertyGenerator, { item });
+ * 
+ *   return (
+ *     <div>
+ *       ...
+ *     </div>
+ *   );
+ * };
+ *
+ */
+const useHelmet = <T extends Record<string, unknown>>(
+  propertyGenerator: PropertyGenerator<T>,
+  props: T
+) => {
+  const properties = propertyGenerator(props, config);
+
+  if (!properties) return null;
+
+  return (
+    <Helmet>
+      {properties.map((prop, index) => (
+        <meta key={index} {...prop} />
+      ))}
+    </Helmet>
+  );
+};
+
+export default useHelmet;
