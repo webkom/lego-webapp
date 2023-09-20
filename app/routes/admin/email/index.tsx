52a0cb1ae6b8c659d7430fe62c557f828075b610
Ivar Nakken <ivarnakken1441@gmail.com>
2023-09-20 15:32:04 +0200

    Refactor email routes to `react-router` v6

--- app/routes/admin/email/index.tsx
+++ app/routes/admin/email/index.tsx
@@ -1,76 +1,65 @@
-import { Route, Switch } from 'react-router-dom';
+import { useRouteMatch, Route, Switch } from 'react-router-dom';
 import PageNotFound from '../../pageNotFound';
-import CreateEmailListRoute from './CreateEmailListRoute';
-import CreateEmailUserRoute from './CreateEmailUserRoute';
-import CreateRestrictedMailRoute from './CreateRestrictedMailRoute';
-import EmailListRoute from './EmailListRoute';
-import EmailListsRoute from './EmailListsRoute';
-import EmailUserRoute from './EmailUserRoute';
-import EmailUsersRoute from './EmailUsersRoute';
-import RestrictedMailRoute from './RestrictedMailRoute';
-import RestrictedMailsRoute from './RestrictedMailsRoute';
-import EmailRoute from './components/EmailRoute';
+import EmailListEditor from './components/EmailListEditor';
+import EmailLists from './components/EmailLists';
+import EmailPage from './components/EmailPage';
+import EmailUserEditor from './components/EmailUserEditor';
+import EmailUsers from './components/EmailUsers';
+import RestrictedMailEditor from './components/RestrictedMailEditor';
+import RestrictedMails from './components/RestrictedMails';
 
-const groupRoute = ({
-  match,
-}: {
-  match: {
-    path: string;
-  };
-}) => (
-  <Switch>
-    <Route path={`${match.path}`}>
-      <EmailRoute>
-        <Switch>
-          <Route exact path={`${match.path}`} component={EmailListsRoute} />
-          <Route
-            exact
-            path={`${match.path}/lists/new`}
-            component={CreateEmailListRoute}
-          />
-          <Route
-            exact
-            path={`${match.path}/lists/:emailListId`}
-            component={EmailListRoute}
-          />
-          <Route
-            exact
-            path={`${match.path}/users`}
-            component={EmailUsersRoute}
-          />
-          <Route
-            exact
-            path={`${match.path}/users/new`}
-            component={CreateEmailUserRoute}
-          />
-          <Route
-            exact
-            path={`${match.path}/users/:emailUserId`}
-            component={EmailUserRoute}
-          />
-          <Route
-            exact
-            path={`${match.path}/restricted`}
-            component={RestrictedMailsRoute}
-          />
-          <Route
-            exact
-            path={`${match.path}/restricted/new`}
-            component={CreateRestrictedMailRoute}
-          />
-          <Route
-            exact
-            path={`${match.path}/restricted/:restrictedMailId`}
-            component={RestrictedMailRoute}
-          />
-          <Route component={PageNotFound} />
-        </Switch>
-      </EmailRoute>
-    </Route>
-    <Route component={PageNotFound} />
-  </Switch>
-);
+const EmailRoute = () => {
+  const { path } = useRouteMatch();
 
-export default function Email() {
-  return <Route path="/admin/email" component={groupRoute} />;
-}
+  return (
+    <Switch>
+      <Route path={path}>
+        <EmailPage>
+          <Switch>
+            <Route exact path={path} component={EmailLists} />
+            <Route
+              exact
+              path={`${path}/lists/new`}
+              component={EmailListEditor}
+            />
+            <Route
+              exact
+              path={`${path}/lists/:emailListId`}
+              component={EmailListEditor}
+            />
+            <Route exact path={`${path}/users`} component={EmailUsers} />
+            <Route
+              exact
+              path={`${path}/users/new`}
+              component={EmailUserEditor}
+            />
+            <Route
+              exact
+              path={`${path}/users/:emailUserId`}
+              component={EmailUserEditor}
+            />
+            <Route
+              exact
+              path={`${path}/restricted`}
+              component={RestrictedMails}
+            />
+            <Route
+              exact
+              path={`${path}/restricted/new`}
+              component={RestrictedMailEditor}
+            />
+            <Route
+              exact
+              path={`${path}/restricted/:restrictedMailId`}
+              component={RestrictedMailEditor}
+            />
+            <Route component={PageNotFound} />
+          </Switch>
+        </EmailPage>
+      </Route>
+      <Route component={PageNotFound} />
+    </Switch>
+  );
+};
+
+export default EmailRoute;
