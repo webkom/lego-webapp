48031df35348f7e99fec6c08aef9e7cb3b3be831
Ivar Nakken <ivarnakken1441@gmail.com>
2023-09-21 23:13:36 +0200

    Refactor contact form route to `react-router` v6

--- app/routes/contact/components/Contact.tsx
+++ app/routes/contact/components/Contact.tsx
@@ -1,23 +1,13 @@
 import { Helmet } from 'react-helmet-async';
 import { Content } from 'app/components/Content';
-import type { Group } from 'app/models';
-import type { ContactForm as ContactFormType } from 'app/reducers/contact';
 import ContactForm from './ContactForm';
-import type { FormProps } from 'redux-form';
 
-type Props = {
-  sendContactMessage: (message: ContactFormType) => Promise<any>;
-  addToast: (arg0: { message: string }) => void;
-  loggedIn: boolean;
-  groups: Array<Group>;
-} & FormProps;
-
-const Contact = (props: Props) => {
+const Contact = () => {
   return (
     <Content>
       <Helmet title="Kontakt" />
       <h1>Kontaktskjema for Abakus</h1>
-      <ContactForm {...props} />
+      <ContactForm />
     </Content>
   );
 };
