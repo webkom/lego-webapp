aee53fc3c1bb567a15ea326e6b569e73798ca7c1
Ivar Nakken <ivarnakken1441@gmail.com>
2023-09-21 21:56:56 +0200

    Rewrite contact form to `react-final-form`

--- app/routes/contact/ContactRoute.ts
+++ app/routes/contact/ContactRoute.ts
@@ -1,21 +1,12 @@
 import { connect } from 'react-redux';
 import { compose } from 'redux';
-import { reduxForm, reset, change } from 'redux-form';
 import { sendContactMessage } from 'app/actions/ContactActions';
 import { fetchAllWithType, fetchGroup } from 'app/actions/GroupActions';
 import { addToast } from 'app/actions/ToastActions';
 import { GroupType } from 'app/models';
 import { selectGroup, selectGroupsWithType } from 'app/reducers/groups';
-import { createValidator, required, maxLength } from 'app/utils/validation';
 import withPreparedDispatch from 'app/utils/withPreparedDispatch';
 import Contact from './components/Contact';
-
-const validate = createValidator({
-  recipient_group: [required()],
-  title: [required(), maxLength(80)],
-  message: [required()],
-  captchaResponse: [required('Captcha er ikke validert')],
-});
 
 const commiteeGroupType = GroupType.Committee;
 const revueBoardGroupId = 59;
@@ -30,15 +21,13 @@ const mapStateToProps = (state) => {
   });
 
   return {
-    groups: [...commitees, revueBoard],
+    groups: [...commitees, revueBoard].filter(Boolean),
   };
 };
 
 const mapDispatchToProps = {
   sendContactMessage,
   addToast,
-  reset,
-  change,
 };
 export default compose(
   withPreparedDispatch('fetchContact', (_, dispatch) =>
@@ -47,13 +36,5 @@ export default compose(
       dispatch(fetchGroup(revueBoardGroupId, { propagateError: false })),
     ])
   ),
-  connect(mapStateToProps, mapDispatchToProps),
-  reduxForm({
-    form: 'contactForm',
-    validate,
-    enableReinitialize: true,
-    initialValues: {
-      anonymous: false,
-    },
-  })
+  connect(mapStateToProps, mapDispatchToProps)
 )(Contact);
