70f5599b1458716d7ec75145d306bf8e9828cb1c
Ivar Nakken <ivarnakken1441@gmail.com>
2023-09-23 18:26:17 +0200

    Refactor interest group routes to `react-router` v6

--- app/routes/interestgroups/components/InterestGroupEdit.tsx
+++ app/routes/interestgroups/components/InterestGroupEdit.tsx
@@ -1,24 +1,25 @@
 import { LoadingIndicator } from '@webkom/lego-bricks';
 import { usePreparedEffect } from '@webkom/react-prepare';
 import { Helmet } from 'react-helmet-async';
-import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom-v5-compat';
 import { fetchGroup } from 'app/actions/GroupActions';
 import { Content } from 'app/components/Content';
 import { LoginPage } from 'app/components/LoginForm';
 import NavigationTab from 'app/components/NavigationTab';
-import { selectIsLoggedIn } from 'app/reducers/auth';
 import { selectGroup } from 'app/reducers/groups';
-import GroupForm from 'app/routes/admin/groups/components/GroupForm';
+import GroupForm from 'app/routes/admin/groups/components/GroupSettings';
 import { useAppDispatch, useAppSelector } from 'app/store/hooks';
 
 const InterestGroupEdit = () => {
-  const isLoggedIn = useAppSelector(selectIsLoggedIn);
   const { groupId } = useParams<{ groupId: string }>();
   const interestGroup = useAppSelector((state) =>
     selectGroup(state, { groupId })
   );
+  const editing = groupId !== undefined;
+  const isLoggedIn = useAppSelector(selectIsLoggedIn);
 
   const dispatch = useAppDispatch();
+
   usePreparedEffect(
     'fetchInterestGroupEdit',
     () => {
@@ -35,14 +36,18 @@ const InterestGroupEdit = () => {
     return <LoadingIndicator loading />;
   }
 
+  const title = editing
+    ? `Redigerer${interestGroup ? `: ${interestGroup?.name}` : ''}`
+    : 'Opprett gruppe';
+
   return (
     <Content>
-      <Helmet title={`Redigerer: ${interestGroup.name}`} />
+      <Helmet title={title} />
       <NavigationTab
-        title={`Redigerer: ${interestGroup.name}`}
+        title={title}
         back={{
           label: 'Tilbake',
-          path: `/interest-groups/${interestGroup.id}`,
+          path: `/interest-groups/${editing ? groupId : ''}`,
         }}
       />
       <GroupForm isInterestGroup />
