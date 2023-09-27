67661aa9dc045926602205adb87e98f09a401f16
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2023-09-27 21:08:10 +0200

    Refactor timeline route to `react-router` v6

--- app/routes/timeline/components/TimelinePage.tsx
+++ app/routes/timeline/components/TimelinePage.tsx
@@ -1,25 +1,51 @@
 import { LoadingIndicator } from '@webkom/lego-bricks';
+import { usePreparedEffect } from '@webkom/react-prepare';
 import { Helmet } from 'react-helmet-async';
+import { fetchPersonalFeed } from 'app/actions/FeedActions';
 import { Content } from 'app/components/Content';
 import Feed from 'app/components/Feed';
+import { LoginPage } from 'app/components/LoginForm';
+import {
+  selectFeedActivitesByFeedId,
+  selectFeedById,
+} from 'app/reducers/feeds';
+import { useUserContext } from 'app/routes/app/AppRoute';
+import { useAppDispatch, useAppSelector } from 'app/store/hooks';
 
-type Props = {
-  feedItems: Array<any>;
-  feed: Record<string, any>;
-};
+const TimelinePage = () => {
+  const { loggedIn } = useUserContext();
+  const dispatch = useAppDispatch();
 
-const TimelinePage = (props: Props) => {
-  const { feed, feedItems } = props;
+  usePreparedEffect(
+    'fetchTimeline',
+    () => {
+      loggedIn && dispatch(fetchPersonalFeed());
+    },
+    [loggedIn]
+  );
+
+  const feed = useAppSelector((state) =>
+    selectFeedById(state, {
+      feedId: 'personal',
+    })
+  );
+
+  const feedItems = useAppSelector((state) =>
+    selectFeedActivitesByFeedId(state, {
+      feedId: 'personal',
+    })
+  );
+
+  if (!loggedIn) {
+    return <LoginPage />;
+  }
+
   return (
     <Content>
       <Helmet title="Tidslinje" />
       <h1>Tidslinje</h1>
 
-      {feed ? (
-        <Feed items={feedItems} feed={feed} />
-      ) : (
-        <LoadingIndicator loading />
-      )}
+      {feed ? <Feed items={feedItems} /> : <LoadingIndicator loading />}
     </Content>
   );
 };
