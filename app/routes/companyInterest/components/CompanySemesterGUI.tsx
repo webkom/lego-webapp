4f6369b2fade76bd6cd6b6a65da548336790fac3
Juni Weisteen Bjerde <juni.w.bjerde@gmail.com>
2023-09-28 15:22:07 +0200

    Rewrite CompanySemesterGUI to use react-final-form

--- app/routes/companyInterest/components/CompanySemesterGUI.tsx
+++ app/routes/companyInterest/components/CompanySemesterGUI.tsx
@@ -1,12 +1,10 @@
 import { Button } from '@webkom/lego-bricks';
-import { reset, Field } from 'redux-form';
 import { Content } from 'app/components/Content';
 import {
   Form,
   TextInput,
   RadioButton,
   MultiSelectGroup,
-  legoForm,
 } from 'app/components/Form';
 import Icon from 'app/components/Icon';
 import Flex from 'app/components/Layout/Flex';
@@ -15,6 +13,8 @@ import { createValidator, required } from 'app/utils/validation';
 import { semesterToText, SemesterNavigation } from '../utils';
 import styles from './CompanyInterest.css';
 import type { FormProps } from 'redux-form';
+import LegoFinalForm from 'app/components/Form/LegoFinalForm';
+import { Field } from 'react-final-form';
 
 type Props = {
   onSubmit: (arg0: CompanySemesterEntity) => Promise<any>;
@@ -27,6 +27,11 @@ type Props = {
   addSemester: (arg0: CompanySemesterEntity) => void;
   activeSemesters: Array<CompanySemesterEntity>;
 } & FormProps;
+
+const validate = createValidator({
+  year: [required()],
+  semester: [required()],
+});
 
 const CompanySemesterGUI = (props: Props) => {
   const activeSemesters = (semesters) => (
@@ -48,83 +53,84 @@ const CompanySemesterGUI = (props: Props) => {
     </Flex>
   );
 
-  const { handleSubmit, autoFocus } = props;
+  const onSubmit = ({ year, semester }) => {
+    const { semesters, addSemester, editSemester } = props;
+    const existingCompanySemester = semesters.find((companySemester) => {
+      return (
+        Number(companySemester.year) === Number(year) &&
+        companySemester.semester === semester
+      );
+    });
+    if (existingCompanySemester)
+      return editSemester({
+        ...existingCompanySemester,
+        activeInterestForm: true,
+      });
+    else
+      return addSemester({
+        year,
+        semester,
+      }); // Default is activeInterestForm: true
+  };
+
+  const { autoFocus, initialValues } = props;
   return (
     <Content>
       <SemesterNavigation title="Endre aktive semestre" />
-      <Form onSubmit={handleSubmit}>
-        <Flex className={styles.guiWrapper}>
-          <Flex
-            column
-            style={{
-              marginRight: '50px',
-            }}
-          >
-            <label className={styles.heading}>Legg til aktivt semester</label>
-            <Field
-              autoFocus={autoFocus}
-              placeholder="2020"
-              label="År"
-              name="year"
-              type="number"
-              component={TextInput.Field}
-              className={styles.yearForm}
-              required
-            />
-            <MultiSelectGroup name="semester" label="Semester">
-              <Field
-                name="Spring"
-                label="Vår"
-                component={RadioButton.Field}
-                inputValue="spring"
-              />
-              <Field
-                name="Autumn"
-                label="Høst"
-                component={RadioButton.Field}
-                inputValue="autumn"
-              />
-            </MultiSelectGroup>
-            <Button submit>Legg til semester</Button>
-          </Flex>
-          <Flex column>
-            <label className={styles.heading}>Deaktiver semestre</label>
-            {activeSemesters(props.activeSemesters)}
-          </Flex>
-        </Flex>
-      </Form>
+      <LegoFinalForm
+        onSubmit={onSubmit}
+        validate={validate}
+        initialValues={initialValues}
+        subscription={{}}
+      >
+        {({ handleSubmit }) => (
+          <Form onSubmit={handleSubmit}>
+            <Flex className={styles.guiWrapper}>
+              <Flex
+                column
+                style={{
+                  marginRight: '50px',
+                }}
+              >
+                <label className={styles.heading}>
+                  Legg til aktivt semester
+                </label>
+                <Field
+                  autoFocus={autoFocus}
+                  placeholder="2020"
+                  label="År"
+                  name="year"
+                  type="number"
+                  component={TextInput.Field}
+                  className={styles.yearForm}
+                  required
+                />
+                <MultiSelectGroup name="semester" label="Semester">
+                  <Field
+                    name="Spring"
+                    label="Vår"
+                    component={RadioButton.Field}
+                    inputValue="spring"
+                  />
+                  <Field
+                    name="Autumn"
+                    label="Høst"
+                    component={RadioButton.Field}
+                    inputValue="autumn"
+                  />
+                </MultiSelectGroup>
+                <Button submit>Legg til semester</Button>
+              </Flex>
+              <Flex column>
+                <label className={styles.heading}>Deaktiver semestre</label>
+                {activeSemesters(props.activeSemesters)}
+              </Flex>
+            </Flex>
+          </Form>
+        )}
+      </LegoFinalForm>
     </Content>
   );
 };
 
-const onSubmit = ({ year, semester }, dispatch, props: Props) => {
-  const { semesters, addSemester, editSemester } = props;
-  const existingCompanySemester = semesters.find((companySemester) => {
-    return (
-      Number(companySemester.year) === Number(year) &&
-      companySemester.semester === semester
-    );
-  });
-  if (existingCompanySemester)
-    return editSemester({
-      ...existingCompanySemester,
-      activeInterestForm: true,
-    });
-  else
-    return addSemester({
-      year,
-      semester,
-    }); // Default is activeInterestForm: true
-};
-
-const validate = createValidator({
-  year: [required()],
-  semester: [required()],
-});
-export default legoForm({
-  form: 'addCompanySemester',
-  onSubmitSuccess: (result, dispatch) => dispatch(reset('addCompanySemester')),
-  onSubmit,
-  enableReinitialize: true,
-  validate,
-})(CompanySemesterGUI);
+export default CompanySemesterGUI;
