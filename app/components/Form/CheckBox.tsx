16b419c9d227ebd70bf63ba995dcbd578c8d39da
Ivar Nakken <ivarnakken1441@gmail.com>
2023-11-03 01:48:06 +0100

    Make checkboxes focusable
    With the changes, it is possible to tab through checkboxes and select
    them.
    
    Turned out to be impossible to do for radio buttons, as essentially according to W3C; a radio button is a group that functions as a single element since it retains only a single value. Tabbing to a radio group will bring you to the first item and then using the arrow keys you navigate within the group.
    
    However, I did add the similar logic for radio buttons in case there is
    no default value to the radio button group (e.g. in the company interest
    form). Despite this being bad practice.

--- app/components/Form/CheckBox.tsx
+++ app/components/Form/CheckBox.tsx
@@ -3,7 +3,7 @@ import cx from 'classnames';
 import styles from './CheckBox.css';
 import { createField } from './Field';
 import type { FormProps } from './Field';
-import type { InputHTMLAttributes } from 'react';
+import type { InputHTMLAttributes, KeyboardEvent } from 'react';
 
 type Props = InputHTMLAttributes<HTMLInputElement> & {
   id?: string;
@@ -31,10 +31,26 @@ const CheckBox = ({
 }: Props) => {
   checked = checked ?? value;
   const normalizedValue = inverted ? !checked : checked;
+
+  const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+
+      const inputElement = event.target as HTMLInputElement;
+      inputElement.click();
+    }
+  };
+
   return (
     <Flex wrap alignItems="center" gap={7}>
       <div className={cx(styles.checkbox, styles.bounce, className)}>
-        <input {...props} id={id} checked={normalizedValue} type="checkbox" />
+        <input
+          {...props}
+          id={id}
+          checked={normalizedValue}
+          type="checkbox"
+          onKeyDown={handleKeyDown}
+        />
         <svg viewBox="0 0 21 21">
           <polyline points="5 10.75 8.5 14.25 16 6" />
         </svg>
