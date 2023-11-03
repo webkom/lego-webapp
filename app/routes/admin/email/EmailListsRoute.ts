b19b0e079728352f06c4b8796ae606b48740c942
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2023-11-03 18:55:12 +0100

    Refactor query handling and rewrite EmailLists to function component

--- app/routes/admin/email/EmailListsRoute.ts
+++ app/routes/admin/email/EmailListsRoute.ts
@@ -1,23 +1,24 @@
 import { push } from 'connected-react-router';
-import qs from 'qs';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { fetch } from 'app/actions/EmailListActions';
 import { selectEmailLists } from 'app/reducers/emailLists';
 import { selectPaginationNext } from 'app/reducers/selectors';
+import { parseQueryString } from 'app/utils/useQuery';
 import withPreparedDispatch from 'app/utils/withPreparedDispatch';
 import EmailLists from './components/EmailLists';
 
+export const emailListsDefaultQuery = {
+  name: '',
+  email: '',
+  requireInternalAddress: undefined as undefined | 'true' | 'false',
+};
+
 const mapStateToProps = (state) => {
-  const { search } = state.router.location;
-  const { filters: qsFilters } = qs.parse(search.slice(1));
-  const filters = JSON.parse(typeof qsFilters === 'string' ? qsFilters : '{}');
-  const { name, email, requireInternalAddress } = filters;
-  const query = {
-    name,
-    email,
-    requireInternalAddress,
-  };
+  const query = parseQueryString(
+    state.router.location.search,
+    emailListsDefaultQuery
+  );
   const { pagination } = selectPaginationNext({
     endpoint: '/email-lists/',
     entity: 'emailLists',
@@ -31,7 +32,6 @@ const mapStateToProps = (state) => {
     hasMore: pagination.hasMore,
     pagination,
     query,
-    filters,
   };
 };
 
