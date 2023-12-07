a8638b90f6c9f510246e660f0885bf6cf86bb3e2
Ivar Nakken <ivarnakken1441@gmail.com>
2023-12-07 21:46:05 +0100

    Clean up the user confirmation jsx
    Styling changes, and removal of unnecessary stuff.

--- app/routes/users/components/UserConfirmation.tsx
+++ app/routes/users/components/UserConfirmation.tsx
@@ -1,6 +1,7 @@
-import { Card, Container, Flex } from '@webkom/lego-bricks';
+import { Card, Flex } from '@webkom/lego-bricks';
 import { Field } from 'react-final-form';
 import { Link } from 'react-router-dom';
+import { Content } from 'app/components/Content';
 import {
   TextInput,
   MultiSelectGroup,
@@ -17,8 +18,8 @@ import styles from './UserConfirmation.css';
 import type { UserEntity } from 'app/reducers/users';
 import type { RouteChildrenProps } from 'react-router';
 
-export type Props = {
-  token: string;
+type Props = {
+  token: EncodedToken;
   user: UserEntity;
   createUser: (token: string, data: Record<string, any>) => void;
   submitSucceeded: boolean;
@@ -34,134 +35,110 @@ const UserConfirmationForm = ({
 
   if (submitSucceeded) {
     return (
-      <Container>
-        <div className={styles.root}>
-          <Flex wrap justifyContent="center">
-            <div>
-              <h2>Du er nå registrert!</h2>
-              <h3
-                style={{
-                  margin: 0,
-                }}
-              >
-                Er du student?
-              </h3>
-              <Card severity="warning">
-                <span>
-                  For å kunne melde deg på arrangementer i Abakus må du
-                  verifisere at du er student.
-                </span>
-              </Card>
-              <Flex>
-                <Link to="/users/me/settings/student-confirmation/">
-                  <Button success>Verifiser studentstatus</Button>
-                </Link>
-              </Flex>
-              <Flex>
-                <Link
-                  to="/"
-                  style={{
-                    marginTop: '1em',
-                  }}
-                >
-                  <Button>Eller gå til hovedsiden</Button>
-                </Link>
-              </Flex>
-            </div>
-          </Flex>
-        </div>
-      </Container>
+      <Content>
+        <h1>Du er nå registrert!</h1>
+        <Card severity="warning">
+          <Card.Header>Er du student?</Card.Header>
+          <span>
+            For å kunne melde deg på arrangementer i Abakus må du verifisere at
+            du er student.
+          </span>
+        </Card>
+        <Flex column gap="1rem">
+          <Link to="/users/me/settings/student-confirmation/">
+            <Button success>Verifiser studentstatus</Button>
+          </Link>
+          <Link to="/">
+            <Button>Eller gå til hovedsiden</Button>
+          </Link>
+        </Flex>
+      </Content>
     );
   }
 
   if (!token) {
     return (
-      <Container>
-        <div className={styles.root}>
-          <h2>Token ikke gyldig</h2>
-        </div>
-      </Container>
+      <Content>
+        <Card severity="danger">
+          <Card.Header>Token ikke gyldig</Card.Header>
+        </Card>
+      </Content>
     );
   }
 
   return (
-    <Container>
-      <div className={styles.root}>
-        <h2>Registrer bruker</h2>
-        <LegoFinalForm onSubmit={onSubmit} validate={validate}>
-          {({ handleSubmit }) => (
-            <form onSubmit={handleSubmit}>
+    <Content>
+      <h1>Registrer bruker</h1>
+      <LegoFinalForm onSubmit={onSubmit} validate={validate}>
+        {({ handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <Field
+              name="username"
+              placeholder="Brukernavn"
+              label="Brukernavn"
+              component={TextInput.Field}
+            />
+            <PasswordField user={user} />
+            <Field
+              label="Gjenta passord"
+              name="retypePassword"
+              type="password"
+              autocomplete="new-password"
+              component={TextInput.Field}
+            />
+            <Field
+              name="firstName"
+              placeholder="Fornavn"
+              label="Fornavn"
+              autocomplete="given-name additional-name"
+              component={TextInput.Field}
+            />
+            <Field
+              name="lastName"
+              label="Etternavn"
+              placeholder="Etternavn"
+              autocomplete="family-name"
+              component={TextInput.Field}
+            />
+            <MultiSelectGroup label="Kjønn" name="gender">
               <Field
-                name="username"
-                placeholder="Brukernavn"
-                label="Brukernavn"
-                component={TextInput.Field}
-              />
-              <PasswordField user={user} />
-              <Field
-                label="Passord (gjenta)"
-                name="retypePassword"
-                type="password"
-                autocomplete="new-password"
-                component={TextInput.Field}
+                name="genderMan"
+                label="Mann"
+                component={RadioButton.Field}
+                inputValue="male"
               />
               <Field
-                name="firstName"
-                placeholder="Fornavn"
-                label="Fornavn"
-                autocomplete="given-name additional-name"
-                component={TextInput.Field}
+                name="genderWoman"
+                label="Kvinne"
+                component={RadioButton.Field}
+                inputValue="female"
               />
               <Field
-                name="lastName"
-                label="Etternavn"
-                placeholder="Etternavn"
-                autocomplete="family-name"
-                component={TextInput.Field}
+                name="genderOther"
+                label="Annet"
+                component={RadioButton.Field}
+                inputValue="other"
               />
-              <MultiSelectGroup label="Kjønn" name="gender">
-                <Field
-                  name="genderMan"
-                  label="Mann"
-                  component={RadioButton.Field}
-                  inputValue="male"
-                />
-                <Field
-                  name="genderWoman"
-                  label="Kvinne"
-                  component={RadioButton.Field}
-                  inputValue="female"
-                />
-                <Field
-                  name="genderOther"
-                  label="Annet"
-                  component={RadioButton.Field}
-                  inputValue="other"
-                />
-              </MultiSelectGroup>
-              <Field
-                name="allergies"
-                placeholder="Allergier"
-                label="Matallergier/preferanser"
-                component={TextInput.Field}
-              />
+            </MultiSelectGroup>
+            <Field
+              name="allergies"
+              placeholder="Allergier"
+              label="Matallergier/preferanser"
+              component={TextInput.Field}
+            />
 
-              <Field
-                label="Telefonnummer"
-                name="phoneNumber"
-                autocomplete="tel"
-                component={PhoneNumberInput.Field}
-              />
-              {spySubmittable((submittable) => (
-                <Button submit disabled={!submittable}>
-                  Registrer bruker
-                </Button>
-              ))}
-            </form>
-          )}
-        </LegoFinalForm>
-      </div>
-    </Container>
+            <Field
+              label="Telefonnummer"
+              name="phoneNumber"
+              autocomplete="tel"
+              component={PhoneNumberInput.Field}
+            />
+
+            <SubmitButton>Registrer bruker</SubmitButton>
+          </form>
+        )}
+      </LegoFinalForm>
+    </Content>
   );
 };
 
