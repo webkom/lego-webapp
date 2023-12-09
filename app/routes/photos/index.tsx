91c1ed2b575a77a2f81653fd91204c25aadc98ab
Ivar Nakken <ivarnakken1441@gmail.com>
2023-12-09 16:12:09 +0100

    Refactor gallery list to support `react-router` v6

--- app/routes/photos/index.tsx
+++ app/routes/photos/index.tsx
@@ -1,84 +1,75 @@
-import { Route, Switch } from 'react-router-dom';
+import { useRouteMatch, Route, Switch } from 'react-router-dom';
+import { CompatRoute } from 'react-router-dom-v5-compat';
 import RouteWrapper from 'app/components/RouteWrapper';
 import { UserContext } from 'app/routes/app/AppRoute';
 import PageNotFound from '../pageNotFound';
 import GalleryCreateRoute from './GalleryCreateRoute';
 import GalleryDetailRoute from './GalleryDetailRoute';
 import GalleryEditRoute from './GalleryEditRoute';
-import GalleryListRoute from './GalleryListRoute';
 import GalleryPictureEditRoute from './GalleryPictureEditRoute';
 import GalleryPictureRoute from './GalleryPictureRoute';
+import Overview from './components/Overview';
 
-const photosRoute = ({
-  match,
-}: {
-  match: {
-    path: string;
-  };
-}) => (
-  <UserContext.Consumer>
-    {({ currentUser, loggedIn }) => (
-      <Switch>
-        <RouteWrapper
-          exact
-          path={`${match.path}`}
-          passedProps={{
-            currentUser,
-            loggedIn,
-          }}
-          Component={GalleryListRoute}
-        />
-        <RouteWrapper
-          exact
-          path={`${match.path}/new`}
-          passedProps={{
-            currentUser,
-            loggedIn,
-          }}
-          Component={GalleryCreateRoute}
-        />
-        <RouteWrapper
-          exact
-          path={`${match.path}/:galleryId`}
-          passedProps={{
-            currentUser,
-            loggedIn,
-          }}
-          Component={GalleryDetailRoute}
-        />
-        <RouteWrapper
-          exact
-          path={`${match.path}/:galleryId/edit`}
-          passedProps={{
-            currentUser,
-            loggedIn,
-          }}
-          Component={GalleryEditRoute}
-        />
-        <RouteWrapper
-          exact
-          path={`${match.path}/:galleryId/picture/:pictureId`}
-          passedProps={{
-            currentUser,
-            loggedIn,
-          }}
-          Component={GalleryPictureRoute}
-        />
-        <RouteWrapper
-          exact
-          path={`${match.path}/:galleryId/picture/:pictureId/edit`}
-          passedProps={{
-            currentUser,
-            loggedIn,
-          }}
-          Component={GalleryPictureEditRoute}
-        />
-        <Route component={PageNotFound} />
-      </Switch>
-    )}
-  </UserContext.Consumer>
-);
+const PhotosRoute = () => {
+  const { path } = useRouteMatch();
+
+  return (
+    <UserContext.Consumer>
+      {({ currentUser, loggedIn }) => (
+        <Switch>
+          <CompatRoute exact path={`${path}`} component={Overview} />
+          <RouteWrapper
+            exact
+            path={`${path}/new`}
+            passedProps={{
+              currentUser,
+              loggedIn,
+            }}
+            Component={GalleryCreateRoute}
+          />
+          <RouteWrapper
+            exact
+            path={`${path}/:galleryId`}
+            passedProps={{
+              currentUser,
+              loggedIn,
+            }}
+            Component={GalleryDetailRoute}
+          />
+          <RouteWrapper
+            exact
+            path={`${path}/:galleryId/edit`}
+            passedProps={{
+              currentUser,
+              loggedIn,
+            }}
+            Component={GalleryEditRoute}
+          />
+          <RouteWrapper
+            exact
+            path={`${path}/:galleryId/picture/:pictureId`}
+            passedProps={{
+              currentUser,
+              loggedIn,
+            }}
+            Component={GalleryPictureRoute}
+          />
+          <RouteWrapper
+            exact
+            path={`${path}/:galleryId/picture/:pictureId/edit`}
+            passedProps={{
+              currentUser,
+              loggedIn,
+            }}
+            Component={GalleryPictureEditRoute}
+          />
+          <Route component={PageNotFound} />
+        </Switch>
+      )}
+    </UserContext.Consumer>
+  );
+};
 
 export default function Photos() {
-  return <Route path="/photos" component={photosRoute} />;
+  return <Route path="/photos" component={PhotosRoute} />;
 }
