1d0e1f771ab1aeacbfdb0906a780c7fc6b9890f7
Ivar Nakken <ivarnakken1441@gmail.com>
2023-12-13 15:58:55 +0100

    Refactor meeting list to support `react-router` v6

--- app/routes/meetings/index.tsx
+++ app/routes/meetings/index.tsx
@@ -1,62 +1,52 @@
-import { Route, Switch } from 'react-router-dom';
+import { useRouteMatch, Route, Switch } from 'react-router-dom';
 import RouteWrapper from 'app/components/RouteWrapper';
 import { UserContext } from 'app/routes/app/AppRoute';
 import PageNotFound from '../pageNotFound';
 import MeetingCreateRoute from './MeetingCreateRoute';
 import MeetingDetailRoute from './MeetingDetailRoute';
 import MeetingEditRoute from './MeetingEditRoute';
-import MeetingListRoute from './MeetingListRoute';
+import MeetingList from './components/MeetingList';
 
-const meetingRoute = ({
-  match,
-}: {
-  match: {
-    path: string;
-  };
-}) => (
-  <UserContext.Consumer>
-    {({ currentUser, loggedIn }) => (
-      <Switch>
-        <RouteWrapper
-          exact
-          path={`${match.path}`}
-          passedProps={{
-            currentUser,
-            loggedIn,
-          }}
-          Component={MeetingListRoute}
-        />
-        <RouteWrapper
-          path={`${match.path}/create`}
-          passedProps={{
-            currentUser,
-            loggedIn,
-          }}
-          Component={MeetingCreateRoute}
-        />
-        <RouteWrapper
-          exact
-          path={`${match.path}/:meetingId`}
-          passedProps={{
-            currentUser,
-            loggedIn,
-          }}
-          Component={MeetingDetailRoute}
-        />
-        <RouteWrapper
-          path={`${match.path}/:meetingId/edit`}
-          passedProps={{
-            currentUser,
-            loggedIn,
-          }}
-          Component={MeetingEditRoute}
-        />
-        <Route component={PageNotFound} />
-      </Switch>
-    )}
-  </UserContext.Consumer>
-);
+const MeetingRoute = () => {
+  const { path } = useRouteMatch();
+
+  return (
+    <UserContext.Consumer>
+      {({ currentUser, loggedIn }) => (
+        <Switch>
+          <Route exact path={`${path}`} component={MeetingList} />
+          <RouteWrapper
+            path={`${path}/create`}
+            passedProps={{
+              currentUser,
+              loggedIn,
+            }}
+            Component={MeetingCreateRoute}
+          />
+          <RouteWrapper
+            exact
+            path={`${path}/:meetingId`}
+            passedProps={{
+              currentUser,
+              loggedIn,
+            }}
+            Component={MeetingDetailRoute}
+          />
+          <RouteWrapper
+            path={`${path}/:meetingId/edit`}
+            passedProps={{
+              currentUser,
+              loggedIn,
+            }}
+            Component={MeetingEditRoute}
+          />
+          <Route component={PageNotFound} />
+        </Switch>
+      )}
+    </UserContext.Consumer>
+  );
+};
 
 export default function Meetings() {
-  return <Route path="/meetings" component={meetingRoute} />;
+  return <Route path="/meetings" component={MeetingRoute} />;
 }
