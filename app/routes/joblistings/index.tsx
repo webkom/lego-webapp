82b6b6c22d075e0842ca3e7d60c3e63d0ec4fc38
Ivar Nakken <ivarnakken1441@gmail.com>
2023-12-24 00:34:38 +0900

    Replace Switch with Routes in /joblistings
    Also fix the fetching of company contacts. `company.value` is no such
    thing, so this would cause a fetch to an undefined endpoint.

--- app/routes/joblistings/index.tsx
+++ app/routes/joblistings/index.tsx
@@ -1,31 +1,17 @@
-import { useRouteMatch, Route, Switch } from 'react-router-dom';
-import { CompatRoute } from 'react-router-dom-v5-compat';
+import { Route, Routes } from 'react-router-dom-v5-compat';
 import PageNotFound from '../pageNotFound';
 import JoblistingDetail from './components/JoblistingDetail';
 import JoblistingEditor from './components/JoblistingEditor';
 import JoblistingsPage from './components/JoblistingPage';
 
-const JobListingRoute = () => {
-  const { path } = useRouteMatch();
+const JobListingsRoute = () => (
+  <Routes>
+    <Route index element={<JoblistingsPage />} />
+    <Route path="create" element={<JoblistingEditor />} />
+    <Route path=":joblistingIdOrSlug" element={<JoblistingDetail />} />
+    <Route path=":joblistingId/edit" element={<JoblistingEditor />} />
+    <Route path="*" element={<PageNotFound />} />
+  </Routes>
+);
 
-  return (
-    <Switch>
-      <CompatRoute exact path={path} component={JoblistingsPage} />
-      <CompatRoute path={`${path}/create`} component={JoblistingEditor} />
-      <CompatRoute
-        exact
-        path={`${path}/:joblistingIdOrSlug`}
-        component={JoblistingDetail}
-      />
-      <CompatRoute
-        path={`${path}/:joblistingId/edit`}
-        component={JoblistingEditor}
-      />
-      <Route component={PageNotFound} />
-    </Switch>
-  );
-};
-
-export default function Joblistings() {
-  return <Route path="/joblistings" component={JobListingRoute} />;
-}
+export default JobListingsRoute;
