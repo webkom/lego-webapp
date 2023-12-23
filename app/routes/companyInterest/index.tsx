f10a1b425a3f335fab8f06d1cb1175632a847387
Ivar Nakken <ivarnakken1441@gmail.com>
2023-12-24 01:06:56 +0900

    Replace Switch with Routes in various routes
    /timeline, /tags, /companies, /register-interest and /companyInterests.

--- app/routes/companyInterest/index.tsx
+++ app/routes/companyInterest/index.tsx
@@ -1,38 +1,17 @@
-import { useRouteMatch, Route, Switch } from 'react-router-dom';
-import { CompatRoute } from 'react-router-dom-v5-compat';
+import { Route, Routes } from 'react-router-dom-v5-compat';
 import PageNotFound from 'app/routes/pageNotFound';
 import CompanyInterestList from './components/CompanyInterestList';
 import CompanyInterestPage from './components/CompanyInterestPage';
 import CompanySemesterGUI from './components/CompanySemesterGUI';
 
-const CompanyInterestRoute = () => {
-  const { path } = useRouteMatch();
+const CompanyInterestRoute = () => (
+  <Routes>
+    <Route index element={<CompanyInterestList />} />
+    <Route path="create" element={<CompanyInterestPage />} />
+    <Route path="semesters" element={<CompanySemesterGUI />} />
+    <Route path=":companyInterestId/edit" element={<CompanyInterestPage />} />
+    <Route path="*" element={<PageNotFound />} />
+  </Routes>
+);
 
-  return (
-    <Switch>
-      <CompatRoute exact path={path} component={CompanyInterestList} />
-      <CompatRoute
-        exact
-        path={`${path}/create`}
-        component={CompanyInterestPage}
-      />
-      <Route exact path={`${path}/semesters`} component={CompanySemesterGUI} />
-      <CompatRoute
-        exact
-        path={`${path}/:companyInterestId/edit`}
-        component={CompanyInterestPage}
-      />
-      <Route component={PageNotFound} />
-    </Switch>
-  );
-};
-
-export const CompanyInterestInfoRoute = () => {
-  const { path } = useRouteMatch();
-
-  return <Route exact path={path} component={CompanyInterestPage} />;
-};
-
-export function CompanyInterest() {
-  return <Route path="/companyInterest" component={CompanyInterestRoute} />;
-}
+export default CompanyInterestRoute;
