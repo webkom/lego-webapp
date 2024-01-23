84c32aa4c492808e775de7a914949798f6278b02
Ivar Nakken <ivarnakken1441@gmail.com>
2024-01-23 17:10:16 +0100

    Refactor forgot password form to `react-final-form`

--- app/components/LoginForm/ForgotPasswordForm.tsx
+++ app/components/LoginForm/ForgotPasswordForm.tsx
@@ -1,86 +1,56 @@
-import { Button } from '@webkom/lego-bricks';
-import { Component } from 'react';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
-import { reduxForm, Field, SubmissionError } from 'redux-form';
+import { useEffect, useState } from 'react';
+import { Field } from 'react-final-form';
 import { sendForgotPasswordEmail } from 'app/actions/UserActions';
+import { useAppDispatch } from 'app/store/hooks';
 import { createValidator, required, isEmail } from 'app/utils/validation';
-import { Form, TextInput } from '../Form';
-import type { FormProps } from 'redux-form';
-
-type Props = {
-  sendForgotPasswordEmail: (arg0: { email: string }) => any;
-} & FormProps;
-type State = {
-  submitted: boolean;
-};
-
-class ForgotPasswordForm extends Component<Props, State> {
-  mounted = false;
-  state = {
-    submitted: false,
-  };
-
-  componentDidMount() {
-    this.mounted = true;
-  }
-
-  componentWillUnmount() {
-    this.mounted = false;
-  }
-
-  onSubmit = (data) => {
-    return this.props
-      .sendForgotPasswordEmail(data)
-      .then(() => {
-        if (this.mounted) {
-          this.setState({
-            submitted: true,
-          });
-        }
-      })
-      .catch((err) => {
-        if (this.mounted && err.payload && err.payload.response) {
-          throw new SubmissionError(err.payload.response.jsonData);
-        }
-      });
-  };
-
-  render() {
-    const { handleSubmit, invalid, pristine, submitting } = this.props;
-
-    if (this.state.submitted) {
-      return (
-        <div>
-          Vi har sendt en e-post til deg med informasjon om hvordan du kan
-          tilbakestille passordet.
-        </div>
-      );
-    }
-
-    return (
-      <Form
-        onSubmit={handleSubmit(this.onSubmit)}
-        onClick={(e) => e.stopPropagation()}
-      >
-        <Field name="email" component={TextInput.Field} placeholder="E-post" />
-        <Button danger submit disabled={invalid || pristine || submitting}>
-          Tilbakestill passord
-        </Button>
-      </Form>
-    );
-  }
-}
+import { Form, LegoFinalForm, SubmitButton, TextInput } from '../Form';
 
 const validate = createValidator({
   email: [required(), isEmail()],
 });
-export default compose(
-  connect<any, any, any, any, any, any>(null, {
-    sendForgotPasswordEmail,
-  }),
-  reduxForm({
-    form: 'ForgotPasswordForm',
-    validate,
-  })
-)(ForgotPasswordForm);
+
+const ForgotPasswordForm = () => {
+  const [mounted, setMounted] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+    return () => setMounted(false);
+  }, []);
+
+  const dispatch = useAppDispatch();
+
+  const onSubmit = (data) => {
+    dispatch(sendForgotPasswordEmail(data)).then(() => {
+      if (mounted) {
+        setSubmitted(true);
+      }
+    });
+  };
+
+  if (submitted) {
+    return (
+      <div>
+        Vi har sendt en e-post til deg med informasjon om hvordan du kan
+        tilbakestille passordet
+      </div>
+    );
+  }
+
+  return (
+    <LegoFinalForm onSubmit={onSubmit} validate={validate}>
+      {({ handleSubmit }) => (
+        <Form onSubmit={handleSubmit} onClick={(e) => e.stopPropagation()}>
+          <Field
+            name="email"
+            component={TextInput.Field}
+            placeholder="E-post"
+          />
+          <SubmitButton danger>Tilbakestill passord</SubmitButton>
+        </Form>
+      )}
+    </LegoFinalForm>
+  );
+};
+
+export default ForgotPasswordForm;
