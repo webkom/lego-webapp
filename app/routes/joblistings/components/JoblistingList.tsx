0c5a92f818eaa1415b3f0905bbdc7ec7257f1f61
Ivar Nakken <ivarnakken1441@gmail.com>
2024-02-04 18:43:11 +0100

    Employ the skeleton component on /joblistings

--- app/routes/joblistings/components/JoblistingList.tsx
+++ app/routes/joblistings/components/JoblistingList.tsx
@@ -1,5 +1,7 @@
-import { Flex } from '@webkom/lego-bricks';
+import { Flex, Skeleton } from '@webkom/lego-bricks';
 import JoblistingItem from 'app/components/JoblistingItem';
+import sharedStyles from 'app/components/JoblistingItem/JoblistingItem.css';
+import { useAppSelector } from 'app/store/hooks';
 import styles from './JoblistingList.css';
 import type { ListJoblisting } from 'app/store/models/Joblisting';
 
@@ -7,16 +9,25 @@ type JobListingsListProps = {
   joblistings: ListJoblisting[];
 };
 
-const JoblistingsList = ({ joblistings }: JobListingsListProps) => (
-  <Flex column className={styles.joblistingList}>
-    <Flex className={styles.heading}>
-      <h2 className={styles.headingText}>Jobbannonser</h2>
-      <h4 className={styles.headingDeadline}>Søknadsfrist:</h4>
+const JoblistingsList = ({ joblistings }: JobListingsListProps) => {
+  const fetching = useAppSelector((state) => state.joblistings.fetching);
+
+  return (
+    <Flex column className={styles.joblistingList}>
+      <Flex className={styles.heading}>
+        <h2 className={styles.headingText}>Jobbannonser</h2>
+        <h4 className={styles.headingDeadline}>Søknadsfrist:</h4>
+      </Flex>
+
+      {fetching && !joblistings.length
+        ? [...Array(5)].map((i) => (
+            <Skeleton key={i} className={sharedStyles.joblistingItem} />
+          ))
+        : joblistings.map((joblisting) => (
+            <JoblistingItem key={joblisting.id} joblisting={joblisting} />
+          ))}
     </Flex>
-    {joblistings.map((joblisting) => (
-      <JoblistingItem key={joblisting.id} joblisting={joblisting} />
-    ))}
-  </Flex>
-);
+  );
+};
 
 export default JoblistingsList;
