b79c4ed43f65b04452922773a65a748b7359bf80
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2024-03-04 15:32:19 +0800

    Improve typing of Meta.FETCH and allowed slice

--- app/actions/MetaActions.ts
+++ app/actions/MetaActions.ts
@@ -1,9 +1,43 @@
 import callAPI from 'app/actions/callAPI';
 import { Meta } from './ActionTypes';
-import type { Thunk } from 'app/types';
+import type { ResolvedPromiseAction } from 'app/store/middleware/promiseMiddleware';
 
-export function fetchMeta(): Thunk<any> {
-  return callAPI({
+export type AllowedPages = {
+  announcements: boolean;
+  articles: boolean;
+  bdb: boolean;
+  companies: boolean;
+  email: boolean;
+  events: boolean;
+  forums: boolean;
+  galleries: boolean;
+  groups: boolean;
+  interestGroups: boolean;
+  joblistings: boolean;
+  meetings: boolean;
+  penalties: boolean;
+  polls: boolean;
+  quotes: boolean;
+  surveys: boolean;
+  users: boolean;
+};
+
+type MetaPayload = {
+  site: {
+    name: string;
+    slogan: string;
+    contactEmail: string;
+    documentationUrl: string;
+    domain: string;
+    owner: string;
+  };
+  isAllowed: AllowedPages;
+};
+
+export type FetchMetaSuccessAction = ResolvedPromiseAction<MetaPayload>;
+
+export function fetchMeta() {
+  return callAPI<MetaPayload>({
     types: Meta.FETCH,
     endpoint: '/site-meta/',
     meta: {
