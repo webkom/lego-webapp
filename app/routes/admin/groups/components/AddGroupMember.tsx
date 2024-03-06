4ae6baef402520fdc4fa5cd177500ad35dd8c07f
Vebjorn <vebjorn.elvekrok@gmail.com>
2024-03-06 21:26:17 +0100

    Remove unnecessary refetch

--- app/routes/admin/groups/components/AddGroupMember.tsx
+++ app/routes/admin/groups/components/AddGroupMember.tsx
@@ -1,15 +1,10 @@
 import { Field } from 'react-final-form';
-import {
-  addMember,
-  fetchMembershipsPagination,
-} from 'app/actions/GroupActions';
+import { addMember } from 'app/actions/GroupActions';
 import { Form, LegoFinalForm, SelectInput } from 'app/components/Form';
 import SubmissionError from 'app/components/Form/SubmissionError';
 import { SubmitButton } from 'app/components/Form/SubmitButton';
-import { defaultGroupMembersQuery } from 'app/routes/admin/groups/components/GroupMembers';
 import { useAppDispatch } from 'app/store/hooks';
 import { roleOptions } from 'app/utils/constants';
-import useQuery from 'app/utils/useQuery';
 import { createValidator, required } from 'app/utils/validation';
 
 const validate = createValidator({
@@ -23,7 +18,6 @@ type Props = {
 
 const AddGroupMember = ({ groupId }: Props) => {
   const dispatch = useAppDispatch();
-  const { query } = useQuery(defaultGroupMembersQuery);
 
   const handleSubmit = (values, form) => {
     dispatch(
@@ -32,19 +26,9 @@ const AddGroupMember = ({ groupId }: Props) => {
         userId: values.user.id,
         role: values.role.value,
       })
-    )
-      .then(() =>
-        dispatch(
-          fetchMembershipsPagination({
-            groupId,
-            next: true,
-            query,
-          })
-        )
-      )
-      .then(() => {
-        form.reset();
-      });
+    ).then(() => {
+      form.reset();
+    });
   };
 
   return (
