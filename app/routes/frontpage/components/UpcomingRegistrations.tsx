d0b1d63dc049015096b870ce9afa41f2efdfc5dc
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2024-03-09 00:00:52 +0800

    Refactor upcoming registration component to function component

--- app/routes/frontpage/components/UpcomingRegistrations.tsx
+++ app/routes/frontpage/components/UpcomingRegistrations.tsx
@@ -1,6 +1,6 @@
 import { Flex, Icon, Skeleton } from '@webkom/lego-bricks';
 import moment from 'moment-timezone';
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Tooltip from 'app/components/Tooltip';
 import { selectEvents } from 'app/reducers/events';
@@ -10,85 +10,69 @@ import truncateString from 'app/utils/truncateString';
 import styles from './UpcomingRegistrations.css';
 import type { FrontpageEvent } from 'app/store/models/Event';
 
+const createTimeString = (event: FrontpageEvent) => {
+  const time = moment(event.activationTime);
+  // If it's 1 day left we would like to say 'i morgen' and not 1 day
+  if (moment().add('1', 'day').isSame(time, 'day')) {
+    return 'i morgen';
+  }
+
+  let timeString = '';
+  if (moment().isAfter(time)) {
+    timeString += 'for ';
+  }
+  timeString += time.fromNow();
+  return timeString.replace('minutter', 'min').replace('sekunder', 'sek');
+};
+
 type Props = {
   event: FrontpageEvent;
 };
-type State = {
-  time: string;
+const UpcomingRegistration = ({ event }: Props) => {
+  const [timeString, setTimeString] = useState(createTimeString(event));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeString(createTimeString(event));
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [event]);
+
+  const activeString = moment(event.activationTime).format('LLLL');
+
+  return (
+    <Tooltip content={`Påmelding: ${activeString}`}>
+      <Flex
+        column
+        style={{
+          borderColor: colorForEventType(event.eventType),
+        }}
+        className={styles.eventItem}
+      >
+        <Link to={`/events/${event.slug}`} className={styles.title}>
+          <h4>{truncateString(event.title, 43)}</h4>
+        </Link>
+
+        <Flex
+          alignItems="center"
+          gap="var(--spacing-sm)"
+          className={styles.info}
+        >
+          <Icon name="alarm-outline" size={20} />
+          <div>
+            <span>
+              Påmelding
+              {moment().isBefore(event.activationTime) ? ' åpner' : ' åpnet'}
+            </span>
+            <span className={styles.time}>{timeString}</span>
+          </div>
+        </Flex>
+      </Flex>
+    </Tooltip>
+  );
 };
 
-class EventItem extends Component<Props, State> {
-  state = {
-    time: this.constructor.generateTime(this.props),
-  };
-  interval?: ReturnType<typeof setInterval>;
-
-  static generateTime(props: Props) {
-    const now = moment();
-    const start = moment(props.event && props.event.activationTime);
-    const time = now.to(start);
-    // If it's 1 day left we would like to say 'i morgen' and not 1 day
-    const isTomorrow = moment().add('1', 'day').isSame(start, 'day');
-    return isTomorrow ? 'i morgen' : time;
-  }
-
-  updateTime() {
-    this.setState({
-      time: EventItem.generateTime(this.props),
-    });
-  }
-
-  componentDidMount() {
-    this.updateTime();
-    this.interval = setInterval(() => this.updateTime(), 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  render() {
-    const selected = this.props.event;
-    const activeString = moment(selected.activationTime).format('LLLL');
-    return (
-      <Tooltip content={`Påmelding: ${activeString}`}>
-        <Flex
-          column
-          style={{
-            borderColor: colorForEventType(selected.eventType),
-          }}
-          className={styles.eventItem}
-        >
-          <Link to={`/events/${selected.slug}`} className={styles.title}>
-            <h4>{truncateString(selected.title, 43)}</h4>
-          </Link>
-
-          <Flex
-            alignItems="center"
-            gap="var(--spacing-sm)"
-            className={styles.info}
-          >
-            <Icon name="alarm-outline" size={20} />
-            <div>
-              <span>
-                Påmelding {/*Change based on time*/}
-                {moment().isBefore(selected.activationTime) ? 'åpner' : 'åpnet'}
-              </span>
-              <span className={styles.time}>
-                {/*Add for if the moment has passed*/}
-                {moment().isAfter(selected.activationTime) && 'for '}
-                {/*Minutter is too long of string*/}
-                {this.state.time.replace('minutter', 'min')}
-              </span>
-            </div>
-          </Flex>
-        </Flex>
-      </Tooltip>
-    );
-  }
-} // Component when there is no events
-
-const Filler = () => (
+const NoUpcomingRegistrations = () => (
   <Flex column className={styles.filler}>
     <Icon
       name="eye-off-outline"
@@ -114,7 +98,7 @@ const inRange = (event: FrontpageEvent) => {
   );
 };
 
-const NEXT_EVENTS_LIMIT = 2;
+const UPCOMING_REGISTRATIONS_LIMIT = 2;
 
 const UpcomingRegistrations = () => {
   const events = useAppSelector(selectEvents) as unknown as FrontpageEvent[];
@@ -125,7 +109,7 @@ const UpcomingRegistrations = () => {
     .filter(hasActivation)
     .filter(inRange)
     .sort((a, b) => moment(a.activationTime).diff(moment(b.activationTime)))
-    .splice(0, NEXT_EVENTS_LIMIT);
+    .splice(0, UPCOMING_REGISTRATIONS_LIMIT);
 
   const fetching = useAppSelector(
     (state) => state.frontpage.fetching || state.events.fetching,
@@ -134,11 +118,16 @@ const UpcomingRegistrations = () => {
   return (
     <div className={styles.wrapper}>
       {fetching && !events.length ? (
-        <Skeleton array={NEXT_EVENTS_LIMIT} className={styles.eventItem} />
+        <Skeleton
+          array={UPCOMING_REGISTRATIONS_LIMIT}
+          className={styles.eventItem}
+        />
       ) : orderedEvents.length > 0 ? (
-        orderedEvents.map((event) => <EventItem key={event.id} event={event} />)
+        orderedEvents.map((event) => (
+          <UpcomingRegistration key={event.id} event={event} />
+        ))
       ) : (
-        <Filler />
+        <NoUpcomingRegistrations />
       )}
     </div>
   );
