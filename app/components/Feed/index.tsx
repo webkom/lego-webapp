21162f2ddf34b5f6558487703dd19c42f9603557
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2024-03-11 02:00:43 +0800

    Use createSlice and legoAdapter for feeds redux state

--- app/components/Feed/index.tsx
+++ app/components/Feed/index.tsx
@@ -1,8 +1,10 @@
 import EmptyState from 'app/components/EmptyState';
 import ErrorBoundary from 'app/components/ErrorBoundary';
+import { selectFeedActivitiesByFeedId } from 'app/reducers/feeds';
+import { useAppSelector } from 'app/store/hooks';
 import { FeedActivityVerb } from 'app/store/models/FeedActivity';
 import styles from './Feed.css';
-import AggregatedActivityItem from './activity';
+import Activity from './activity';
 import AdminRegistrationRenderer from './renders/adminRegistration';
 import AnnouncementRenderer from './renders/announcement';
 import CommentRenderer from './renders/comment';
@@ -12,8 +14,9 @@ import GroupJoinRenderer from './renders/group';
 import MeetingInvitationRenderer from './renders/meetingInvitation';
 import RegistrationBumpRenderer from './renders/registrationBump';
 import RestrictedMailSentRenderer from './renders/restrictedMail';
+import type { EntityId } from '@reduxjs/toolkit';
 import type ActivityRenderer from 'app/components/Feed/ActivityRenderer';
-import type AggregatedFeedActivity from 'app/store/models/FeedActivity';
+import type { ReactNode } from 'react';
 
 export const activityRenderers: Record<FeedActivityVerb, ActivityRenderer> = {
   [FeedActivityVerb.Comment]: CommentRenderer,
@@ -28,33 +31,36 @@ export const activityRenderers: Record<FeedActivityVerb, ActivityRenderer> = {
 };
 
 type Props = {
-  items: AggregatedFeedActivity[];
+  feedId: EntityId;
 };
 
-const Feed = ({ items }: Props) => (
-  <div
-    style={{
-      width: '100%',
-    }}
-  >
-    {items.length ? (
-      items.map((item) => {
-        const activityRenderer = activityRenderers[item.verb];
-        return activityRenderer ? (
-          <ErrorBoundary hidden key={item.id}>
-            <AggregatedActivityItem
-              aggregatedActivity={item}
-              activityRenderer={activityRenderer}
-            />
-          </ErrorBoundary>
-        ) : null;
-      })
-    ) : (
-      <EmptyState>
-        <h2 className={styles.emptyState}>Ingen aktiviteter i feeden</h2>
-      </EmptyState>
-    )}
-  </div>
-);
+const Feed = ({ feedId }: Props): ReactNode => {
+  const feedActivities = useAppSelector((state) =>
+    selectFeedActivitiesByFeedId(state, feedId),
+  );
+
+  return (
+    <div
+      style={{
+        width: '100%',
+      }}
+    >
+      {feedActivities.length ? (
+        feedActivities.map((item) => {
+          const renders = activityRenderers[item.verb];
+          return renders ? (
+            <ErrorBoundary hidden key={item.id}>
+              <Activity aggregatedActivity={item} renders={renders} />
+            </ErrorBoundary>
+          ) : null;
+        })
+      ) : (
+        <EmptyState>
+          <h2 className={styles.emptyState}>Ingen aktiviteter i feeden</h2>
+        </EmptyState>
+      )}
+    </div>
+  );
+};
 
 export default Feed;
--- app/reducers/feedActivities.ts
+++ app/reducers/feedActivities.ts
@@ -1,7 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { createSelector } from 'reselect';
 import { EntityType } from 'app/store/models/entities';
 import createLegoAdapter from 'app/utils/legoAdapter/createLegoAdapter';
 import { Feed } from '../actions/ActionTypes';
+import type { EntityId } from '@reduxjs/toolkit';
 import type { RootState } from 'app/store/createRootReducer';
 
 const legoAdapter = createLegoAdapter(EntityType.FeedActivities);
@@ -19,3 +21,9 @@ export default feedActivitiesSlice.reducer;
 
 export const { selectEntities: selectFeedActivityEntities } =
   legoAdapter.getSelectors((state: RootState) => state.feedActivities);
+
+export const selectFeedActivitiesByIds = createSelector(
+  selectFeedActivityEntities,
+  (_: RootState, ids: EntityId[]) => ids,
+  (entities, ids) => ids.map((id) => entities[id]),
+);
