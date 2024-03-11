76584f2f554f841deb62d98b602429ec987004de
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2024-03-11 16:45:47 +0800

    Fix types within legoAdapter

--- app/utils/legoAdapter/buildEntitiesReducer.ts
+++ app/utils/legoAdapter/buildEntitiesReducer.ts
@@ -1,28 +1,23 @@
 import { isAsyncApiActionSuccess } from 'app/utils/legoAdapter/asyncApiActions';
-import type { EntityAdapter } from '@reduxjs/toolkit';
+import type { EntityAdapter, EntityId } from '@reduxjs/toolkit';
 import type { EntityState } from '@reduxjs/toolkit/src/entities/models';
 import type { ActionReducerMapBuilder } from '@reduxjs/toolkit/src/mapBuilders';
-import type { ID } from 'app/store/models';
 import type Entities from 'app/store/models/entities';
 import type { EntityType } from 'app/store/models/entities';
 
 const buildEntitiesReducer = <
   T extends EntityType,
-  Entity extends Entities[T][ID] = Entities[T][ID],
+  Id extends EntityId,
+  Entity extends Entities[T][Id] = Entities[T][Id],
 >(
-  builder: ActionReducerMapBuilder<EntityState<Entity>>,
-  adapter: EntityAdapter<Entity>,
+  builder: ActionReducerMapBuilder<EntityState<Entity, Id>>,
+  adapter: EntityAdapter<Entity, Id>,
   entityType: T,
 ) => {
   builder.addMatcher(
     isAsyncApiActionSuccess.containingEntity(entityType),
     (state, action) => {
-      // typescript is not quite able to infer this type on its own
-      const payloadEntities = action.payload.entities[entityType] as Record<
-        ID,
-        Entity
-      >;
-      adapter.upsertMany(state, payloadEntities);
+      adapter.upsertMany(state, action.payload.entities[entityType]);
     },
   );
 };
