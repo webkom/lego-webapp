656429102fe3e56e3aa985399d8f88ad2154c619
ShaileshS1702 <shsha001@outlook.com>
2024-03-18 14:00:50 +0100

    fixed prettier og lint

--- app/components/CountDown/index.tsx
+++ app/components/CountDown/index.tsx
@@ -8,54 +8,51 @@ const CountDown = () => {
   );
   useEffect(() => {
     const interval = setInterval(() => {
-      findTime();
+      const now = new Date();
+      const start = new Date('2024-03-20T12:00:00');
+      const slutt = new Date('2024-03-22T12:00:00');
+      const started = start.getTime() - now.getTime() < 0;
+      const diff = started
+        ? slutt.getTime() - now.getTime()
+        : start.getTime() - now.getTime();
+
+      if (started) {
+        setTimeStringHeader(t => t + ' ER I GONG');
+      }
+
+      const withUnit = (
+        value: number,
+        unitSingular: string,
+        unitPlural: string,
+      ) => {
+        return `${value} ${value == 1 ? unitSingular : unitPlural}`;
+      };
+
+      const daysLeft = Math.floor(diff / (1000 * 60 * 60 * 24));
+      const hoursLeft = Math.floor(
+        (diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+      );
+      const minutesLeft = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+      const secondsLeft = Math.floor((diff % (1000 * 60)) / 1000);
+
+      const units = [
+        withUnit(daysLeft, 'dag', 'dagar'),
+        withUnit(hoursLeft, 'time', 'timar'),
+        withUnit(minutesLeft, 'minutt', 'minuttar'),
+        withUnit(secondsLeft, 'sekund', 'sekundar'),
+      ];
+
+      const timeLeft = units.slice(0, -1).join(', ') + ' og ' + units.slice(-1);
+      if (started) {
+        setTimeString(`Jakta sluttar om ${timeLeft}`);
+      } else {
+        setTimeString(`byrjar om ${timeLeft}`);
+      }
     }, 1000);
 
     return () => clearInterval(interval);
   }, []);
 
-  const findTime = () => {
-    const now = new Date();
-    const start = new Date('2024-03-20T12:00:00');
-    const slutt = new Date('2024-03-22T12:00:00');
-    const started = start.getTime() - now.getTime() < 0;
-    const diff = started
-      ? slutt.getTime() - now.getTime()
-      : start.getTime() - now.getTime();
-
-    if (started) {
-      setTimeStringHeader(timeStringHeader + ' ER I GONG');
-    }
-
-    const withUnit = (
-      value: number,
-      unitSingular: string,
-      unitPlural: string,
-    ) => {
-      return `${value} ${value == 1 ? unitSingular : unitPlural}`;
-    };
-
-    const daysLeft = Math.floor(diff / (1000 * 60 * 60 * 24));
-    const hoursLeft = Math.floor(
-      (diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
-    );
-    const minutesLeft = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-    const secondsLeft = Math.floor((diff % (1000 * 60)) / 1000);
-
-    const units = [
-      withUnit(daysLeft, 'dag', 'dagar'),
-      withUnit(hoursLeft, 'time', 'timar'),
-      withUnit(minutesLeft, 'minutt', 'minuttar'),
-      withUnit(secondsLeft, 'sekund', 'sekundar'),
-    ];
-
-    const timeLeft = units.slice(0, -1).join(', ') + ' og ' + units.slice(-1);
-    if (started) {
-      setTimeString(`Jakta sluttar om ${timeLeft}`);
-    } else {
-      setTimeString(`byrjar om ${timeLeft}`);
-    }
-  };
   return (
     <Banner
       header={timeStringHeader}
