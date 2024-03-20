7ff843b1b851961e293c69b6ec4904b80e20448f
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2024-03-20 20:32:44 +0800

    Use createSlice and legoAdapter for forums redux state

--- app/routes/forum/components/ForumDetail.tsx
+++ app/routes/forum/components/ForumDetail.tsx
@@ -1,15 +1,19 @@
+import { LoadingIndicator } from '@webkom/lego-bricks';
 import { usePreparedEffect } from '@webkom/react-prepare';
 import { useParams } from 'react-router-dom';
 import { fetchForum } from 'app/actions/ForumActions';
 import { Content, ContentMain } from 'app/components/Content';
 import NavigationTab, { NavigationLink } from 'app/components/NavigationTab';
-import { selectForumsById } from 'app/reducers/forums';
+import { selectForumById } from 'app/reducers/forums';
 import { useAppDispatch, useAppSelector } from 'app/store/hooks';
 import ThreadList from './ThreadList';
 import type { DetailedForum } from 'app/store/models/Forum';
 
+type ForumDetailParams = {
+  forumId: string;
+};
 const ForumDetail = () => {
-  const { forumId } = useParams<{ forumId: string }>();
+  const { forumId } = useParams<ForumDetailParams>() as ForumDetailParams;
   const dispatch = useAppDispatch();
 
   usePreparedEffect(
@@ -18,38 +22,39 @@ const ForumDetail = () => {
     [forumId],
   );
 
-  const forum: DetailedForum = useAppSelector((state) =>
-    selectForumsById(state, { forumId }),
-  );
+  const forum = useAppSelector((state) =>
+    selectForumById(state, forumId),
+  ) as DetailedForum;
   const detailActionGrant = forum?.actionGrant;
 
-  return (
-    forum &&
-    detailActionGrant && (
-      <Content>
-        <ContentMain>
-          <NavigationTab
-            back={{
-              label: 'Tilbake til liste',
-              path: '/forum',
-            }}
-          >
-            {detailActionGrant.includes('edit') && (
-              <NavigationLink to={`/forum/${forumId}/edit`}>
-                Rediger
-              </NavigationLink>
-            )}
-            <NavigationLink to={`/forum/${forumId}/new`}>
-              Oprett tråd
-            </NavigationLink>
-          </NavigationTab>
+  if (!forum || !detailActionGrant) {
+    return <LoadingIndicator loading />;
+  }
 
-          <h1>{forum.title}</h1>
-          <p className="secondaryFontColor">{forum.description}</p>
-          <ThreadList forumId={forumId} />
-        </ContentMain>
-      </Content>
-    )
+  return (
+    <Content>
+      <ContentMain>
+        <NavigationTab
+          back={{
+            label: 'Tilbake til liste',
+            path: '/forum',
+          }}
+        >
+          {detailActionGrant.includes('edit') && (
+            <NavigationLink to={`/forum/${forumId}/edit`}>
+              Rediger
+            </NavigationLink>
+          )}
+          <NavigationLink to={`/forum/${forumId}/new`}>
+            Oprett tråd
+          </NavigationLink>
+        </NavigationTab>
+
+        <h1>{forum.title}</h1>
+        <p className="secondaryFontColor">{forum.description}</p>
+        <ThreadList forumId={forumId} />
+      </ContentMain>
+    </Content>
   );
 };
 
