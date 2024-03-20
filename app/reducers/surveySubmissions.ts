33671da980e8b9262f53f3fcec9b0e7bfbb75e0e
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2024-03-20 15:43:48 +0800

    Use createSlice and legoAdapter for surveySubmissions redux state

--- app/reducers/surveySubmissions.ts
+++ app/reducers/surveySubmissions.ts
@@ -1,54 +1,45 @@
+import { createSlice } from '@reduxjs/toolkit';
 import { usePreparedEffect } from '@webkom/react-prepare';
-import { produce } from 'immer';
 import { createSelector } from 'reselect';
 import { fetchSubmissions } from 'app/actions/SurveySubmissionActions';
 import { useAppDispatch, useAppSelector } from 'app/store/hooks';
-import createEntityReducer from 'app/utils/createEntityReducer';
+import { EntityType } from 'app/store/models/entities';
+import createLegoAdapter from 'app/utils/legoAdapter/createLegoAdapter';
 import { SurveySubmission } from '../actions/ActionTypes';
 import type { AnyAction } from '@reduxjs/toolkit';
 import type { RootState } from 'app/store/createRootReducer';
 import type { ID } from 'app/store/models';
 import type { SurveySubmission as SurveySubmissionType } from 'app/store/models/SurveySubmission';
-import type { EntityReducerState } from 'app/utils/createEntityReducer';
 
-type State = EntityReducerState<SurveySubmissionType>;
-const mutateSurveySubmissions = produce(
-  (newState: State, action: AnyAction): void => {
-    switch (action.type) {
-      case SurveySubmission.ADD.SUCCESS: {
+const legoAdapter = createLegoAdapter(EntityType.SurveySubmissions);
+
+const surveySubmissionsSlice = createSlice({
+  name: EntityType.SurveySubmissions,
+  initialState: legoAdapter.getInitialState(),
+  reducers: {},
+  extraReducers: legoAdapter.buildReducers({
+    fetchActions: [SurveySubmission.FETCH, SurveySubmission.FETCH_ALL],
+    extraCases: (addCase) => {
+      addCase(SurveySubmission.ADD.SUCCESS, (state, action: AnyAction) => {
         const { surveyId } = action.meta;
         const id = action.payload.result;
         const surveySubmission = action.payload.entities.surveySubmissions[id];
-        newState.byId[id] = { ...surveySubmission, survey: surveyId };
-        break;
-      }
-
-      default:
-        break;
-    }
-  },
-);
-export default createEntityReducer({
-  key: 'surveySubmissions',
-  types: {
-    fetch: [SurveySubmission.FETCH, SurveySubmission.FETCH_ALL],
-    mutate: SurveySubmission.ADD,
-  },
-  mutate: mutateSurveySubmissions,
+        legoAdapter.upsertOne(state, { ...surveySubmission, survey: surveyId });
+      });
+    },
+  }),
 });
 
-export const selectSurveySubmissions = createSelector(
-  (_: RootState, props: { surveyId: ID }) => props.surveyId,
-  (state: RootState) => state.surveySubmissions.items,
-  (state: RootState) => state.surveySubmissions.byId,
-  (surveyId, surveySubmissionIds, surveySubmissionsById) =>
-    surveySubmissionIds
-      .map((surveySubmissionId) => surveySubmissionsById[surveySubmissionId])
-      .filter((surveySubmission) => surveySubmission.survey === surveyId),
-);
+export default surveySubmissionsSlice.reducer;
+const { selectByField: selectSurveySubmissionsByField } =
+  legoAdapter.getSelectors((state: RootState) => state.surveySubmissions);
+
+export const selectSurveySubmissionsBySurveyId =
+  selectSurveySubmissionsByField('survey');
+
 export const selectSurveySubmissionForUser = createSelector(
   (state: RootState, props: { surveyId: ID }) =>
-    selectSurveySubmissions(state, props),
+    selectSurveySubmissionsBySurveyId(state, props.surveyId),
   (_: RootState, props: { currentUserId: ID }) => props.currentUserId,
   (submissions, userId) =>
     submissions.find((surveySubmission) => surveySubmission.user === userId),
@@ -65,6 +56,6 @@ export const useFetchedSurveySubmissions = (
     [surveyId],
   );
   return useAppSelector((state: RootState) =>
-    selectSurveySubmissions(state, { surveyId: Number(surveyId) }),
+    selectSurveySubmissionsBySurveyId(state, Number(surveyId)),
   );
 };
