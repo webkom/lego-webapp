d157362563682675c5570e586233f371fb11d59d
Ivar Nakken <ivarnakken1441@gmail.com>
2024-07-04 14:40:49 +0200

    Update empty state component
    Fixes some bad styling

--- app/components/EmptyState/EmptyState.md
+++
@@ -1,8 +0,0 @@
-Example usage of `EmptyState`:
-
-    <div>
-        <EmptyState icon="photos-outline">
-            <h1>Ingen bilder å redigere</h1>
-            <h4>Gå <a>hit</a> for å legge inn bilder</h4>
-        </EmptyState>
-    </div>
--- app/components/EmptyState/index.tsx
+++ app/components/EmptyState/index.tsx
@@ -1,23 +1,16 @@
-import { Icon } from '@webkom/lego-bricks';
+import { Flex, Icon } from '@webkom/lego-bricks';
 import cx from 'classnames';
 import styles from './EmptyState.css';
 import type { ReactNode } from 'react';
 
 type Props = {
-  /** name of icon */
   icon?: string;
-
-  /** html to display in an EmptyState */
   children?: ReactNode;
   size?: number;
   className?: string;
 };
 
-/**
- * A basic EmptyState component
- *
- */
-const EmptyState = ({ icon, size = 88, className, children }: Props) => (
+const EmptyState = ({ icon, size = 45, className, children }: Props) => (
   <div className={cx(styles.container, icon && styles.centered, className)}>
     {icon && <Icon name={icon} size={size} />}
     {children}
