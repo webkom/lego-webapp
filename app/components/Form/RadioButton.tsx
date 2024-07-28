86e92d28b4996b771866e076266efcb98ab70f5f
Ivar Nakken <ivarnakken1441@gmail.com>
2024-07-28 15:53:13 +0200

    Fix cursor on checkbox and radio button labels
    It only worked on Field components

--- app/components/Form/RadioButton.tsx
+++ app/components/Form/RadioButton.tsx
@@ -44,7 +44,10 @@ function RadioButton({
         </svg>
       </div>
       {label && (
-        <label htmlFor={id} className={styles.label}>
+        <label
+          htmlFor={id}
+          className={cx(styles.label, props.disabled && styles.disabled)}
+        >
           {label}
         </label>
       )}
