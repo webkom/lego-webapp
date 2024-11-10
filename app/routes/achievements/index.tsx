db171b624a4c2ef0dddbd23b1c50f48629f150e0
Jonas de Luna Skulberg <jonasdls@gmail.com>
2024-11-10 18:29:30 +0100

    Add leaderboard and overview

--- app/routes/achievements/index.tsx
+++ app/routes/achievements/index.tsx
@@ -5,9 +5,9 @@ const Leaderboard = loadable(() => import('./components/Leaderboard'));
 const Overview = loadable(() => import('./components/Overview'));
 
 const achievementRoute: RouteObject[] = [
-  { index: true, Component: Leaderboard },
-  { path: 'leaderboard', Component: Leaderboard },
+  { index: true, Component: Overview },
   { path: 'overview', Component: Overview },
+  { path: 'leaderboard', Component: Leaderboard },
 ];
 
 export default achievementRoute;
