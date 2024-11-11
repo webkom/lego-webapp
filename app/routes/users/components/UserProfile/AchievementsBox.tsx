00473d6147855259b2cfc1c97fb3aedce90ca618
Håkon Hoelsæter <hakon1108@gmail.com>
2024-11-11 20:14:00 +0100

    Fixed spelling error in badge, changed Opnådd to Oppnådd

--- app/routes/users/components/UserProfile/AchievementsBox.tsx
+++ app/routes/users/components/UserProfile/AchievementsBox.tsx
@@ -21,7 +21,7 @@ export const AchievementsBox = ({
     return [...achievements].sort(
       (a, b) =>
         AchievementsInfo[b.identifier][b.level].rarity -
-        AchievementsInfo[a.identifier][a.level].rarity,
+        AchievementsInfo[a.identifier][a.level].rarity
     );
   }, [achievements]);
 
@@ -62,7 +62,7 @@ export const AchievementsBox = ({
                       </i>
                     </p>
                     <p>
-                      Opnådd den {moment(e.updatedAt).format('D. MMMM YYYY')}
+                      Oppnådd den {moment(e.updatedAt).format('D. MMMM YYYY')}
                     </p>
                     <p>{e.percentage.toFixed(1)}% har denne!</p>
                   </div>
