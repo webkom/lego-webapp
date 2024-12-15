f9fb8e559aa3620d17bf0a138b2cacb58eeb8562
Ivar Nakken <ivarnakken1441@gmail.com>
2024-12-15 12:31:03 +0100

    Implement and show upptime status in footer
    Fetching data from the readme of the repo hehe - couldn't find a better api

---
+++ app/reducers/status.ts
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchSystemStatus } from 'app/actions/StatusActions';
+import { EntityType } from 'app/store/models/entities';
+import createLegoAdapter from 'app/utils/legoAdapter/createLegoAdapter';
+import type { SystemStatus } from 'app/actions/StatusActions';
+import type { RootState } from 'app/store/createRootReducer';
+
+type ExtraStatusState = {
+  systemStatus: SystemStatus | null;
+};
+
+const legoAdapter = createLegoAdapter(EntityType.SystemStatus);
+
+const statusSlice = createSlice({
+  name: EntityType.SystemStatus,
+  initialState: legoAdapter.getInitialState({
+    systemStatus: null,
+  } as ExtraStatusState),
+  reducers: {},
+  extraReducers: legoAdapter.buildReducers({
+    extraCases: (addCase) => {
+      addCase(fetchSystemStatus.fulfilled, (state, action) => {
+        state.systemStatus = action.payload;
+      });
+    },
+  }),
+});
+
+export default statusSlice.reducer;
+
+export const selectSystemStatus = (state: RootState) =>
+  state.status.systemStatus;
