effaccb1aafe441f0f58a314bb5646564be00ba0
Isak Berg Endresen <isakbendresen@gmail.com>
2025-01-22 19:20:58 +0100

    Make each item an Accordion

--- app/routes/surveys/components/Submissions/SubmissionsIndividual.tsx
+++ app/routes/surveys/components/Submissions/SubmissionsIndividual.tsx
@@ -1,4 +1,6 @@
+import { Accordion, Button, Icon } from '@webkom/lego-bricks';
 import cx from 'classnames';
+import { ChevronRight } from 'lucide-react';
 import { useOutletContext } from 'react-router-dom';
 import StaticSubmission from '../StaticSubmission';
 import styles from '../surveys.module.css';
@@ -7,19 +9,41 @@ import type { SurveysRouteContext } from 'app/routes/surveys';
 const SubmissionPage = () => {
   const { submissions, survey } = useOutletContext<SurveysRouteContext>();
   return (
-    <ul>
-      {submissions.map((submission, i) => (
-        <li key={submission.id}>
-          <h3>Svar {i + 1}</h3>
+    <>
+      <ul>
+        {submissions.map((submission, i) => (
+          <li key={submission.id}>
+            <Accordion 
+              triggerComponent={({ onClick, rotateClassName }) => (
+                <div 
+                  className={styles.answerTrigger} 
+                  onClick={onClick}
+                >
+                  <h3>Svar {i + 1}</h3>
+                  <Icon
+                    onPress={onClick}
+                    iconNode={<ChevronRight />}
+                    className={rotateClassName}
+                  />
+                </div>
+          )}
+            >
+              <ul className={cx(styles.answers, styles.detailQuestions)}>
+                {survey.questions && (
+                  <StaticSubmission survey={survey} submission={submission} />
+                )}
+              </ul>
+            </Accordion>
+          </li>
+        ))}
+      </ul>
 
-          <ul className={cx(styles.answers, styles.detailQuestions)}>
-            {survey.questions && (
-              <StaticSubmission survey={survey} submission={submission} />
-            )}
-          </ul>
-        </li>
-      ))}
-    </ul>
+      <Button
+        onPress={() => {console.log("Fetching more submissions")}}
+      >
+        Last inn mer
+      </Button>
+    </>
   );
 };
 
