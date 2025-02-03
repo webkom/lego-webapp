0c21c7f77305ea3117902916761559c7b38dfbdc
Christoffer Nguyen <113469446+ch0rizo@users.noreply.github.com>
2025-02-03 10:12:11 +0100

    Add: information about prikk (#5316)
    * Add: information about prikk
    
    * small fixes
    
    * fix: lint
    
    * adjust size of icon

--- app/routes/users/components/UserProfile/Penalties.tsx
+++ app/routes/users/components/UserProfile/Penalties.tsx
@@ -27,69 +27,78 @@ const Penalties = ({ userId }: Props) => {
 
   return (
     <ProfileSection title="Prikker">
-      {penalties.length ? (
-        <>
-          {penalties.map((penalty, index) => (
-            <>
-              <Flex key={penalty.id} column gap="var(--spacing-sm)">
-                <span className={styles.weight}>
-                  {penalty.weight} {penalty.weight > 1 ? 'prikker' : 'prikk'}
-                </span>
-                <LinkInfoField
-                  name="Fra arragement"
-                  to={`/events/${
-                    penalty.sourceEvent.slug || penalty.sourceEvent.id
-                  }`}
-                >
-                  {penalty.sourceEvent.title}
-                </LinkInfoField>
-                <InfoField name="Begrunnelse">{penalty.reason}</InfoField>
-                <InfoField name="Utgår">
-                  <FormatTime
-                    time={penalty.exactExpiration}
-                    className={cx('secondaryFontColor', styles.time)}
-                  />
-                </InfoField>
-
-                {canDeletePenalties && (
-                  <ConfirmModal
-                    title="Slett prikk"
-                    message="Er du sikker på at du vil slette denne prikken?"
-                    onConfirm={() => {
-                      dispatch(deletePenalty(penalty.id));
-                    }}
-                    closeOnConfirm
+      <div className={styles.cardContainer}>
+        {penalties.length ? (
+          <>
+            {penalties.map((penalty, index) => (
+              <>
+                <Flex key={penalty.id} column gap="var(--spacing-sm)">
+                  <span className={styles.weight}>
+                    {penalty.weight} {penalty.weight > 1 ? 'prikker' : 'prikk'}
+                  </span>
+                  <LinkInfoField
+                    name="Fra arragement"
+                    to={`/events/${
+                      penalty.sourceEvent.slug || penalty.sourceEvent.id
+                    }`}
                   >
-                    {({ openConfirmModal }) => (
-                      <Button danger onPress={openConfirmModal}>
-                        Slett prikk
-                      </Button>
-                    )}
-                  </ConfirmModal>
-                )}
-              </Flex>
+                    {penalty.sourceEvent.title}
+                  </LinkInfoField>
+                  <InfoField name="Begrunnelse">{penalty.reason}</InfoField>
+                  <InfoField name="Utgår">
+                    <FormatTime
+                      time={penalty.exactExpiration}
+                      className={cx('secondaryFontColor', styles.time)}
+                    />
+                  </InfoField>
 
-              {index !== penalties.length - 1 && (
-                <div className={styles.divider} />
-              )}
-            </>
-          ))}
-        </>
-      ) : (
-        <>
-          <Flex alignItems="center" gap="var(--spacing-md)">
-            <Icon
-              name="thumbs-up-outline"
-              size={40}
-              className={styles.success}
-            />
-            <Flex column className={cx('secondaryFontColor', styles.info)}>
-              <span>Puh ...</span>
-              <span>Du har ingen prikker!</span>
+                  {canDeletePenalties && (
+                    <ConfirmModal
+                      title="Slett prikk"
+                      message="Er du sikker på at du vil slette denne prikken?"
+                      onConfirm={() => {
+                        dispatch(deletePenalty(penalty.id));
+                      }}
+                      closeOnConfirm
+                    >
+                      {({ openConfirmModal }) => (
+                        <Button danger onPress={openConfirmModal}>
+                          Slett prikk
+                        </Button>
+                      )}
+                    </ConfirmModal>
+                  )}
+                </Flex>
+
+                {index !== penalties.length - 1 && (
+                  <div className={styles.divider} />
+                )}
+              </>
+            ))}
+          </>
+        ) : (
+          <>
+            <Flex alignItems="center" gap="var(--spacing-md)">
+              <Icon
+                name="thumbs-up-outline"
+                size={40}
+                className={styles.success}
+              />
+              <Flex column className={cx('secondaryFontColor', styles.info)}>
+                <span>Puh ...</span>
+                <span>Du har ingen prikker!</span>
+              </Flex>
             </Flex>
-          </Flex>
-        </>
-      )}
+          </>
+        )}
+        <a href="https://abakus.no/pages/arrangementer/26-arrangementsregler">
+          <Icon
+            name="information-circle-outline"
+            size={22}
+            className={styles.infoIcon}
+          />
+        </a>
+      </div>
 
       <PenaltyForm userId={userId} />
     </ProfileSection>
