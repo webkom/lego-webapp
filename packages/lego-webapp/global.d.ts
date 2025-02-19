e9b9994005d83fda6de8ee97576192581fa44655
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2025-02-19 17:12:54 +0100

    Add react router support

--- packages/lego-webapp/global.d.ts
+++ packages/lego-webapp/global.d.ts
@@ -1,11 +1,24 @@
+import { Router } from 'react-router';
 import { Store } from './redux/createStore';
 import { RootState } from './redux/rootReducer';
+import type { createBrowserRouter } from 'react-router';
+import type { Config } from '~/utils/config';
 
 declare global {
   namespace Vike {
     interface PageContext {
       store: Store;
       storeInitialState: RootState;
+      router: Router;
     }
+  }
+}
+
+declare global {
+  interface Window {
+    __staticRouterHydrationData: Parameters<
+      typeof createBrowserRouter
+    >[1]['hydrationData'];
+    __CONFIG__: Config;
   }
 }
--- packages/lego-webapp/pages/+onBeforeRenderHtml.ts
+++ packages/lego-webapp/pages/+onBeforeRenderHtml.ts
@@ -1,8 +1,44 @@
 import { parse } from 'cookie';
+import {
+  createStaticHandler,
+  createStaticRouter,
+  type StaticHandlerContext,
+} from 'react-router';
 import { PageContextServer } from 'vike/types';
+import { routerConfig } from '~/pages/react-router/routerConfig';
 import createStore from '../redux/createStore';
 
-export function onBeforeRenderHtml(pageContext: PageContextServer) {
+const createReactRouterFetchRequest = (pageContext: PageContextServer) => {
+  const origin = `${pageContext.urlParsed.protocol}://${pageContext.urlParsed.hostname}`;
+  // Note: This had to take originalUrl into account for presumably vite's proxying
+  const url = new URL(pageContext.urlOriginal || pageContext.url, origin);
+
+  // const controller = new AbortController();
+  // res.on('close', () => controller.abort());
+
+  const headers = new Headers();
+
+  for (const [key, values] of Object.entries(pageContext.headers ?? {})) {
+    if (values) {
+      if (Array.isArray(values)) {
+        for (const value of values) {
+          headers.append(key, value);
+        }
+      } else {
+        headers.set(key, values);
+      }
+    }
+  }
+
+  const init = {
+    method: 'GET',
+    headers,
+  };
+
+  return new Request(url.href, init);
+};
+
+export async function onBeforeRenderHtml(pageContext: PageContextServer) {
   const cookies = parse(pageContext.headers?.['cookie'] ?? '');
   pageContext.store = createStore(
     {},
@@ -10,4 +46,9 @@ export function onBeforeRenderHtml(pageContext: PageContextServer) {
       getCookie: (key) => cookies[key],
     },
   );
+  // React router support
+  const { query, dataRoutes } = createStaticHandler(routerConfig);
+  const fetchRequest = createReactRouterFetchRequest(pageContext);
+  const context = (await query(fetchRequest)) as StaticHandlerContext;
+  pageContext.router = createStaticRouter(dataRoutes, context);
 }
