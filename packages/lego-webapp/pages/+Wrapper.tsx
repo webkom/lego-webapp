242ec7ad5d4361dce0395504e63ae74c91a82cca
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2025-02-22 13:12:53 +0100

    Fix setting theme

--- packages/lego-webapp/pages/+Wrapper.tsx
+++ packages/lego-webapp/pages/+Wrapper.tsx
@@ -2,6 +2,7 @@ import { PropsWithChildren, useState } from 'react';
 import { HelmetProvider } from 'react-helmet-async';
 import { Provider } from 'react-redux';
 import { usePageContext } from 'vike-react/usePageContext';
+import { ThemeContextListener } from 'app/utils/themeUtils';
 
 export default function StoreProvider({ children }: PropsWithChildren) {
   const pageContext = usePageContext();
@@ -12,6 +13,7 @@ export default function StoreProvider({ children }: PropsWithChildren) {
       <HelmetProvider
         context={import.meta.env.SSR ? pageContext.helmetContext : undefined}
       >
+        <ThemeContextListener />
         {children}
       </HelmetProvider>
     </Provider>
