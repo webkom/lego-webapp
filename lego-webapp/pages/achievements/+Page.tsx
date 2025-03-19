b1e8e7c36d92f2aa59a58faeceeef18fd76751b9
Isak Berg Endresen <isakbendresen@gmail.com>
2025-03-19 21:13:07 +0100

    Initial redesign

--- lego-webapp/pages/achievements/+Page.tsx
+++ lego-webapp/pages/achievements/+Page.tsx
@@ -1,12 +1,108 @@
-import { Flex } from '@webkom/lego-bricks';
-import { useEffect } from 'react';
+import { Card, Flex, Icon } from '@webkom/lego-bricks';
+import { CircleHelp } from 'lucide-react';
+import { ReactNode, useEffect } from 'react';
 import { navigate } from 'vike/client/router';
+import { ExclusifyUnion } from 'app/types';
 import { ContentMain } from '~/components/Content';
+import Tooltip from '~/components/Tooltip';
 import { postKeypress } from '~/redux/actions/AchievementActions';
 import { useAppDispatch, useAppSelector } from '~/redux/hooks';
+import {
+  AchievementData,
+  CurrentUser,
+  PublicUserWithGroups,
+} from '~/redux/models/User';
+import { useCurrentUser } from '~/redux/slices/auth';
+import { selectUserById, selectUserByUsername } from '~/redux/slices/users';
 import AchievementsInfo from '~/utils/achievementConstants';
 import useQuery from '~/utils/useQuery';
+import { useIsCurrentUser } from '../users/utils';
 import styles from './Overview.module.css';
+
+const AchievementTooltip = ({
+  achievement,
+  children,
+}: {
+  achievement: any;
+  children: ReactNode;
+}) => {
+  return (
+    <Tooltip
+      content={
+        <Flex column gap="var(--spacing-sm)">
+          <h3>{achievement.name}</h3>
+          {achievement.description && (
+            <span>
+              {achievement.hidden ? '?????????' : achievement.description}
+            </span>
+          )}
+          {achievement.rarity && (
+            <span>Sjeldenhet: {achievement.rarity + 1}</span>
+          )}
+        </Flex>
+      }
+      positions={['bottom', 'top', 'right', 'left']}
+      style={{ height: 'auto' }}
+    >
+      {children}
+    </Tooltip>
+  );
+};
+
+const AchievementGroup = ({
+  achievementGroup,
+  userLevel,
+}: {
+  achievementGroup: any;
+  userLevel: number;
+}) => {
+  return (
+    <Card hideOverflow className={styles.listItem}>
+      <Flex
+        gap="var(--spacing-sm)"
+        alignItems="baseline"
+        margin="0 0 var(--spacing-md) 0"
+      >
+        <h3>{achievementGroup[0].identifier}</h3>
+        <span>deltatt på x arrangementer</span>
+      </Flex>
+      <Flex justifyContent="space-between" alignItems="baseline">
+        <Flex alignItems="center" justifyContent="center">
+          {userLevel === -1 ? (
+            <AchievementTooltip
+              achievement={{
+                name: 'Uoppnådd trofe',
+              }}
+            >
+              <span style={{ fontSize: 100 }}>?</span>
+            </AchievementTooltip>
+          ) : (
+            <AchievementTooltip achievement={achievementGroup[userLevel]}>
+              <img
+                src={achievementGroup[userLevel].image ?? ''}
+                alt="Current trophy"
+                className={styles.currentAchievementImage}
+              />
+            </AchievementTooltip>
+          )}
+        </Flex>
+        <Flex gap="var(--spacing-sm)">
+          {achievementGroup.map((achievement: any, i: number) => (
+            <AchievementTooltip key={i} achievement={achievement}>
+              <Card className={styles.achievementImageCard}>
+                <img
+                  src={achievement.image ?? ''}
+                  alt="Trofe"
+                  className={styles.trophyImage}
+                />
+              </Card>
+            </AchievementTooltip>
+          ))}
+        </Flex>
+      </Flex>
+    </Card>
+  );
+};
 
 const Overview = () => {
   const { query } = useQuery({
@@ -16,7 +112,14 @@ const Overview = () => {
     sort_order: 'asc',
   });
 
-  const allTrophies = Object.entries(AchievementsInfo).flatMap(
+  const currentUser = useCurrentUser();
+  const username = currentUser?.username;
+  const user = useAppSelector((state) =>
+    selectUserByUsername<CurrentUser>(state, username),
+  );
+  const userAchievements = user?.achievements ?? [];
+
+  const achievementsGrouped = Object.entries(AchievementsInfo).map(
     ([key, achievements]) =>
       achievements.map((achievement, index) => ({
         ...achievement,
@@ -25,25 +128,30 @@ const Overview = () => {
       })),
   );
 
-  const filteredTrophies = allTrophies.filter((trophy) => {
-    const rarity = trophy.rarity;
-    const minRarity =
-      query.min_rarity !== 'any' ? parseInt(query.min_rarity, 10) : 0;
-    const maxRarity =
-      query.max_rarity !== 'any' ? parseInt(query.max_rarity, 10) : Infinity;
-    return rarity >= minRarity && rarity <= maxRarity;
-  });
+  const filteredAchievementsGrouped = achievementsGrouped.map((group) =>
+    group.filter((achievement) => {
+      const rarity = achievement.rarity;
+      const minRarity =
+        query.min_rarity !== 'any' ? parseInt(query.min_rarity, 10) : 0;
+      const maxRarity =
+        query.max_rarity !== 'any' ? parseInt(query.max_rarity, 10) : Infinity;
+      return rarity >= minRarity && rarity <= maxRarity;
+    }),
+  );
 
-  const sortedTrophies = [...filteredTrophies].sort((a, b) => {
-    let comparison = 0;
-    if (query.sort === 'rarity') comparison = a.rarity - b.rarity;
-    if (query.sort === 'alphabetical')
-      comparison = a.name.localeCompare(b.name);
-    if (query.sort === 'hidden')
-      comparison = a.hidden === b.hidden ? 0 : a.hidden ? -1 : 1;
+  // needed ?
+  const sortedAchievementsGrouped = filteredAchievementsGrouped.map((group) =>
+    [...group].sort((a, b) => {
+      let comparison = 0;
+      if (query.sort === 'rarity') comparison = a.rarity - b.rarity;
+      if (query.sort === 'alphabetical')
+        comparison = a.name.localeCompare(b.name);
+      if (query.sort === 'hidden')
+        comparison = a.hidden === b.hidden ? 0 : a.hidden ? -1 : 1;
 
-    return query.sort_order === 'desc' ? -comparison : comparison;
-  });
+      return query.sort_order === 'desc' ? -comparison : comparison;
+    }),
+  );
 
   const dispatch = useAppDispatch();
   const sudoAdminAccess = useAppSelector((state) => state.allowed.sudo);
@@ -70,26 +178,25 @@ const Overview = () => {
     return () => window.removeEventListener('keydown', keyHandler);
   }, [dispatch, sudoAdminAccess]);
 
+  const hasAchievementGroup = (group: any) => {
+    return userAchievements
+      .filter((achievement) => achievement.identifier === group[0].identifier)
+      .reduce(
+        (max, achievement) =>
+          max.level > achievement.level ? max : achievement,
+        { level: -1 },
+      );
+  };
+
   return (
     <ContentMain>
-      <Flex className={styles.listWrapper}>
-        {sortedTrophies.map((e) => (
-          <Flex className={styles.listItem} key={e.name + e.rarity}>
-            <img
-              src={AchievementsInfo[e.identifier][e.level].image ?? ''}
-              alt="Trofe"
-              className={styles.trophyImage}
-            />
-            <Flex column gap="var(--spacing-sm)" className="secondaryFontColor">
-              <h3>{e.name}</h3>
-              <span className={styles.description}>
-                {e.hidden ? '?????????' : e.description}
-              </span>
-              <span className={styles.description}>
-                Sjeldenhet: {e.rarity + 1}
-              </span>
-            </Flex>
-          </Flex>
+      <Flex column gap="var(--spacing-md)">
+        {sortedAchievementsGrouped.map((group, index) => (
+          <AchievementGroup
+            key={index}
+            achievementGroup={group}
+            userLevel={hasAchievementGroup(group).level}
+          />
         ))}
       </Flex>
     </ContentMain>
