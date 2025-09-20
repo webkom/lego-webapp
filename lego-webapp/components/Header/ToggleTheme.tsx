9fd632831a71639f29663896643e2d6252825574
Christoffer Nguyen <christofferhoang@gmail.com>
2025-09-20 19:21:28 +0200

    Now transition comes after React mount

--- lego-webapp/components/Header/ToggleTheme.tsx
+++ lego-webapp/components/Header/ToggleTheme.tsx
@@ -1,6 +1,7 @@
 import { Icon } from '@webkom/lego-bricks';
 import cx from 'classnames';
-import { Moon, Sun } from 'lucide-react';
+import { MoonStar, Sun } from 'lucide-react';
+import { useEffect, useState } from 'react';
 import { applySelectedTheme, getTheme, useTheme } from '~/utils/themeUtils';
 import styles from './toggleTheme.module.css';
 import type { ReactNode, MouseEvent } from 'react';
@@ -13,6 +14,11 @@ type Props = {
 
 const ToggleTheme = ({ className, children, isButton = true }: Props) => {
   const theme = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   const handleThemeChange = (e: MouseEvent) => {
     e.preventDefault();
@@ -26,13 +32,15 @@ const ToggleTheme = ({ className, children, isButton = true }: Props) => {
   return (
     <Component
       name="Endre tema"
-      className={cx(className, styles.toggleWrapper)}
+      className={cx(className, styles.toggleWrapper, {
+        [styles.noTransition]: !mounted,
+      })}
       onClick={handleThemeChange}
     >
       {children}
       <div className={styles.iconTrack} data-theme={theme}>
         <Icon iconNode={<Sun />} className={styles.icon} />
-        <Icon iconNode={<Moon />} className={styles.icon} />
+        <Icon iconNode={<MoonStar />} className={styles.icon} />
       </div>
     </Component>
   );
