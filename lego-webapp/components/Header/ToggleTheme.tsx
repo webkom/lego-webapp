16b9556f5751d8f9be585cb1a1ff24bcff066bb7
Christoffer Nguyen <christofferhoang@gmail.com>
2025-09-22 14:07:51 +0200

    Fix default and header animation on toggle dark/light

--- lego-webapp/components/Header/ToggleTheme.tsx
+++ lego-webapp/components/Header/ToggleTheme.tsx
@@ -10,9 +10,15 @@ type Props = {
   className?: string;
   children?: ReactNode;
   isButton?: boolean;
+  variant?: 'default' | 'header';
 };
 
-const ToggleTheme = ({ className, children, isButton = true }: Props) => {
+const ToggleTheme = ({
+  className,
+  children,
+  isButton = true,
+  variant = 'default',
+}: Props) => {
   const theme = useTheme();
   const [mounted, setMounted] = useState(false);
 
@@ -32,7 +38,7 @@ const ToggleTheme = ({ className, children, isButton = true }: Props) => {
   return (
     <Component
       name="Endre tema"
-      className={cx(className, styles.toggleWrapper, {
+      className={cx(className, styles.toggleWrapper, styles[variant], {
         [styles.noTransition]: !mounted,
       })}
       onClick={handleThemeChange}
