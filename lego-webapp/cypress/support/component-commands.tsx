7d0d229c0e4259d02740a5c54cdca02cb77ef212
Isak Berg Endresen <isakbendresen@gmail.com>
2025-10-18 16:14:08 +0200

    Configure cypress component tests

---
+++ lego-webapp/cypress/support/component-commands.tsx
@@ -0,0 +1,12 @@
+import { mount } from 'cypress/react'
+import { Provider } from 'react-redux';
+import store from '../fixtures/store';
+
+Cypress.Commands.add('mount', (component, options = {}) => {
+  // Use the default store if one is not provided
+  const { reduxStore = store(), ...mountOptions } = options
+
+  const wrapped = <Provider store={reduxStore}>{component}</Provider>
+
+  return mount(wrapped, mountOptions)
+})
