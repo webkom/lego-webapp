1fcf7aee37c5b6459bbca3a72813f48cc402d556
Aronbr <aron.reinen@gmail.com>
2025-10-29 21:01:49 +0100

    remove videofiles before merge

--- lego-webapp/pages/index/_christmasCalendar/videoplayer_fold/Videoplayer.tsx
+++ lego-webapp/pages/index/_christmasCalendar/videoplayer_fold/Videoplayer.tsx
@@ -1,55 +1,75 @@
 import { useState } from 'react';
-import sample from "./videoer/sample.mp4";
-import styles from './videospiller.module.css';
-import { TextInput } from "~/components/Form";
+import { TextInput } from '~/components/Form';
+import styles from './videoplayer.module.css';
 
 const Videospiller = ({
-   text = "Hvor mange Cava flasker kalerer Abacava å drikke på en cava-søndag?",
-   videoFile = sample
-  }) => {
+  question = '',
+  videoFile_1 = "",
+  videoFile_2 = "",
+  fasit = '-1',
+}) => {
+  const [videoFile, setVideoFile] = useState(videoFile_1);
+  const [Text, setText] = useState(question);
+  const [showText, setShowText] = useState(false);
   const [showBox, setShowBox] = useState(false);
-  const [inputValue, setInputValue] = useState("")
+  const [inputValue, setInputValue] = useState('');
   const [submittedValue, setSubmittedValue] = useState('');
 
   const handleVideoEnd = () => {
-    console.log("Video ended!");
-    setShowBox(true);
+    setShowText(true);
+    if (submittedValue != '') {
+      setShowBox(false);
+      if (submittedValue == fasit) {
+        setText('Gratulerer! Du svarte rikitg');
+      } else {
+        setText('Du svarte dessverre galt ):');
+      }
+    } else {
+      setShowBox(true);
+    }
+
+    // else{
   };
 
-    const handleKeyDown = (e) => {
+  const handleKeyDown = (e) => {
     if (e.key === 'Enter') {
       setSubmittedValue(inputValue); // Only update when Enter is pressed
+      setVideoFile(videoFile_2);
+      setShowBox(false);
+      setShowText(false);
     }
   };
 
   return (
     <>
       <video
+        key={videoFile} // <- forces React to remount when src changes
         onEnded={handleVideoEnd}
         className={styles.video}
         controls
+        autoPlay
       >
         <source src={videoFile} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
-      {showBox && 
-      <p className={styles.text}>{text}</p> }
+      {showText && <p className={styles.text}>{Text}</p>}
       {showBox && (
-        <TextInput 
+        <TextInput
           className={styles.box}
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
-          onKeyDown={handleKeyDown} // But only submit on Enter
-          placeholder="Type your answer and press Enter"
+          onKeyDown={handleKeyDown}
+          placeholder="Skriv inn svaret ditt"
+          centered={true}
         />
-      )}git commit -m "first videoplayer commit"
+      )}
 
       {showBox && submittedValue && (
         <div className={styles.display}>
           <p>Your answer: {submittedValue}</p>
         </div>
-      )}git commit -m "first videoplayer commit"
+      )}
     </>
   );
 };
