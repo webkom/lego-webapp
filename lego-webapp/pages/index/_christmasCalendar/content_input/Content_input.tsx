be9d264bd4dd60aabb446e2dda9a00e01112901a
Aronbr <aron.reinen@gmail.com>
2025-10-31 11:29:13 +0100

    First content_input commit

---
+++ lego-webapp/pages/index/_christmasCalendar/content_input/Content_input.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import styles from './content_input.module.css';
+import sample from './placeholder.jpg'
+import { TextInput } from '~/components/Form';
+
+const Content_input = ({
+  image = sample,
+  question = 'Hva er 2 - 3?',
+  answer = '-1',
+}) => {
+  const [message, setMessage] = useState("");
+  const [inputValue, setInputValue] = useState('');
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      if (inputValue == answer){
+        setMessage("Rikitg!")
+      }
+      else{
+        setMessage("Feil")
+      }
+
+    }
+  };
+
+
+
+  return (
+    <>
+      <img src={image} alt=""/>
+
+      <p className={styles.text}>{question}</p>
+      <TextInput
+          className={styles.box}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          onKeyDown={handleKeyDown}
+          placeholder="Skriv inn svaret ditt"
+          centered={true}
+        />
+      <p className={styles.text}>{message}</p>
+
+    </>
+  );
+};
+
+export default Content_input;
