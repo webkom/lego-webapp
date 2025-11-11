e15c3064a50daa5727c471ed75255f677b03935a
Astrid <astrid.saeveraas2@gmail.com>
2025-11-11 19:51:53 +0100

    Add snowflaks Random Size

--- lego-webapp/pages/index/_christmasCalendar/snow.tsx
+++ lego-webapp/pages/index/_christmasCalendar/snow.tsx
@@ -3,18 +3,22 @@ import styles from './snow.module.css';
 export default function SnowBackground() {
   return (
     <div className={styles.container}>
-      {Array.from({ length: 50 }).map((_, i) => (
+      {Array.from({ length: 50 }).map((_, i) => { 
+        var random_size = Math.floor(Math.random() * 9) + 4;
+        return (
         <div
           key={i}
           className={styles.snow}
           style={{
             left: `${Math.floor(Math.random() * 100)}%`,
+            width: `${random_size}px`,
+            height: `${random_size}px`,
             animationDuration: `${Math.floor(Math.random() * (21)) + 30}s`,
             animationDelay: `${Math.floor(Math.random() * 30) + 1}s`,
-            opacity: `${Math.random()* (100 - 20) + 20}%`,
+            opacity: Math.random() * 0.8 + 0.2,
           }}
         />
-      ))} 
+      )})}
     </div>
   );
 }
