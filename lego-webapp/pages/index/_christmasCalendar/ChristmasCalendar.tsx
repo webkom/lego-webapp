852be7e2adc99db4ae488dcaf058560f95e0f082
Astrid <astrid.saeveraas2@gmail.com>
2025-11-11 19:39:24 +0100

    Add snowflakse

--- lego-webapp/pages/index/_christmasCalendar/ChristmasCalendar.tsx
+++ lego-webapp/pages/index/_christmasCalendar/ChristmasCalendar.tsx
@@ -1,43 +1,43 @@
 import utilStyles from '~/styles/utilities.module.css';
-import styles from './ChristmasCalendar.module.css'
+import styles from './ChristmasCalendar.module.css';
 import { Button, Flex, Modal } from '@webkom/lego-bricks';
 import { useState } from 'react';
 
 const ChristmasCalendar = () => {
-    return (
-        <div>
-            <h3 className={utilStyles.frontPageHeader}>Julekalender</h3>
-            <Flex gap="var(--spacing-sm)">
-                <ChristmasCalendarSlot day={1}/>
-                <ChristmasCalendarSlot day={2}/>
-                <ChristmasCalendarSlot day={3}/>
-                <ChristmasCalendarSlot day={4}/>
-                <ChristmasCalendarSlot day={5}/>
-            </Flex>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h3 className={utilStyles.frontPageHeader}>Julekalender</h3>
+      <Flex gap="var(--spacing-sm)">
+        <ChristmasCalendarSlot day={1} />
+        <ChristmasCalendarSlot day={2} />
+        <ChristmasCalendarSlot day={3} />
+        <ChristmasCalendarSlot day={4} />
+        <ChristmasCalendarSlot day={5} />
+      </Flex>
+    </div>
+  );
+};
 
 type ChristmasCalendarSlotType = {
-    day: number;
-    color?: string;
-}
+  day: number;
+  color?: string;
+};
 
 const ChristmasCalendarSlot = (props: ChristmasCalendarSlotType) => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    return (
-        <div>
-            <Button onPress={() => setIsOpen(true)}>Luke {props.day}</Button>
-            <Modal
-            isOpen={isOpen}
-            onOpenChange={setIsOpen}
-            title={`Luke ${props.day}`}
-            >
-                Morsom video
-            </Modal>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Button onPress={() => setIsOpen(true)}>Luke {props.day}</Button>
+      <Modal
+        isOpen={isOpen}
+        onOpenChange={setIsOpen}
+        title={`Luke ${props.day}`}
+      >
+        Morsom video
+      </Modal>
+    </div>
+  );
+};
 
 export default ChristmasCalendar;
