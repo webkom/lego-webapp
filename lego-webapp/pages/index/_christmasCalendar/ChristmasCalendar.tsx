c7f1cca2dbf803929d9aaccca37de33d11f05a34
Celia Schmidt-Cisternas <celia@schmidt-cisternas.net>
2025-11-18 18:56:40 +0100

    Add quiz questions

---
+++ lego-webapp/pages/index/_christmasCalendar/ChristmasCalendar.tsx
@@ -0,0 +1,163 @@
+import { Flex, Modal } from '@webkom/lego-bricks';
+import React from 'react';
+import styles from './ChristmasCalendar.module.css';
+import ArcadeGameBox from './_arcadeGame/ArcadeGameCanvas';
+import QuizGame from './QuizGame/QuizGame';
+
+type ChristmasCalendarType = {
+  className: string;
+};
+
+const ChristmasCalendar = ({ className }: ChristmasCalendarType) => {
+  const content = [
+    <p key={1}>{<ArcadeGameBox dateNr={1} />}</p>,
+    <p key={2}>test</p>,
+    <p key={3}>{<QuizGame index={3} />}</p>,
+    <p key={4}>test</p>,
+    <p key={5}>test</p>,
+    <p key={6}>{<QuizGame index={6} />}</p>,
+    <p key={7}>test</p>,
+    <p key={8}>{<ArcadeGameBox dateNr={8} />}</p>,
+    <p key={9}>test</p>,
+    <p key={10}>test</p>,
+    <p key={11}>{<ArcadeGameBox dateNr={11} />}</p>,
+    <p key={12}>test</p>,
+    <p key={13}>{<QuizGame index={13} />}</p>,
+    <p key={14}>test</p>,
+    <p key={15}>{<ArcadeGameBox dateNr={15} />}</p>,
+    <p key={16}>test</p>,
+    <p key={17}>{<QuizGame index={17} />}</p>,
+    <p key={18}>test</p>,
+    <p key={19}>test</p>,
+    <p key={20}>{<ArcadeGameBox dateNr={20} />}</p>,
+    <p key={21}>{<QuizGame index={21} />}</p>,
+    <p key={22}>test</p>,
+    <p key={23}>test</p>,
+    <p key={24}>test</p>,
+  ];
+  const rows: Array<Array<React.ReactElement>> = [];
+  const complete = [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ];
+
+  for (let i = 0; i < content.length; i += 4) {
+    rows.push(content.slice(i, i + 4));
+  }
+
+  return (
+    <div className={className}>
+      <Flex width={'100%'}>
+        <AbakusPole />
+        <div className={styles.abakusContainer}>
+          {rows.map((row, rowIndex) => (
+            <div key={rowIndex} className={styles.abakusRow}>
+              <div className={styles.abakusBalls}>
+                <Flex gap={(Math.random() * 0.9 + 0.2) * 10}>
+                  {row.map((content, index) =>
+                    complete[4 * rowIndex + index] ? (
+                      <AbakusBall
+                        key={index}
+                        date={4 * rowIndex + index + 1}
+                        content={content}
+                        complete={complete}
+                      />
+                    ) : null,
+                  )}
+                </Flex>
+                <Flex
+                  gap={(Math.random() * 0.9 + 0.2) * 10}
+                  justifyContent="flex-end"
+                >
+                  {row.map((content, index) =>
+                    !complete[4 * rowIndex + index] ? (
+                      <AbakusBall
+                        key={index}
+                        date={4 * rowIndex + index + 1}
+                        content={content}
+                        complete={complete}
+                      />
+                    ) : null,
+                  )}
+                </Flex>
+              </div>
+              <div className={styles.abakusWire} />
+            </div>
+          ))}
+        </div>
+        <AbakusPole />
+      </Flex>
+    </div>
+  );
+};
+
+type AbakusBallType = {
+  date: number;
+  content: React.ReactElement;
+  complete: Array<boolean>;
+};
+
+const AbakusBall = ({ date, content, complete }: AbakusBallType) => {
+  const [open, setOpen] = React.useState(false);
+  const TEST_DATE = 10;
+
+  return (
+    <>
+      <button
+        className={
+          Number(date) === TEST_DATE
+            ? `${styles.abakusBall} ${styles.ballShake}`
+            : styles.abakusBall
+        }
+        onClick={() =>
+          TEST_DATE >= Number(date) && !complete[date - 1]
+            ? setOpen(true)
+            : false
+        }
+      >
+        <p>{date}</p>
+      </button>
+      <Modal
+        contentClassName={styles.slotModal}
+        isOpen={open}
+        onOpenChange={setOpen}
+        title={`Luke ${date}`}
+      >
+        {content}
+      </Modal>
+    </>
+  );
+};
+
+const AbakusPole = () => {
+  return (
+    <Flex column alignItems="center">
+      <div className={styles.abakusPole} />
+      <div className={styles.abakusBrick} />
+    </Flex>
+  );
+};
+
+export default ChristmasCalendar;
---
+++ lego-webapp/pages/index/_christmasCalendar/QuizGame/QuizGame.tsx
@@ -0,0 +1,198 @@
+import { Button, Flex } from '@webkom/lego-bricks';
+import { useState, useEffect, useCallback } from 'react';
+import styles from './QuizGame.module.css';
+
+type QuizGameType = {
+  index: string;
+};
+
+const QuizGame = ({ index }: QuizGameType) => {
+  const slots = {
+    '3': [
+      {
+        question: 'Hvor mange interessegrupper er det i Abakus?',
+        options: ['32', '27', '28', '34'],
+        answer: '32',
+      },
+      {
+        question: 'Hvilke av disse er ikke en av abakus sine verdier?',
+        options: ['Bærekraft', 'Attraktiv', 'Effektiv', 'Åpen'],
+        answer: 'Effektiv',
+      },
+      {
+        question: 'Hvilket år ble abakus stiftet?',
+        options: ['1992', '1997', '1981', '1993'],
+        answer: '1997',
+      },
+    ],
+    '6': [
+      {
+        question: 'Hva er hovedingrediensen i brunost?',
+        options: ['Myse', 'Rømme', 'Sukker', 'Geit'],
+        answer: 'Myse',
+      },
+      {
+        question: 'Hvilken varm drikke er mest populær i Norge?',
+        options: ['Kakao', 'Te', 'Kaffe', 'Gløgg'],
+        answer: 'Kaffe',
+      },
+      {
+        question: 'Hva er tradisjonelt norsk pålegg laget av potet og fisk?',
+        options: ['Røkelaks', 'Potetsalat', 'Lutefisk', 'Laksepaté'],
+        answer: 'Lutefisk',
+      },
+      {
+        question: 'Hva slags kjøtt brukes vanligvis i pinnekjøtt?',
+        options: ['Ku', 'Okse', 'Geit', 'Sau'],
+        answer: 'Sau',
+      },
+    ],
+    '13': [
+      {
+        question: 'Hvilken dag feires Lucia-dagen?',
+        options: ['13. des', '25. des', '17. des', '11. des'],
+        answer: 'Paris',
+      },
+      {
+        question: 'Hvilket land kommer feiringen opprinnelig fra?',
+        options: ['Sverige', 'Spania', 'Italia', 'Østerrike'],
+        answer: 'JavaScript',
+      },
+      {
+        question: 'Hva symboliserer lysene Lucia bærer på hodet?',
+        options: [
+          'Dagene blir lysere',
+          'Høstens avslutning',
+          'Lysets seier over mørket',
+          'Den hellige ånd',
+        ],
+        answer: 'Lysets seier over mørket',
+      },
+    ],
+    '17': [
+      {
+        question: 'Norge har flere elger enn innbyggere?',
+        options: ['Sant', 'Usant'],
+        answer: 'Usant',
+      },
+      {
+        question:
+          'I Sverige finnes det en offisiell konkurranse i å spise lussekatter på tid',
+        options: ['Sant', 'Usant'],
+        answer: 'Usant',
+      },
+      {
+        question:
+          'I Norge er brunost laget av geitemelk, mens i Sveits er brunost laget av ku-melk',
+        options: ['Sant', 'Usant'],
+        answer: 'Sant',
+      },
+      {
+        question: 'Julenissen har egen postkode i Norge',
+        options: ['Sant', 'Usant'],
+        answer: 'Sant',
+      },
+      {
+        question:
+          'Den lengste dagen (der sola ikke går ned) i Norge varer i over 24 timer på Svalbard om sommeren',
+        options: ['Sant', 'Usant'],
+        answer: 'Sant',
+      },
+    ],
+    '21': [
+      {
+        question: 'Hvilket krydder brukes ofte for å skape juleduft hjemme?',
+        options: ['Gurkemeie', 'Kanel', 'Kardemomme', 'Chilli'],
+        answer: 'Kanel',
+      },
+      {
+        question: 'Hva er ofte synonymt med “kos” på norsk??',
+        options: ['Stress', 'Eksamen', 'Varme, hygge og ro', 'Intervaller'],
+        answer: 'Varme, hygge og ro',
+      },
+      {
+        question: 'Who developed the theory of relativity?',
+        options: ['Newton', 'Einstein', 'Galileo', 'Tesla'],
+        answer: 'Einstein',
+      },
+    ],
+  };
+
+  const questions = slots[index];
+
+  const [currentQuestion, setCurrentQuestion] = useState<number>(0);
+  const [score, setScore] = useState<number>(0);
+  const [showResult, setShowResult] = useState<boolean>(false);
+  const [timeLeft, setTimeLeft] = useState<number>(10);
+
+  const handleNextQuestion = useCallback(() => {
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+      setTimeLeft(10);
+    } else {
+      setShowResult(true);
+    }
+  }, [currentQuestion, questions.length]);
+
+  useEffect(() => {
+    if (timeLeft === 0) {
+      handleNextQuestion();
+    }
+    const timer = setInterval(() => {
+      setTimeLeft((prev) => (prev > 0 ? prev - 1 : 0));
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [handleNextQuestion, timeLeft]);
+
+  const handleAnswer = (option: string) => {
+    if (option === questions[currentQuestion].answer) {
+      setScore(score + 1);
+    }
+    handleNextQuestion();
+  };
+
+  return (
+    <>
+      {showResult ? (
+        <Flex column alignItems="center" gap={'20px'}>
+          <h2>GRATULERER!</h2>
+          <Flex gap={'20px'} alignItems="center">
+            <p>
+              Dine poeng: {score} / {questions.length}
+            </p>
+            <Button
+              onPress={() => {
+                setCurrentQuestion(0);
+                setScore(0);
+                setShowResult(false);
+                setTimeLeft(10);
+              }}
+            >
+              Begynn på nytt
+            </Button>
+          </Flex>
+        </Flex>
+      ) : (
+        <Flex column gap={'20px'} alignItems="center">
+          <h2>{questions[currentQuestion].question}</h2>
+          <Flex width={'100%'} column alignItems="center">
+            <div className={styles.options}>
+              {questions[currentQuestion].options.map((option) => (
+                <button
+                  key={option}
+                  className={styles.optionButton}
+                  onClick={() => handleAnswer(option)}
+                >
+                  {option}
+                </button>
+              ))}
+            </div>
+            <p className={styles.timer}>Tid igjen: {timeLeft}s</p>
+          </Flex>
+        </Flex>
+      )}
+    </>
+  );
+};
+export default QuizGame;
