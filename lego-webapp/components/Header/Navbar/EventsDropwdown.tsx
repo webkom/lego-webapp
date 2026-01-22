621d177b8843e26f5633144c1c42fb9804f846aa
Aronbr <aron.reinen@gmail.com>
2026-01-22 20:45:26 +0100

    Added link to interest groups under events in navbar, with changing logo

--- lego-webapp/components/Header/Navbar/EventsDropwdown.tsx
+++ lego-webapp/components/Header/Navbar/EventsDropwdown.tsx
@@ -1,4 +1,5 @@
-import { AlignLeft, CalendarRange } from 'lucide-react';
+import { sample } from 'lodash-es';
+import { AlignLeft, Banana, CalendarRange, MountainSnow, Gamepad } from 'lucide-react';
 import { ItemList } from './ItemList';
 import type { ItemProps } from './Item';
 
@@ -6,7 +7,9 @@ const EventsDropdown = () => {
   const items: ItemProps[] = [
     { title: 'Oversikt', icon: <AlignLeft />, href: '/events' },
     { title: 'Kalender', icon: <CalendarRange />, href: '/events/calendar' },
+    { title: 'Interessegrupper', icon: sample([<Banana key="1" />, <Gamepad key="2" />,<MountainSnow key="3" />,]), href: '/interest-events' },
   ];
+  
 
   return <ItemList items={items} />;
 };
