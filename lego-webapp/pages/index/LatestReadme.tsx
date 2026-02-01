760f1d8cbeb0e58b1e3ce2958c844540d30a1a4c
Magnus Brecke <magnus.brecke@gmail.com>
2026-02-01 11:13:05 +0100

    prettier

--- lego-webapp/pages/index/LatestReadme.tsx
+++ lego-webapp/pages/index/LatestReadme.tsx
@@ -1,4 +1,11 @@
-import { Accordion, Card, Flex, Icon, Image, LinkButton } from '@webkom/lego-bricks';
+import {
+  Accordion,
+  Card,
+  Flex,
+  Icon,
+  Image,
+  LinkButton,
+} from '@webkom/lego-bricks';
 import cx from 'classnames';
 import { ChevronRight } from 'lucide-react';
 import { readmeIfy } from '~/components/ReadmeLogo';
@@ -53,9 +60,7 @@ const LatestReadme = ({
           ))}
         </div>
         <div className={styles.seeAllButton}>
-          <LinkButton href="https://readme.abakus.no">
-            Se alle
-          </LinkButton>
+          <LinkButton href="https://readme.abakus.no">Se alle</LinkButton>
         </div>
       </Accordion>
     </Card>
