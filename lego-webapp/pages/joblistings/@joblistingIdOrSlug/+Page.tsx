8c45c8dfcba5e2ebdd108e9caafbc17c093ce3be
Viljen Apalset Vassbø <90694640+Viljen789@users.noreply.github.com>
2026-03-20 13:37:23 +0100

    Fix not-given on company contacts

--- lego-webapp/pages/joblistings/@joblistingIdOrSlug/+Page.tsx
+++ lego-webapp/pages/joblistings/@joblistingIdOrSlug/+Page.tsx
@@ -3,14 +3,11 @@ import { usePreparedEffect } from '@webkom/react-prepare';
 import { Pencil } from 'lucide-react';
 import { useEffect } from 'react';
 import { navigate } from 'vike/client/router';
-import {
-  ContentSection,
-  ContentMain,
-  ContentSidebar,
-} from '~/components/Content';
+import { ContentMain, ContentSection, ContentSidebar } from '~/components/Content';
 import DisplayContent from '~/components/DisplayContent';
 import InfoList from '~/components/InfoList';
-import { jobType, Year, Workplaces } from '~/components/JoblistingItem/Items';
+import { jobType, Workplaces, Year } from '~/components/JoblistingItem/Items';
+import type { PropertyGenerator } from '~/components/PropertyHelmet';
 import PropertyHelmet from '~/components/PropertyHelmet';
 import Time from '~/components/Time';
 import YoutubeCover from '~/pages/pages/_components/YoutubeCover';
@@ -20,7 +17,6 @@ import { selectJoblistingByIdOrSlug } from '~/redux/slices/joblistings';
 import { isTruthy } from '~/utils';
 import { appConfig } from '~/utils/appConfig';
 import { useParams } from '~/utils/useParams';
-import type { PropertyGenerator } from '~/components/PropertyHelmet';
 import type { DetailedJoblisting } from '~/redux/models/Joblisting';
 
 const propertyGenerator: PropertyGenerator<{
@@ -64,8 +60,6 @@ const MailWrapper = ({ mail }: { mail: string }) => (
     <wbr />@{mail.split('@')[1]}
   </a>
 );
-
-const NotGiven = () => <span className="secondaryFontColor">Ikke oppgitt</span>;
 
 const JoblistingDetail = () => {
   const { joblistingIdOrSlug } = useParams();
@@ -206,28 +200,33 @@ const JoblistingDetail = () => {
                     {showContactMail && <h4>Kontaktperson</h4>}
                     <InfoList
                       items={[
-                        {
-                          key: 'Navn',
-                          value: joblisting.responsible.name || <NotGiven />,
-                        },
-                        {
-                          key: 'E-post',
-                          value: joblisting.responsible.mail ? (
-                            <MailWrapper mail={joblisting.responsible.mail} />
-                          ) : (
-                            <NotGiven />
-                          ),
-                        },
-                        {
-                          key: 'Telefon',
-                          value: joblisting.responsible.phone ? (
-                            <a href={`tel:${joblisting.responsible.phone}`}>
-                              {joblisting.responsible.phone}
-                            </a>
-                          ) : (
-                            <NotGiven />
-                          ),
-                        },
+                        joblisting.responsible.name
+                          ? {
+                              key: 'Navn',
+                              value: joblisting.responsible.name,
+                            }
+                          : null,
+
+                        joblisting.responsible.mail
+                          ? {
+                              key: 'E-post',
+                              value: (
+                                <MailWrapper
+                                  mail={joblisting.responsible.mail}
+                                />
+                              ),
+                            }
+                          : null,
+                        joblisting.responsible.phone
+                          ? {
+                              key: 'Telefon',
+                              value: (
+                                <a href={`tel:${joblisting.responsible.phone}`}>
+                                  {joblisting.responsible.phone}
+                                </a>
+                              ),
+                            }
+                          : null,
                       ]}
                     />
                   </div>
