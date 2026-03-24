fae9d8fa95f445a301919e148d915d11df129686
Viljen Apalset Vassbø <90694640+Viljen789@users.noreply.github.com>
2026-03-24 20:39:21 +0100

    Import new base64 converter library

---
+++ lego-webapp/pages/påskeegg/+Page.tsx
@@ -0,0 +1,15 @@
+import { Flex } from '@webkom/lego-bricks';
+
+const Egg = () => {
+  return (
+    <Flex alignItems="center" column>
+      <img
+        src="../../assets/interest-group-logos/6169555d9248be7e184f52250129b0d66c9932af74f4ac7bc716c20013fca362.png"
+        alt="An image was supposed to be here but is not"
+        width={200}
+      />
+    </Flex>
+  );
+};
+
+export default Egg;
