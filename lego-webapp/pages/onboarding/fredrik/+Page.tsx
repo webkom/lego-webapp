f177082f9449763831220d1cc91e4522a08e154e
Fredrik Strømsholm <fredrik@dhcp-10-22-106-122.wlan.ntnu.no>
2026-09-16 18:36:57 +0200

    Added fredrikpage info

--- lego-webapp/pages/onboarding/fredrik/+Page.tsx
+++ lego-webapp/pages/onboarding/fredrik/+Page.tsx
@@ -1,5 +1,15 @@
-const Content = () => {
-  <></>;
+import cx from 'classnames';
+import styles from './style.module.css'
+
+const FredrikPage = () => {
+ return( 
+ 
+ <div className={styles.green}>Dette er siden til Fredrik!
+  <ul> 
+    <li className={styles.purple}>Hva skjer</li>
+  </ul>
+</div>
+)
 };
 
-export default Content;
+export default FredrikPage;
