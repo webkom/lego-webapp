da03c238414a6fc66de77e46a1136dba437bd379
Ane Ek <acfryd@icloud.com>
2026-09-16 20:16:37 +0200

    Format with prettier

--- lego-webapp/pages/onboarding/ane/+Page.tsx
+++ lego-webapp/pages/onboarding/ane/+Page.tsx
@@ -1,21 +1,19 @@
-import style from "./style.module.css"
+import style from './style.module.css';
 const Content = () => {
-  return(<div>
+  return (
+    <div>
+      <h1 className={style.cute}>ANE</h1>
 
-<h1 className={style.cute}>
-  ANE
-</h1>
-
-<p className={style.pink}>
-  Hei. Jeg heter Ane! <br/>
-  Fakta om meg: <br/>
-  Haarfarge: Blond <br/>
-  Navn: Ane <br/>
-  Favoritt farge: Lilla <br/>
-  Favoritt drikke: Pepsi max
-</p>
-
-  </div>)
+      <p className={style.pink}>
+        Hei. Jeg heter Ane! <br />
+        Fakta om meg: <br />
+        Haarfarge: Blond <br />
+        Navn: Ane <br />
+        Favoritt farge: Lilla <br />
+        Favoritt drikke: Pepsi max
+      </p>
+    </div>
+  );
 };
 
 export default Content;
