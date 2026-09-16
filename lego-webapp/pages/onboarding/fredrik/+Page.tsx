d30587d98b4652828dfc0d54a575eb47f795c6ae
Fredrik Strømsholm <fredrik@dhcp-10-22-106-122.wlan.ntnu.no>
2026-09-16 19:01:52 +0200

    Added user stuff

--- lego-webapp/pages/onboarding/fredrik/+Page.tsx
+++ lego-webapp/pages/onboarding/fredrik/+Page.tsx
@@ -1,9 +1,17 @@
+import { CurrentUser } from '~/redux/models/User';
 import styles from './style.module.css'
+import { useCurrentUser } from '~/redux/slices/auth'
 
 const FredrikPage = () => {
+  const user: CurrentUser | undefined = useCurrentUser()
+
  return( 
  
  <div className={styles.green}>Dette er siden til Fredrik!
+  <div>Hva er {user?.fullName}?</div>
+  <div> -- {user?.gender}</div>
+  <div>Er {user?.fullName} med i noen grupper?</div>
+  <div> -- Ja, han er med i {user?.abakusGroups}</div>
   <ul> 
     <li className={styles.purple}>Hva skjer</li>
     <li className={styles.yellow}>Merk: den aller kuleste nye webkommeren</li>
