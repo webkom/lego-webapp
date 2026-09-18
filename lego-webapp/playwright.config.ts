4dc8067934d950f489db0ec7240a597a5b61fb95
Christoffer Nguyen <christofferhoang@gmail.com>
2026-09-18 20:37:38 +0200

    prettier

--- lego-webapp/playwright.config.ts
+++ lego-webapp/playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-/* CI retries */
+  /* CI retries */
   retries: 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
