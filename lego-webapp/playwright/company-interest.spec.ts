62e79d1978cd4b4f646a66bcd0cc238f2eefbe1a
Christoffer Nguyen <christofferhoang@gmail.com>
2026-09-18 15:38:19 +0200

    check fail tests

--- lego-webapp/playwright/company-interest.spec.ts
+++ lego-webapp/playwright/company-interest.spec.ts
@@ -4,5 +4,5 @@ test('company interest form renders', async ({ page }) => {
   await page.goto('/interesse');
   await expect(
     page.getByRole('heading', { name: 'Meld interesse' }),
-  ).toBeVisible();
+  ).not.toBeVisible();
 });
